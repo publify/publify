0ae6cbdc5cafa1ea7d5d631589090a29666eb3fe
tobi <tobi@tobi.com>
2005-01-21 16:42:21 +0000

    added atom feed
    git-svn-id: http://svn.typosphere.org/typo/trunk@17 820eb932-12ee-0310-9ca8-eeb645f39767

--- app/controllers/xml_controller.rb
+++ app/controllers/xml_controller.rb
@@ -1,6 +1,10 @@
 class XmlController < ApplicationController
   
   def rss
-    @articles = Article.find_all('published=1', 'created_at DESC', '20')
+    @articles = Article.find_all('published=1', 'created_at DESC', '10')
+  end
+
+  def atom
+    @articles = Article.find_all('published=1', 'created_at DESC', '10')
   end
 end
