1e2694346e22428b0b3da643722821c2d608caa7
tobi <tobi@tobi.com>
2005-03-30 14:54:39 +0000

    git-svn-id: http://svn.typosphere.org/typo/trunk@92 820eb932-12ee-0310-9ca8-eeb645f39767

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
 Dependencies.mechanism                             = :require
 ActionController::Base.consider_all_requests_local = false
 ActionController::Base.perform_caching             = true
-ActionController::Base.page_cache_​extension       = ''
+ActionController::Base.page_cache_extension        = ''
