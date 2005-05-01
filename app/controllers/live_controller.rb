001c63ede46467c49bd8595bdd00d9ee173afc35
scoop <scoop@scoop.com>
2005-05-01 16:42:41 +0000

    Force live search results into utf-8 mode
    git-svn-id: http://svn.typosphere.org/typo/trunk@260 820eb932-12ee-0310-9ca8-eeb645f39767

--- app/controllers/live_controller.rb
+++ app/controllers/live_controller.rb
@@ -3,6 +3,7 @@ class LiveController < ApplicationController
   def search
     @search = @request.raw_post
     @articles = Article.search(@search)
+    @headers["Content-Type"] = "text/html; charset=utf-8"
   end
   
 end
