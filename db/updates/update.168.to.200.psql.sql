08843a3f262380248c7c3d3699966dba340e64ae
tobi <tobi@tobi.com>
2005-07-09 22:40:35 +0000

    closes #106
    git-svn-id: http://svn.typosphere.org/typo/trunk@291 820eb932-12ee-0310-9ca8-eeb645f39767

---
+++ db/updates/update.168.to.200.psql.sql
@@ -0,0 +1,30 @@
+CREATE TABLE blacklist_patterns (
+  id SERIAL PRIMARY KEY NOT NULL,
+  type varchar(15) default NULL,
+  pattern varchar(255) default NULL
+);
+
+CREATE TABLE sessions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  sessid varchar(255) default NULL,
+  data text,
+  created_at TIMESTAMP DEFAULT now(),
+  updated_at TIMESTAMP DEFAULT now()
+);                                                                                                                                                                  
+
+CREATE TABLE settings (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name varchar(255) default NULL,
+  value varchar(255) default NULL,
+  position int default NULL
+);
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  login varchar(40) default NULL,
+  password varchar(40) default NULL,
+  UNIQUE (login)
+);
+
+ALTER TABLE comments ADD ip varchar(15) DEFAULT NULL;
+ALTER TABLE articles ADD text_filter varchar(20) DEFAULT NULL;
