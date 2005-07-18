e5137465fbebb1916473d64ef50bca81ab3f3cc1
tobi <tobi@tobi.com>
2005-07-18 14:12:56 +0000

    disable caching in development mode
    git-svn-id: http://svn.typosphere.org/typo/trunk@338 820eb932-12ee-0310-9ca8-eeb645f39767

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,5 +2,5 @@ require 'active_support/whiny_nil'
 
 Dependencies.mechanism                             = :load
 ActionController::Base.consider_all_requests_local = true
-ActionController::Base.perform_caching             = true
+ActionController::Base.perform_caching             = false
 BREAKPOINT_SERVER_PORT = 42531
