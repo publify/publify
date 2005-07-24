2bb415b9f67b5862b342e16cce52a0bdfa92e1b4
tobi <tobi@tobi.com>
2005-07-24 14:21:01 +0000

    delete files first. otherwise permission problems leave this migration in limbo
    git-svn-id: http://svn.typosphere.org/typo/trunk@401 820eb932-12ee-0310-9ca8-eeb645f39767

--- db/migrate/5_add_cache_table.rb
+++ db/migrate/5_add_cache_table.rb
@@ -2,13 +2,13 @@ require 'fileutils'
 
 class AddCacheTable < ActiveRecord::Migration
   def self.up
+    FileUtils.rm_rf("public/articles")
+    FileUtils.rm_rf("public/xml")
+    FileUtils.rm_rf("public/index.html")
     create_table :page_caches do |t|
       t.column :name, :string
     end
     add_index :page_caches,:name
-    FileUtils.rm_rf("public/articles")
-    FileUtils.rm_rf("public/xml")
-    FileUtils.rm_rf("public/index.html")
   end
 
   def self.down
