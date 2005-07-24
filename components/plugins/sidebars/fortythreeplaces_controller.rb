30d5093c1280b1efed225319bd420a414b12e16b
scoop <scoop@scoop.com>
2005-07-24 10:27:29 +0000

    Added sidebar plugin for 43places.com (closes #199) [robby@planetargon.com]
    git-svn-id: http://svn.typosphere.org/typo/trunk@397 820eb932-12ee-0310-9ca8-eeb645f39767

---
+++ components/plugins/sidebars/fortythreeplaces_controller.rb
@@ -0,0 +1,20 @@
+class Plugins::Sidebars::FortythreeplacesController < Sidebars::Plugin
+  def self.display_name
+    "43places"
+  end
+
+  def self.description
+    "List of your <a href=""http://www.43places.com/"">43places.com</a>."
+  end
+
+  def self.default_config
+    {'feed'=>'http://www.43places.com/rss/uber/author?username=USER','count'=>43}
+  end
+
+  def content
+    @fortythreeplaces=check_cache(Fortythree, @sb_config['feed']) rescue nil
+  end
+
+  def configure
+  end
+end
