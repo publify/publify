1382604f761c46573cb15ae456f9d8e51ba28845
tobi <tobi@tobi.com>
2005-07-24 17:21:43 +0000

    When there are unapplied migrations available you will be taken to a update schema page in the admin interface.
    git-svn-id: http://svn.typosphere.org/typo/trunk@407 820eb932-12ee-0310-9ca8-eeb645f39767

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,27 @@
 class Admin::BaseController < ApplicationController
   layout 'administration'
-  
   before_filter :login_required, :except => [ :login, :signup ]
+  before_filter :look_for_needed_db_updates, :except => [:update_database, :migrate]
+  private
+  
+  def look_for_needed_db_updates
+    redirect_to :controller => '/admin/general', :action => 'update_database' if current_schema_version != max_schema_version
+  end
+  
+  def migrations_path
+    "#{RAILS_ROOT}/db/migrate"
+  end
+
+  def available_migrations
+    @available_migrations ||= Dir["#{migrations_path}/[0-9]*_*.rb"].sort
+  end
+
+  def current_schema_version
+    ActiveRecord::Migrator.current_version
+  end
+
+  def max_schema_version
+     available_migrations.size
+  end
+  
 end
--- app/controllers/admin/general_controller.rb
+++ app/controllers/admin/general_controller.rb
@@ -2,6 +2,22 @@ class Admin::GeneralController < Admin::BaseController
   def index
   end
   
+  def update_database
+    @current_version = current_schema_version
+    @needed_version = max_schema_version    
+    @support = ActiveRecord::Base.connection.supports_migrations?
+    @needed_migrations = available_migrations[@current_version..@needed_version].collect do |mig|   
+      mig.scan(/\d+\_([\w_]+)\.rb$/).flatten.first.humanize
+    end
+  end
+  
+  def migrate
+    if request.post?      
+      ActiveRecord::Migrator.migrate("#{migrations_path}/")
+      redirect_to :action => 'update_database'
+    end
+  end
+  
   def update
     if request.post? 
       Setting.transaction do 
@@ -17,6 +33,7 @@ class Admin::GeneralController < Admin::BaseController
   end
       
   private
+
 
     def find_or_create(name)
       unless setting = Setting.find_by_name(name)
