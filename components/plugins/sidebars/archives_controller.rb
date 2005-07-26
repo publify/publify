a0a7987ac4ca8d91ab3f067342da3306b6a33e63
scoop <scoop@scoop.com>
2005-07-26 13:29:12 +0000

    New sidebar plugin: Archives (closes #221, although rewritten to do the calculations in the controller and lose the ugly counters) [kevin@sb.org] NOTE: This would be half as ugly and half as bad performance-wise if SQLite had support for DATE_FORMAT() *sigh*
    git-svn-id: http://svn.typosphere.org/typo/trunk@452 820eb932-12ee-0310-9ca8-eeb645f39767

---
+++ components/plugins/sidebars/archives_controller.rb
@@ -0,0 +1,35 @@
+class Plugins::Sidebars::ArchivesController < Sidebars::Plugin
+  def self.display_name
+    "Archives"
+  end
+
+  def self.description
+    'Displays links to monthly archives'
+  end
+
+  def self.default_config
+    {'count' => 10, 'show_count' => true}
+  end
+
+  def content
+    @archives = Hash.new
+
+    Article.find(:all, :conditions => 'published != 0', :order => 'created_at DESC').each do |a|
+      index = a.created_at.strftime("%Y-%m").to_sym
+
+      if @archives[index].nil?
+        @archives[index] = Hash.new(0)
+        @archives[index][:name] = a.created_at.strftime("%B %Y")
+        @archives[index][:year], @archives[index][:month] = index.to_s.split("-")
+      end
+      @archives[index][:count] += 1
+      
+      break if @archives.size == @sb_config['count'].to_i
+    end
+    
+    @archives = @archives.sort_by { |index, month| index.to_s }.reverse.collect { |a| a.last }
+  end
+
+  def configure
+  end
+end
