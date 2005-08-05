36eb8fecb1bfd41c814d41d6226b6ecd36643aad
scott <scott@google.com>
2005-08-05 00:29:40 +0000

    Fix date display bug.  (closes #269)
    git-svn-id: http://svn.typosphere.org/typo/trunk@497 820eb932-12ee-0310-9ca8-eeb645f39767

--- public/javascripts/typo.js
+++ public/javascripts/typo.js
@@ -16,7 +16,7 @@ function show_dates_as_local_time() {
 function get_local_time_for_date(time) {
   system_date = new Date(time);
   user_date = new Date();
-  delta_minutes = Math.round((user_date - system_date) / (60 * 1000));
+  delta_minutes = Math.floor((user_date - system_date) / (60 * 1000));
   if (Math.abs(delta_minutes) <= (8*7*24*60)) { // eight weeks... I'm lazy to count days for longer than that
     distance = distance_of_time_in_words(delta_minutes);
     if (delta_minutes < 0) {
@@ -35,7 +35,7 @@ function distance_of_time_in_words(minutes) {
   if (minutes.isNaN) return "";
   minutes = Math.abs(minutes);
   if (minutes < 1) return ('less than a minute');
-  if (minutes < 50) return (minutes + ' minutes');
+  if (minutes < 50) return (minutes + ' minute' + (minutes == 1 ? '' : 's'));
   if (minutes < 90) return ('about one hour');
   if (minutes < 1080) return (Math.round(minutes / 60) + ' hours');
   if (minutes < 1440) return ('one day');
