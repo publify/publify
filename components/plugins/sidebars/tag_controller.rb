3ae586016c4ea4700b4e9020781338224779e477
scott <scott@google.com>
2005-08-22 13:11:26 +0000

    Limit tag sidebar sizes when there are fewer then 20 tags on the tag list.  (closes #336)
    git-svn-id: http://svn.typosphere.org/typo/trunk@542 820eb932-12ee-0310-9ca8-eeb645f39767

--- components/plugins/sidebars/tag_controller.rb
+++ components/plugins/sidebars/tag_controller.rb
@@ -10,6 +10,6 @@ class Plugins::Sidebars::TagController < Sidebars::Plugin
   def content
     @tags = Tag.find_all_with_article_counters.sort_by {|t| -t.article_counter.to_i}.slice(0..20).sort_by {|t| t.name}
     total=@tags.inject(1) {|total,tag| total += tag.article_counter.to_i }
-    @font_multiplier = 20.0*100.0/total
+    @font_multiplier = @tags.size*100.0/total
   end
 end
