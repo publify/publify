f8fcfd23e9f787f70c1178724b8918bf95d7725d
scott <scott@google.com>
2005-08-26 15:46:17 +0000

    Configurable tag sidebar.  (closes #359)
    git-svn-id: http://svn.typosphere.org/typo/trunk@582 820eb932-12ee-0310-9ca8-eeb645f39767

--- components/plugins/sidebars/tag_controller.rb
+++ components/plugins/sidebars/tag_controller.rb
@@ -7,8 +7,15 @@ class Plugins::Sidebars::TagController < Sidebars::Plugin
     "Show most popular tags for this blog"
   end
 
+  def self.default_config
+    { 'maximum_tags' => 20 }
+  end
+
+  def configure
+  end
+
   def content
-    @tags = Tag.find_all_with_article_counters.sort_by {|t| -t.article_counter.to_i}.slice(0..20).sort_by {|t| t.name}
+    @tags = Tag.find_all_with_article_counters.sort_by {|t| -t.article_counter.to_i}.slice(0...@sb_config['maximum_tags'].to_i).sort_by {|t| t.name}
     total=@tags.inject(1) {|total,tag| total += tag.article_counter.to_i }
     @font_multiplier = @tags.size*100.0/total
   end
