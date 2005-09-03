3151755ca5ad934c3cbb23927366bbd269ef5feb
scott <scott@google.com>
2005-09-03 15:31:34 +0000

    Turn on transactional tests for better performance.  This cuts 20% off of the testing time on my system.
    git-svn-id: http://svn.typosphere.org/typo/trunk@612 820eb932-12ee-0310-9ca8-eeb645f39767

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,9 +17,10 @@ User.salt = 'change-me'
 Test::Unit::TestCase.fixture_path = File.dirname(__FILE__) + "/fixtures/"
 
 class Test::Unit::TestCase
-  # Turn these on to use transactional fixtures with table_name(:fixture_name) instantiation of fixtures
-  # self.use_transactional_fixtures = true
-  # self.use_instantiated_fixtures  = false
+  # Turn these off to use instantiated fixtures.  This is really only useful with MySQL with
+  # MyISAM tables.
+  self.use_transactional_fixtures = true
+  self.use_instantiated_fixtures  = false
 
   def create_fixtures(*table_names)
     Fixtures.create_fixtures(File.dirname(__FILE__) + "/fixtures", table_names)
