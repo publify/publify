e4cc0fc4e0aa82d51029316c5c5dc19f1e9536a9
scott <scott@google.com>
2005-09-03 05:06:13 +0000

    More print/puts in migrations
    git-svn-id: http://svn.typosphere.org/typo/trunk@610 820eb932-12ee-0310-9ca8-eeb645f39767

--- db/migrate/11_add_article_id.rb
+++ db/migrate/11_add_article_id.rb
@@ -3,7 +3,7 @@ class AddArticleId < ActiveRecord::Migration
     begin
       STDERR.puts "Making your upload directory: "+File.expand_path("#{RAILS_ROOT}/public/files")
       Dir.mkdir("#{RAILS_ROOT}/public/files") unless File.directory?("#{RAILS_ROOT}/public/files") 
-      print "Trying to add article_id to resources table ..."
+      STDERR.print "Trying to add article_id to resources table ..."
       add_column :resources, :article_id, :integer
       Resource.find(:all) { |r| r.update }
       STDERR.puts " done."
@@ -14,7 +14,7 @@ class AddArticleId < ActiveRecord::Migration
 
   def self.down
     begin
-      print "Trying to drop article_id from resources table ..."
+      STDERR.print "Trying to drop article_id from resources table ..."
       remove_column :resources, :article_id
       STDERR.puts " done."
     rescue
