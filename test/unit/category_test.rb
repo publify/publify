c193861042aa4c25a6d7081e0a31b692e49e93c6
scott <scott@google.com>
2005-09-03 03:10:20 +0000

    Fix category list to not show unpublished articles.  Fix category tests to actually test using fixtures.  (closes #377)
    git-svn-id: http://svn.typosphere.org/typo/trunk@604 820eb932-12ee-0310-9ca8-eeb645f39767

--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class CategoryTest < Test::Unit::TestCase
-  fixtures :articles, :categories
+  fixtures :articles, :categories, :articles_categories
 
   def setup
     @category = Category.find(1)
@@ -12,18 +12,15 @@ class CategoryTest < Test::Unit::TestCase
   end
   
   def test_find_all_with_article_counters
-    @software.articles << @article1 << @article2
-    @personal.articles << @article3
-
     c = Category.find_all_with_article_counters
-
+    
     assert_equal @software, c[0]
     assert_equal @hardware, c[1]
     assert_equal @personal, c[2]
 
-    assert_equal @software.articles_count, c[0].article_counter.to_i
-    assert_equal @hardware.articles_count, c[1].article_counter.to_i
-    assert_equal @personal.articles_count, c[2].article_counter.to_i
+    assert_equal 1, c[0].article_counter.to_i
+    assert_equal 1, c[1].article_counter.to_i
+    assert_equal 3, c[2].article_counter.to_i
   end
 
   def test_reorder
