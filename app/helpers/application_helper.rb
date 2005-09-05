77017aa0451d82d3982da7e7d8ed2faace3e1e74
scott <scott@google.com>
2005-09-05 03:40:45 +0000

    I forgot to save.  Doh!
    git-svn-id: http://svn.typosphere.org/typo/trunk@625 820eb932-12ee-0310-9ca8-eeb645f39767

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -99,6 +99,7 @@ module ApplicationHelper
     if(article.body_html.blank?)
       article.body_html = @controller.filter_text_by_name(article.body, article.text_filter.name) rescue article.body
       article.extended_html = @controller.filter_text_by_name(article.extended, article.text_filter.name) rescue article.extended
+      article.save
     end
     
     case what
@@ -116,6 +117,7 @@ module ApplicationHelper
   def comment_html(comment)
     if(comment.body_html.blank?)
       comment.body_html = @controller.filter_text_by_name(comment.body, config[:comment_text_filter]) rescue comment.body
+      comment.save
     end
     
     comment.body_html
@@ -124,6 +126,7 @@ module ApplicationHelper
   def page_html(page)
     if(page.body_html.blank?)
       page.body_html = @controller.filter_text_by_name(page.body, page.text_filter.name) rescue page.body
+      page.save
     end
     
     page.body_html
