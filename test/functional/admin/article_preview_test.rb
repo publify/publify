39caca9e0803bebc632333dedb49faac815f70e0
scott <scott@google.com>
2005-10-15 16:59:17 +0000

    Add article preview tests.  I faled to svk add this during an earlier patch.
    git-svn-id: http://svn.typosphere.org/typo/trunk@682 820eb932-12ee-0310-9ca8-eeb645f39767

---
+++ test/functional/admin/article_preview_test.rb
@@ -0,0 +1,67 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+require 'admin/content_controller'
+require 'http_mock'
+
+class Admin::ContentController; def rescue_action(e) raise e end; end
+
+class Admin::ArticlePreviewTest < Test::Unit::TestCase
+  fixtures :contents, :users, :categories, :resources, :text_filters, :settings
+  
+  def setup
+    @controller = Admin::ContentController.new
+    @request = ActionController::TestRequest.new
+    @response = ActionController::TestResponse.new
+    @request.session = {:user => @tobi}
+
+    @art_count = Article.find(:all).size
+  end
+
+  def assert_no_new_articles
+    assert_equal @art_count, Article.find(:all).size
+  end
+
+  def test_only_title
+    post :preview, 'article' => { :title => 'A title' }
+    assert_response :success
+    assert_rendered_file 'preview'
+    assert_tag :tag => 'h4', :content => 'A title'
+    assert_no_tag :tag => 'p'
+    assert_no_new_articles
+  end
+
+  def test_only_body
+    post :preview, :article => { :body => 'A body' }
+
+    assert_tag :tag => 'p',
+      :child => 'A body',
+      :after => { :tag => 'h4', :content => nil }
+
+    assert_no_new_articles
+  end
+
+  def test_only_extended
+    post :preview, :article => { :body => 'An extension' }
+
+    assert_tag :tag => 'p',
+      :child => 'An extension',
+      :after => { :tag => 'h4', :content => nil }
+
+    assert_no_new_articles
+  end
+
+  def test_full_post
+    post :preview, :article => {
+      :title => 'A title', :body => 'A body',
+      :extended => 'An extension' }
+
+    assert_tag \
+      :tag => 'p',
+      :child => 'An extension',
+      :after => {:tag => 'p', :child => 'A body',
+        :after => { :tag => 'h4', :content => nil }}
+
+    assert_no_new_articles
+  end
+
+
+end
