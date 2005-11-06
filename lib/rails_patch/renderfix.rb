0bc17b8f3b340ed0dd663ec1e048200d4b29990c
scott <scott@google.com>
2005-11-06 06:04:51 +0000

    Add a comment to renderfix
    git-svn-id: http://svn.typosphere.org/typo/trunk@722 820eb932-12ee-0310-9ca8-eeb645f39767

--- lib/rails_patch/renderfix.rb
+++ lib/rails_patch/renderfix.rb
@@ -1,3 +1,8 @@
+#
+# Rails, by default, only looks for views in a single location, usually in app/views.
+# However, we'd *really* like to be able to override views via themes, and that means
+# adding something like a search path.  So here it is.
+
 module ActionView
   class Base
     alias_method :__render_file, :render_file
