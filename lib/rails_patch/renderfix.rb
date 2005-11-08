ad956758f8e60cfbb484640619cd0acd5d1e4ec0
scott <scott@google.com>
2005-11-08 01:30:02 +0000

    Update renderfix code to fix bugs reported by kellan on mailing list:
    1.  Exceptions in theme views cause a fallback to the default view.
    2.  Plugins (sidebars, etc) are not easy to override.
    
    Both of these should be fixed now.  To override sidebars, put the new
    views in:
    
        themes/*/views/plugins/sidebars/*/content.rhtml
    
    
    git-svn-id: http://svn.typosphere.org/typo/trunk@724 820eb932-12ee-0310-9ca8-eeb645f39767

--- lib/rails_patch/renderfix.rb
+++ lib/rails_patch/renderfix.rb
@@ -8,13 +8,21 @@ module ActionView
     alias_method :__render_file, :render_file
     
     def render_file(template_path, use_full_path = true, local_assigns = {})
+      search_path = [
+        "../themes/#{config[:theme]}/views",     # for components
+        "../../themes/#{config[:theme]}/views",  # for normal views
+        "."                                      # fallback
+      ]
+      
       if use_full_path
-        begin
-          theme_path = "../../themes/#{config[:theme]}/views/#{template_path}"
-          template_extension = pick_template_extension(theme_path)
-          __render_file(theme_path, use_full_path, local_assigns)
-        rescue => err
-          __render_file(template_path, use_full_path, local_assigns)
+        search_path.each do |prefix|
+          theme_path = prefix+'/'+template_path
+          begin
+            template_extension = pick_template_extension(theme_path)
+          rescue ActionView::ActionViewError => err
+            next
+          end
+          return __render_file(theme_path, use_full_path, local_assigns)
         end
       else
         __render_file(template_path, use_full_path, local_assigns)
