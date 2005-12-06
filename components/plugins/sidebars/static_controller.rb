bd92b56ef27781908ed95f6073b2bb713788a10d
Piers Cawley <pdcawley@bofh.org.uk>
2005-12-06 09:54:54 +0000

    r713@Menhir:  pdcawley | 2005-12-06 02:51:29 +0000  added planettypo and nubyonrails.com to the default links
    git-svn-id: http://svn.typosphere.org/typo/trunk@773 820eb932-12ee-0310-9ca8-eeb645f39767

--- components/plugins/sidebars/static_controller.rb
+++ components/plugins/sidebars/static_controller.rb
@@ -17,6 +17,8 @@ class Plugins::Sidebars::StaticController < Sidebars::Plugin
   <li><a href="http://encytemedia.com" title="Encyte Media">EncyteMedia</a></li>
   <li><a href="http://scottstuff.net" title="Scottstuff">scottstuff.net</a></li>
   <li><a href="http://www.bofh.org.uk" title="Just a Summary">Just A Summary</a></li>
+  <li><a href="http://nubyonrails.com" title="Topfunky">Topfunky</a></li>
+  <li><a href="http://planettypo.com" title="PlanetTypo">PlanetTypo</a></li>
 </ul>
 }}
   end
