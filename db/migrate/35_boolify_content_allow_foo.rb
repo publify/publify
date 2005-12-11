c25d6e311e2ad94262c455b49d619af2f1015b01
Piers Cawley <pdcawley@bofh.org.uk>
2005-12-11 22:42:47 +0000

    Applied Adam Greenfield's patch from ticket 597
    git-svn-id: http://svn.typosphere.org/typo/trunk@797 820eb932-12ee-0310-9ca8-eeb645f39767

--- db/migrate/35_boolify_content_allow_foo.rb
+++ db/migrate/35_boolify_content_allow_foo.rb
@@ -1,32 +1,40 @@
 class BoolifyContentAllowFoo < ActiveRecord::Migration
-  class << self
-    def up
-      STDERR.puts "Boolifying contents.allow_(comments|pings)"
-      Content.transaction do
-        rename_column :contents, :allow_pings, :old_ap
-        add_column :contents, :allow_pings, :boolean
-        rename_column :contents, :allow_comments, :old_ac
-        add_column :contents, :allow_comments, :boolean
-        
-        if not $schema_generator
-          Content.connection.select_all(%{
-          SELECT
-            id, old_ap, old_ac
-          FROM contents}).each do |res|
-            if !res["oldval"].nil?
-              c = Content.find(res["id"])
-              c.allow_pings = !res["old_ap"].to_i.zero?
-              c.allow_comments = !res["old_ac"].to_i.zero?
-              c.save
-            end
-          end
+  def self.up
+    STDERR.puts "Boolifying contents.allow_(comments|pings)"
+    Content.transaction do
+      rename_column :contents, :allow_pings, :old_ap
+      add_column :contents, :allow_pings, :boolean
+      rename_column :contents, :allow_comments, :old_ac
+      add_column :contents, :allow_comments, :boolean
+
+      unless $schema_generator
+        Article.find(:all).each do |c|
+          c.allow_pings = !c.old_ap.zero? ? true : false unless c.old_ap.nil?
+          c.allow_comments = !c.old_ac.zero? ? true : false unless c.old_ac.nil?
+          c.save
         end
-        remove_column :contents, :old_ap
-        remove_column :contents, :old_ac
       end
+      remove_column :contents, :old_ap
+      remove_column :contents, :old_ac
     end
-    def self.down
-      raise "Can't downgrade"
+  end
+  def self.down
+    STDERR.puts "Un-Boolifying contents.allow_(comments|pings)"
+    Content.transaction do 
+      rename_column :contents, :allow_pings, :old_ap
+      add_column :contents, :allow_pings, :integer
+      rename_column :contents, :allow_comments, :old_ac
+      add_column :contents, :allow_comments, :integer
+
+      unless $schema_generator
+        Article.find(:all).each do |c|
+          c.allow_pings = c.old_ap ? 1 : 0 unless c.old_ap.nil?
+          c.allow_comments = c.old_ac ? 1 : 0 unless c.old_ac.nil?
+          c.save
+        end
+      end
+      remove_column :contents, :old_ap
+      remove_column :contents, :old_ac
     end
   end
 end
