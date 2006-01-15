bcb0f1b1115c8b2de937df630bb15479bd5bd6ed
kevin <kevin@kevin.com>
2006-01-15 15:02:51 +0000

    Fix the None textfilter
    git-svn-id: http://svn.typosphere.org/typo/trunk@842 820eb932-12ee-0310-9ca8-eeb645f39767

--- components/plugins/textfilters/none_controller.rb
+++ components/plugins/textfilters/none_controller.rb
@@ -2,7 +2,7 @@ class Plugins::Textfilters::NoneController < TextFilterPlugin::Markup
   plugin_display_name "None"
   plugin_description 'Raw HTML only'
 
-  def self.filter(controller,text,params)
+  def self.filtertext(controller,text,params)
     text
   end
 end
