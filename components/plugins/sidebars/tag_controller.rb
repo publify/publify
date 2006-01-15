16bdbc517d3f228d8f19c98518993178ec0be9ba
kevin <kevin@kevin.com>
2006-01-15 15:21:22 +0000

    Limit tag size to 66% instead of 50%
    git-svn-id: http://svn.typosphere.org/typo/trunk@844 820eb932-12ee-0310-9ca8-eeb645f39767

--- components/plugins/sidebars/tag_controller.rb
+++ components/plugins/sidebars/tag_controller.rb
@@ -18,9 +18,9 @@ class Plugins::Sidebars::TagController < Sidebars::Plugin
     @tags = Tag.find_all_with_article_counters(@sb_config['maximum_tags'].to_i).sort_by {|t| t.name}
     total=@tags.inject(0) {|total,tag| total += tag.article_counter }
     average = total.to_f / @tags.size.to_f
-    @sizes = @tags.inject({}) {|h,tag| h[tag] = (tag.article_counter.to_f / average) * 100; h} # create a percentage
+    @sizes = @tags.inject({}) {|h,tag| h[tag] = (tag.article_counter.to_f / average); h} # create a percentage
     # apply a lower limit of 50% and an upper limit of 200%
-    @sizes.each {|tag,size| @sizes[tag] = [[50.0, size].max, 200.0].min}
+    @sizes.each {|tag,size| @sizes[tag] = [[2.0/3.0, size].max, 2].min * 100}
     @font_multiplier = 80 # remove this once themes stop using it
   end
 end
