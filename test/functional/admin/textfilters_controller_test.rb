77fb46530fb6dc79a64208c8a4333b6704cd4b4d
kevin <kevin@kevin.com>
2006-03-11 04:17:34 +0000

    Fix admin/textfixtures_controller_test.rb, whose contents had somehow gotten duplicated
    git-svn-id: http://svn.typosphere.org/typo/trunk@896 820eb932-12ee-0310-9ca8-eeb645f39767

--- test/functional/admin/textfilters_controller_test.rb
+++ test/functional/admin/textfilters_controller_test.rb
@@ -27,32 +27,3 @@ class Admin::TextfiltersControllerTest < Test::Unit::TestCase
     assert_redirected_to :action => 'show'
   end
 end
-require File.dirname(__FILE__) + '/../../test_helper'
-require 'admin/textfilters_controller'
-
-# Re-raise errors caught by the controller.
-class Admin::TextfiltersController; def rescue_action(e) raise e end; end
-
-class Admin::TextfiltersControllerTest < Test::Unit::TestCase
-  fixtures :text_filters, :users
-
-  def setup
-    @controller = Admin::TextfiltersController.new
-    @request    = ActionController::TestRequest.new
-    @response   = ActionController::TestResponse.new
-    
-    @request.session = { :user => users(:tobi) }    
-  end
-
-  def test_new_without_filters
-    post :new, :textfilter => { :name => 'filterx', 
-      :description => 'Filter X', :markup => 'markdown' }
-    assert_redirected_to :action => 'show'
-  end
-
-  def test_edit_without_filters
-    post :edit, :id => 1, :textfilter => { :name => 'filterx', 
-      :description => 'Filter X', :markup => 'markdown' }
-    assert_redirected_to :action => 'show'
-  end
-end
