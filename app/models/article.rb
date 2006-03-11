deba7786ffa6b5423779c17e3469fed41f84f6af
kevin <kevin@kevin.com>
2006-03-11 04:12:43 +0000

    Remove 1 second from end of Article#time_delta range
    git-svn-id: http://svn.typosphere.org/typo/trunk@895 820eb932-12ee-0310-9ca8-eeb645f39767

--- app/models/article.rb
+++ app/models/article.rb
@@ -204,6 +204,7 @@ class Article < Content
     to = from.next_year
     to = from.next_month unless month.blank?
     to = from + 1.day unless day.blank?
+    to = to - 1 # pull off 1 second so we don't overlap onto the next day
     return [from, to]
   end
   
