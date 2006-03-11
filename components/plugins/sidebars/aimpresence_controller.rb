9d6987a2962e9c1fb8b01c4f74d4d556ed90ab80
kevin <kevin@kevin.com>
2006-03-11 10:05:42 +0000

    Add new AIM Presence sidebar. Closes #667
    git-svn-id: http://svn.typosphere.org/typo/trunk@898 820eb932-12ee-0310-9ca8-eeb645f39767

---
+++ components/plugins/sidebars/aimpresence_controller.rb
@@ -0,0 +1,18 @@
+require 'time'
+
+class Plugins::Sidebars::AimpresenceController < Sidebars::Plugin
+	def self.display_name
+		"AIM Presence"
+	end
+	
+	def self.description
+		'Displays the <a href="http://www.aim.com/presence">online presence</a> of an AOL Instant Messenger screen name'
+	end
+	
+	def self.default_config
+		{'sn' => '', 'devkey' => ''}
+	end
+	
+	def configure
+	end
+end
