81b5b7c95057dacaa948fe9f9a8242b533947834
kevin <kevin@kevin.com>
2006-03-25 12:14:31 +0000

    Add new sidebar for Xbox Live Gamer Card.
    git-svn-id: http://svn.typosphere.org/typo/trunk@946 820eb932-12ee-0310-9ca8-eeb645f39767

---
+++ components/plugins/sidebars/xbox_controller.rb
@@ -0,0 +1,16 @@
+class Plugins::Sidebars::XboxController < Sidebars::Plugin
+  def self.display_name
+    "Xbox Gamer Card"
+  end
+
+  def self.description
+    "Displays your Xbox Live Gamer Card"
+  end
+
+  def self.default_config
+    {'gamertag' => ''}
+  end
+
+  def configure
+  end
+end
