497e16a0f98611af5876e12481dd83b81bcafec9
kevin <kevin@kevin.com>
2006-04-13 07:38:33 +0000

    Tweak permalink transformation again so existing tests don't break
    git-svn-id: http://svn.typosphere.org/typo/trunk@1014 820eb932-12ee-0310-9ca8-eeb645f39767

--- lib/transforms.rb
+++ lib/transforms.rb
@@ -4,7 +4,7 @@ class String
   def to_url
     return if self.nil?
 
-    self.downcase.tr("\"'", '').strip.gsub(/\W/, '-').tr(' ', '-').squeeze('-')
+    self.downcase.tr("\"'", '').gsub(/\W/, ' ').strip.tr_s(' ', '-').tr(' ', '-').sub(/^$/, "-")
   end
 
   # A quick and dirty fix to add 'nofollow' to any urls in a string.
