b086d44d9f2877825461484b78441666b66e2bd1
kevin <kevin@kevin.com>
2006-05-03 09:22:37 +0000

    Fix #832 - Xbox Live gamertag sidebar broken
    git-svn-id: http://svn.typosphere.org/typo/trunk@1038 820eb932-12ee-0310-9ca8-eeb645f39767

--- components/plugins/sidebars/xbox_controller.rb
+++ components/plugins/sidebars/xbox_controller.rb
@@ -2,5 +2,5 @@ class Plugins::Sidebars::XboxController < Sidebars::ComponentPlugin
   display_name "Xbox Gamer Card"
   description "Displays your Xbox Live Gamer Card"
 
-  setting :gamertag, '', 'Xbox Live gamertag'
+  setting :gamertag, '', :label => 'Xbox Live gamertag'
 end
