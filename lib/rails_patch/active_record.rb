e9f6b3b8657461081a9982d3ab2d3733c729d5d6
Piers Cawley <pdcawley@bofh.org.uk>
2006-05-13 07:00:17 +0000

    Fixed a SQL syntax error in the monkey patch
    git-svn-id: http://svn.typosphere.org/typo/trunk@1051 820eb932-12ee-0310-9ca8-eeb645f39767

--- lib/rails_patch/active_record.rb
+++ lib/rails_patch/active_record.rb
@@ -12,7 +12,7 @@ module ActiveRecord
       def change_column(table_name, column_name, type, options ={ }) #:nodoc:
         if options[:default].nil?
           options[:default] =
-            select_one("SHOW COLUMNS FROM #{table_name} LIKE #{column_name}")["Default"]
+            select_one("SHOW COLUMNS FROM #{table_name} LIKE '#{column_name}'")["Default"]
         end
         change_column_sql = "ALTER TABLE #{table_name} CHANGE #{column_name} #{column_name} #{type_to_sql(type, options[:limit])}"
         add_column_options!(change_column_sql, options)
