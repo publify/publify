365d4979470499c6475761bc6534420e97f8ec9f
scott <scott@google.com>
2006-06-25 05:34:03 +0000

    Make BlogSweeper watch User, too.  Closes #947
    git-svn-id: http://svn.typosphere.org/typo/trunk@1062 820eb932-12ee-0310-9ca8-eeb645f39767

--- app/models/blog_sweeper.rb
+++ app/models/blog_sweeper.rb
@@ -1,5 +1,5 @@
 class BlogSweeper < ActionController::Caching::Sweeper
-  observe Article, Category, Comment, Trackback, Page, Blog, Sidebar
+  observe Article, Category, Comment, Trackback, Page, Blog, Sidebar, User
 
   def after_save(record)
     expire_for(record)
@@ -11,7 +11,7 @@ class BlogSweeper < ActionController::Caching::Sweeper
 
   def expire_for(record)
     case record
-    when Blog, Sidebar, Category, Comment, Trackback, Article
+    when Blog, Sidebar, Category, Comment, Trackback, Article, User
       PageCache.sweep_all
       expire_fragment(/.*/)
     when Page
