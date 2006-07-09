7ed46295e0f8dd69614c473a6b056347aeb8bad8
scott <scott@google.com>
2006-07-09 04:48:16 +0000

    Partial MySQL archives sidebar fix
    git-svn-id: http://svn.typosphere.org/typo/trunk@1089 820eb932-12ee-0310-9ca8-eeb645f39767

--- components/plugins/sidebars/archives_controller.rb
+++ components/plugins/sidebars/archives_controller.rb
@@ -16,10 +16,10 @@ class Plugins::Sidebars::ArchivesController < Sidebars::ComponentPlugin
       date_func = "extract(year from published_at)||' '||lpad(extract(month from published_at),2,'0')"
     end
     
-    article_counts = Content.find_by_sql(["select count(*) as count, #{date_func} as date from contents where type='Article' and published = ? and published_at < ? group by date order by date desc limit ?",true,Time.now,count])
+    article_counts = Content.find_by_sql(["select count(*) as count, #{date_func} as date from contents where type='Article' and published = ? and published_at < ? group by date order by date desc limit ?",true,Time.now,count.to_i])
     
     @archives = article_counts.map do |entry|
-      year,month=entry.date.split(/ /)
+      year,month=entry.date.split(/ +/)
       {
         :name => entry.date,
         :month => month.to_i,
