a61727fb1d5e5d04d9e3d152a0d2039c0d0c2558
kevin <kevin@kevin.com>
2006-07-23 07:02:57 +0000

    Fix ul_tag_for to compare classes correctly
    git-svn-id: http://svn.typosphere.org/typo/trunk@1164 820eb932-12ee-0310-9ca8-eeb645f39767

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -151,10 +151,10 @@ module ArticlesHelper
   end
 
   def ul_tag_for(grouping_class)
-    case grouping_class
-    when Tag
+    case
+    when grouping_class == Tag
       %{<ul id="taglist" class="tags">}
-    when Category
+    when grouping_class == Category
       %{<ul class="categorylist">}
     else
       '<ul>'
