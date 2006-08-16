3a3e104828eb5371097e28a9a994cff035277266
scott <scott@google.com>
2006-08-16 15:43:54 +0000

    Add blockquote to the allowed comment tags.  Closes #1087
    git-svn-id: http://svn.typosphere.org/typo/trunk@1221 820eb932-12ee-0310-9ca8-eeb645f39767

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -42,7 +42,7 @@ class Comment < Feedback
   end
 
   def body_html_postprocess(value, controller)
-    sanitize(controller.send(:auto_link, value),'a href, b, br, i, p, em, strong, pre, code, ol, ul, li')
+    sanitize(controller.send(:auto_link, value),'a href, b, br, i, p, em, strong, pre, code, ol, ul, li, blockquote')
   end
 
   def default_text_filter_config_key
