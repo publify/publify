dd71892a5485c172cc9986e88d352a4c0c7489bc
kevin <kevin@kevin.com>
2006-09-06 03:55:08 +0000

    The components patch is useless now that we're no longer using components, and the active_record connection adaptors patch appears unnecessary, judging by the source for activerecord-1.14.4
    git-svn-id: http://svn.typosphere.org/typo/trunk@1261 820eb932-12ee-0310-9ca8-eeb645f39767

--- lib/rails_patch/active_record.rb
+++ lib/rails_patch/active_record.rb
@@ -5,32 +5,5 @@ module ActiveRecord
                 :conditions => ["#{field} = ?", value], *options)
     end
   end
-
-  # Horrible monkey patch 'til Rails fixes this
-  module ConnectionAdapters
-    class MysqlAdapter
-      def change_column(table_name, column_name, type, options ={ }) #:nodoc:
-        if options[:default].nil?
-          options[:default] =
-            select_one("SHOW COLUMNS FROM #{table_name} LIKE '#{column_name}'")["Default"]
-        end
-        change_column_sql = "ALTER TABLE #{table_name} CHANGE #{column_name} #{column_name} #{type_to_sql(type, options[:limit])}"
-        add_column_options!(change_column_sql, options)
-        execute(change_column_sql)
-      end
-    end
-
-    class SqliteAdapter
-      def change_column(table_name, column_name, type, options ={ }) #:nodoc:
-        alter_table(table_name) do |definition|
-          definition[column_name].instance_eval do
-            self.type    = type
-            self.limit   = options[:limit] if options[:limit]
-            self.default = options[:default] unless options[:default].nil?
-          end
-        end
-      end
-    end
-  end
 end
 
