01a8e0b62cbe1eb8ef0619a694e451320a884480
Piers Cawley <pdcawley@bofh.org.uk>
2006-11-24 10:56:34 +0000

    A possible fix for an sqlite issue
    git-svn-id: http://svn.typosphere.org/typo/trunk@1303 820eb932-12ee-0310-9ca8-eeb645f39767

--- app/models/content_state/factory.rb
+++ app/models/content_state/factory.rb
@@ -1,7 +1,7 @@
 module ContentState
   class Factory
     def self.new(state_name)
-      return ContentState::New.instance unless state_name
+      return ContentState::New.instance if !state_name || state_name.blank?
       state_name = state_name.to_s.underscore
       unless state_name.rindex('/')
         state_name = 'content_state/' + state_name
