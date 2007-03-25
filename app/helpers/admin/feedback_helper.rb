df61ab2393ce33e825504f2b4f25d8222ead3bbf
Frédéric de Villamil <frederic@de-villamil.com>
2007-03-25 09:31:40 +0000

    git-svn-id: http://svn.typosphere.org/typo/trunk@1400 820eb932-12ee-0310-9ca8-eeb645f39767

--- app/helpers/admin/feedback_helper.rb
+++ app/helpers/admin/feedback_helper.rb
@@ -3,10 +3,10 @@ module Admin::FeedbackHelper
     link_to truncate(article.title, 60), :controller => '/admin/content', :action => 'edit', :id => article.id
   end
 
-  def task_showmod(title)
+  def task_showmod(title, action)
     content_tag :li,
-      link_to(title, :published => (title =~ /spam/ ? 'f' : ''),
-                     :confirmed => (title =~ /unconfirmed/ ? 'f' : ''),
+      link_to(title, :published => (action =~ /spam/ ? 'f' : ''),
+                     :confirmed => (action =~ /unconfirmed/ ? 'f' : ''),
                      :search => params[:search])
   end
 end
