c4a90054fd9eab615199934b32cc9ea8b10c0bb5
Piers Cawley <pdcawley@bofh.org.uk>
2007-05-19 07:00:09 +0000

    Fixed/removed tests broken by the removal of sidebars from the core.
    git-svn-id: http://svn.typosphere.org/typo/trunk@1457 820eb932-12ee-0310-9ca8-eeb645f39767

--- test/unit/sidebar_test.rb
+++ test/unit/sidebar_test.rb
@@ -4,6 +4,6 @@ class SidebarTest < Test::Unit::TestCase
   fixtures :sidebars
 
   def test_available_sidebars
-    assert Sidebar.available_sidebars.size >= 19
+    assert Sidebar.available_sidebars.size >= 6
   end
 end
