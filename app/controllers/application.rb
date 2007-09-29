e83837839a631ac81d4dd4c03bda120a40ead60a
Frédéric de Villamil <frederic@de-villamil.com>
2007-09-29 06:44:52 +0000

    Removed resource consuming and not reliable sitealizer.
    git-svn-id: http://svn.typosphere.org/typo/trunk@1539 820eb932-12ee-0310-9ca8-eeb645f39767

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,8 +2,7 @@
 # Likewise will all the methods added be available for all controllers.
 class ApplicationController < ActionController::Base
   include LoginSystem
-  include Sitealizer
-  before_filter :use_sitealizer, :reset_local_cache, :fire_triggers
+  before_filter :reset_local_cache, :fire_triggers
   after_filter :reset_local_cache
 
   protected
--- vendor/plugins/sitealizer/test/test_helper.rb
+++
@@ -1,7 +0,0 @@
-require File.dirname(__FILE__) + '/../../../../config/environment'
-require 'rubygems'
-require 'test/unit'
-require 'active_record/fixtures'
-require 'action_controller/test_process'
-
-Test::Unit::TestCase.fixture_path = File.dirname(__FILE__) + "/fixtures/"
