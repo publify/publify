9eb77adcf1c1a9d5c277b7029b6d5c18433586a0
Frédéric de Villamil <frederic@de-villamil.com>
2007-10-16 21:30:40 +0000

    Add the comment counting on the feedback menus Limit to spam, Limit to unconfirmed and Limit to unconfirmed spam
    Add the link to the new iconset replacing famfamfa
    
    git-svn-id: http://svn.typosphere.org/typo/trunk@1547 820eb932-12ee-0310-9ca8-eeb645f39767

--- app/helpers/admin/feedback_helper.rb
+++ app/helpers/admin/feedback_helper.rb
@@ -9,4 +9,16 @@ module Admin::FeedbackHelper
                      :confirmed => (action =~ /unconfirmed/ ? 'f' : ''),
                      :search => params[:search])
   end
+  
+  def count_spam
+    _('Limit to spam') + " (" + Feedback.count(:all, :conditions => "state = 'spam'").to_s + ")"
+  end
+  
+  def count_unconfirmed
+    _("Limit to unconfirmed") + " (" + Feedback.count(:all, :conditions => "state in ('presumed_ham', 'presumed_spam')").to_s + ")"
+  end
+  
+  def count_unconfirmed_spam 
+    _('Limit to unconfirmed spam') + " (" + Feedback.count(:all, :conditions => "state = 'presumed_spam'").to_s + ")"
+  end
 end
