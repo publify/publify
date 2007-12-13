3d56f537cb82f50cdb17b09759706c1478f01831
Frédéric de Villamil <frederic@de-villamil.com>
2007-12-13 17:07:14 +0000

    Fixes rails 2.0 migration
    git-svn-id: http://svn.typosphere.org/typo/branches/rails_2@1564 820eb932-12ee-0310-9ca8-eeb645f39767

--- vendor/plugins/page_sidebar/lib/page_sidebar.rb
+++ vendor/plugins/page_sidebar/lib/page_sidebar.rb
@@ -5,6 +5,6 @@ class PageSidebar < Sidebar
   setting :maximum_pages, 10
 
   def pages
-    @pages ||= Page.find_all.sort_by {|t| t.name}
+    @pages ||= Page.find(:all).sort_by {|t| t.name}
   end
 end
