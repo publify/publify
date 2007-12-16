6d474bf845db074a02fa656dbcac5cd3f195dccc
Frédéric de Villamil <frederic@de-villamil.com>
2007-12-16 16:36:19 +0000

    forgot 1 thing
    git-svn-id: http://svn.typosphere.org/typo/branches/rails_2@1573 820eb932-12ee-0310-9ca8-eeb645f39767

--- app/helpers/admin/themes_helper.rb
+++ app/helpers/admin/themes_helper.rb
@@ -4,7 +4,7 @@ module Admin::ThemesHelper
     list = '<ul>'
     Find.find(this_blog.current_theme.path + "/stylesheets") do |path|
       if path =~ /css$/ 
-        list << content_tag(:li, link_to(File.basename(path), {:controller => 'themes', :action => 'edit', :type => 'stylesheet', :file => File.basename(path)}))
+        list << content_tag(:li, link_to(File.basename(path), {:controller => 'themes', :action => 'editor', :type => 'stylesheet', :file => File.basename(path)}))
       end
     end
     list << '</ul>'
@@ -15,7 +15,7 @@ module Admin::ThemesHelper
     list = '<ul>'
     Find.find(this_blog.current_theme.path + "/layouts") do |path|
       if path =~ /rhtml$|erb$/ 
-        list << content_tag(:li, link_to(File.basename(path), {:controller => 'themes', :action => 'edit', :type => 'layout', :file => File.basename(path)}))
+        list << content_tag(:li, link_to(File.basename(path), {:controller => 'themes', :action => 'editor', :type => 'layout', :file => File.basename(path)}))
       end
     end
     list << '</ul>'
