017be9ef4d856b4465e1ed3028434bc26efa7e37
Frédéric de Villamil <frederic@de-villamil.com>
2007-12-16 08:22:49 +0000

    Introducing Typo theme editor
    git-svn-id: http://svn.typosphere.org/typo/branches/rails_2@1569 820eb932-12ee-0310-9ca8-eeb645f39767

--- app/helpers/admin/themes_helper.rb
+++ app/helpers/admin/themes_helper.rb
@@ -1,2 +1,24 @@
 module Admin::ThemesHelper
+  def fetch_stylesheets
+    list = '<ul>'
+    Find.find(this_blog.current_theme.path + "/stylesheets") do |path|
+      if path =~ /css$/ 
+        list << content_tag(:li, link_to(File.basename(path), {:controller => 'themes', :action => 'edit', :type => 'stylesheet', :file => File.basename(path)}))
+      end
+    end
+    list << '</ul>'
+    list
+  end
+
+  def fetch_layouts
+    list = '<ul>'
+    Find.find(this_blog.current_theme.path + "/layouts") do |path|
+      if path =~ /rhtml$|erb$/ 
+        list << content_tag(:li, link_to(File.basename(path), {:controller => 'themes', :action => 'edit', :type => 'layout', :file => File.basename(path)}))
+      end
+    end
+    list << '</ul>'
+    list
+  end
+  
 end
