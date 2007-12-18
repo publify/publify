f939540c7a085d7b9ef450a93ade800b1b943bda
Piers Cawley <pdcawley@bofh.org.uk>
2007-12-18 03:18:48 +0000

    Tests suite is green. Patchy, but green.
    git-svn-id: http://svn.typosphere.org/typo/branches/rails_2@1577 820eb932-12ee-0310-9ca8-eeb645f39767

--- vendor/plugins/archives_sidebar/lib/archives_sidebar.rb
+++ vendor/plugins/archives_sidebar/lib/archives_sidebar.rb
@@ -5,17 +5,26 @@ class ArchivesSidebar < Sidebar
 
   attr_accessor :archives
 
+  def self.date_func
+    @date_func ||=
+      begin
+        if Content.connection.kind_of?(ActiveRecord::ConnectionAdapters::SQLiteAdapter)
+          "strftime('%Y', published_at) as year, strftime('%m', published_at) as month"
+        else
+          "extract(year from published_at) as year,extract(month from published_at) as month"
+        end
+      rescue NameError
+        "extract(year from published_at) as year,extract(month from published_at) as month"
+      end
+  end
+
   def parse_request(contents, params)
     # The original query that was here instantiated every article and every
     # tag, and then sorted through them just to do a 'group by date'.
     # Unfortunately, there's no universally-supported way to do this
     # across all three of our supported DBs.  So, we resort to a bit of
     # DB-specific code.
-    if Content.connection.kind_of? ActiveRecord::ConnectionAdapters::SQLiteAdapter
-      date_func = "strftime('%Y', published_at) as year, strftime('%m', published_at) as month"
-    else
-      date_func = "extract(year from published_at) as year,extract(month from published_at) as month"
-    end
+    date_func = self.class.date_func
 
     article_counts = Content.find_by_sql(["select count(*) as count, #{date_func} from contents where type='Article' and published = ? and published_at < ? group by year,month order by year desc,month desc limit ? ",true,Time.now,count.to_i])
 
