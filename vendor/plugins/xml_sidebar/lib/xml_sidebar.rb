82bf2fa26e6c375fb62efefca4c29f8ce52e6f97
Piers Cawley <pdcawley@bofh.org.uk>
2008-02-25 18:33:47 +0000

    made atom the default feed type in the xml side bar
    git-svn-id: http://svn.typosphere.org/typo/trunk@1663 820eb932-12ee-0310-9ca8-eeb645f39767

--- vendor/plugins/xml_sidebar/lib/xml_sidebar.rb
+++ vendor/plugins/xml_sidebar/lib/xml_sidebar.rb
@@ -6,6 +6,6 @@ class XmlSidebar < Sidebar
   setting :comments,   true,  :input_type => :checkbox
   setting :trackbacks, false, :input_type => :checkbox
 
-  setting :format, 'rss20', :input_type => :radio,
+  setting :format, 'atom10', :input_type => :radio,
           :choices => [["rss20",  "RSS 2.0"], ["atom10", "Atom 1.0"]]
 end
