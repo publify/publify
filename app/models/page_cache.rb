931a7822fe6ccfadd17025d4c75121820cd090b2
Frédéric de Villamil <frederic@de-villamil.com>
2008-05-01 09:36:06 +0000

    Fixes another remaining cache issue
    git-svn-id: http://svn.typosphere.org/typo/trunk@1691 820eb932-12ee-0310-9ca8-eeb645f39767

--- app/models/page_cache.rb
+++ app/models/page_cache.rb
@@ -14,17 +14,17 @@ class PageCache
   def self.sweep_all
     logger.debug "PageCache - sweep_all called by #{caller[1].inspect}"
     unless Blog.default && Blog.default.cache_option == "caches_action_with_params"
-      self.zap_pages('index.*', 'articles.*', 'articles', 'pages',
-                     'pages.*', 'feedback', 'feedback.*',
-                     'comments', 'comments.*',
-                     'categories', 'categories.*',
-                     'tags', 'tags.*')
+      self.zap_pages(%w{index.* articles.* articles pages
+                     pages.* feedback feedback.*
+                     comments comments.*
+                     categories categories.*
+                     tags, tags.*})
     end
   end
 
   def self.sweep_theme_cache
     logger.debug "PageCache - sweep_theme_cache called by #{caller[1].inspect}"
-    self.zap_pages('images/theme', 'stylesheets/theme', 'javascripts/theme')
+    self.zap_pages(%{images/theme stylesheets/theme javascripts/theme})
   end
 
   def self.zap_pages(paths)
