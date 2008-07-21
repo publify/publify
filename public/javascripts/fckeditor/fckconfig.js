efb4f50f8e0336db327c764566c070f4814fe925
Frédéric de Villamil <frederic@de-villamil.com>
2008-07-21 16:53:18 +0000

    Fixing IE6 bugs
    git-svn-id: http://svn.typosphere.org/typo/trunk@1764 820eb932-12ee-0310-9ca8-eeb645f39767

--- public/javascripts/fckeditor/fckconfig.js
+++ public/javascripts/fckeditor/fckconfig.js
@@ -141,7 +141,7 @@ FCKConfig.BrowserContextMenuOnCtrl = false ;
 FCKConfig.EnableMoreFontColors = true ;
 FCKConfig.FontColors = '000000,993300,333300,003300,003366,000080,333399,333333,800000,FF6600,808000,808080,008080,0000FF,666699,808080,FF0000,FF9900,99CC00,339966,33CCCC,3366FF,800080,999999,FF00FF,FFCC00,FFFF00,00FF00,00FFFF,00CCFF,993366,C0C0C0,FF99CC,FFCC99,FFFF99,CCFFCC,CCFFFF,99CCFF,CC99FF,FFFFFF' ;
 
-FCKConfig.FontFormats	= 'p;h1;h2;h3;h4;h5;blockquote;pre;address;div' ;
+FCKConfig.FontFormats	= 'p;h1;h2;h3;h4;h5;pre;address;div' ;
 FCKConfig.FontNames		= 'Arial;Courier New;Tahoma;Times New Roman;Verdana' ;
 FCKConfig.FontSizes		= 'smaller;larger;xx-small;x-small;small;medium;large;x-large;xx-large' ;
 
