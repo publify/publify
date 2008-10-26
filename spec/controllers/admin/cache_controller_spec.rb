6bc7343fc338f4d16519aacf1e54cb548291ab4c
shingara <cyril.mougel@gmail.com>
2008-10-26 17:28:38 +0100

    add spec about cache controller in admin part

---
+++ spec/controllers/admin/cache_controller_spec.rb
@@ -0,0 +1,42 @@
+require File.dirname(__FILE__) + '/../../../test/test_helper'
+require File.dirname(__FILE__) + '/../../spec_helper'
+require 'admin/users_controller'
+
+# Re-raise errors caught by the controller.
+class Admin::UsersController; def rescue_action(e) raise e end; end
+
+describe Admin::CacheController, "rough port of the old functional test" do
+
+  before(:each) do
+    request.session = { :user => users(:tobi).id }
+  end
+
+  describe 'sweep action' do
+
+    it 'should redirect to setting index' do
+      get 'sweep'
+      response.should redirect_to(:controller => '/admin/settings')
+    end
+
+    it 'should send notice' do
+      get 'sweep'
+      flash[:notice].should == 'Cache was cleared' 
+    end
+
+  end
+
+  describe 'sweep_html action' do
+
+    it 'should redirect to setting index' do
+      get 'sweep_html'
+      response.should redirect_to(:controller => '/admin/settings')
+    end
+
+    it 'should send notice' do
+      get 'sweep_html'
+      flash[:notice].should == 'Cache was cleared' 
+    end
+
+  end
+
+end
