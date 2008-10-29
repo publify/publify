0c18750a3c3e4c5d2477d4ba4ffcfeebd5478173
Frederic de Villamil <frederic@de-villamil.com>
2008-10-29 20:30:10 +0100

    adding nofollow option on lin_to_permalink

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,13 +14,13 @@ module ApplicationHelper
   end
 
   # Produce a link to the permalink_url of 'item'.
-  def link_to_permalink(item, title, anchor=nil, style=nil)
+  def link_to_permalink(item, title, anchor=nil, style=nil, nofollow=nil)
     anchor = "##{anchor}" if anchor
     case item
     when Article
-      "<a href=\"#{article_path(item)}#{anchor}\" class=\"#{style}\">#{title}</a>"
+      "<a href=\"#{article_path(item)}#{anchor}\" rel=\"#{nofollow}\" class=\"#{style}\">#{title}</a>"
     else
-      "<a href=\"#{item.permalink_url}#{anchor}\" class=\"#{style}\">#{title}</a>"
+      "<a href=\"#{item.permalink_url}#{anchor}\" rel=\"#{nofollow}\" class=\"#{style}\">#{title}</a>"
     end
   end
 
