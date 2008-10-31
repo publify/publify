d8b7011108003e416829ac97efb3e48251dae6c7
Cyril Mougel <cyril.mougel@gmail.com>
2008-10-31 19:14:15 +0100

    rewrite feedback spec with a better rspec format

--- spec/controllers/admin/feedback_controller_spec.rb
+++ spec/controllers/admin/feedback_controller_spec.rb
@@ -1,53 +1,49 @@
 require File.dirname(__FILE__) + '/../../../test/test_helper'
 require File.dirname(__FILE__) + '/../../spec_helper'
-require 'comment'
-require 'trackback'
 
 describe Admin::FeedbackController do
   before do
     request.session = { :user => users(:tobi).id }
   end
 
-  def test_index
-    get :index
+  describe 'index action' do
 
-    assert_response :success
-    assert_template 'index'
-    #FIXME : Test is useless because the pagination is on 10. Now there are 11
-    #feedback, so there are several feedback :(
-    assert_equal 10, assigns(:feedback).size #Feedback.count, assigns(:feedback).size
+    def should_success_with_index(response)
+      response.should be_success
+      response.should render_template('index')
+    end
+
+    it 'should success' do
+      get :index
+      should_success_with_index(response)
+      #FIXME : Test is useless because the pagination is on 10. Now there are 11
+      #feedback, so there are several feedback :(
+      assert_equal 10, assigns(:feedback).size #Feedback.count, assigns(:feedback).size
+    end
+
+    it 'should view only confirmed feedback' do
+      get :index, :confirmed => 'f'
+      should_success_with_index(response)
+      Feedback.count(:conditions => { :status_confirmed => false }).should == assigns(:feedback).size
+    end
+
+    it 'should view only spam feedback' do
+      get :index, :published => 'f'
+      should_success_with_index(response)
+      Feedback.count(:conditions => { :published => false }).should == assigns(:feedback).size
+    end
+
+    it 'should view unconfirmed_spam' do
+      get :index, :published => 'f', :confirmed => 'f'
+      should_success_with_index(response)
+      Feedback.count(:conditions => { :published => false, :status_confirmed => false }).should == assigns(:feedback).size
+    end
+
+    it 'should get page 1 if page params empty' do
+      get :index, :page => ''
+      should_success_with_index(response)
+    end
+
   end
 
-  def test_list_unconfirmed
-    get :index, :confirmed => 'f'
-
-    assert_response :success
-    assert_template 'index'
-
-    Feedback.count(:conditions => { :status_confirmed => false }).should == assigns(:feedback).size
-  end
-
-  def test_list_spam
-    get :index, :published => 'f'
-
-    assert_response :success
-    assert_template 'index'
-
-    Feedback.count(:conditions => { :published => false }).should == assigns(:feedback).size
-  end
-
-  def test_list_unconfirmed_spam
-    get :index, :published => 'f', :confirmed => 'f'
-
-    assert_response :success
-    assert_template 'index'
-
-    Feedback.count(:conditions => { :published => false, :status_confirmed => false }).should == assigns(:feedback).size
-  end
-
-  it 'should get page 1 if page params empty' do
-    get :index, :page => ''
-    response.should be_success
-    response.should render_template('index')
-  end
 end
