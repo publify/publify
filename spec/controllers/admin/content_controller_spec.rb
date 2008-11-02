16545ff0121f1b6d830003e5cd5e14da7a134401
Cyril Mougel <cyril.mougel@gmail.com>
2008-11-02 16:48:24 +0100

    fix bug in admin/content controller if we are not admin. Add Some spec

--- spec/controllers/admin/content_controller_spec.rb
+++ spec/controllers/admin/content_controller_spec.rb
@@ -13,134 +13,171 @@ class Admin::ContentController; def rescue_action(e) raise e end; end
 describe Admin::ContentController do
   integrate_views
 
+
+  # Like it's a shared, need call everywhere
+  describe 'index action', :shared => true do
+
+    it 'should render template index' do
+      get 'index'
+      response.should render_template('index')
+    end
+
+    it 'should see all published in index' do
+      get :index, :search => {:published => '0', :published_at => '2008-08', :user_id => '2'}
+      response.should render_template('index')
+      response.should be_success
+    end
+
+  end
+
+  describe 'show action', :shared => true do
+
+    it 'should render article' do
+      get :show, 'id' => contents(:article1).id
+      response.should render_template('show')
+      assert_template_has 'article'
+      assigns(:article).should be_valid
+      assigns(:article).should_not be_nil
+    end
+
+    it 'show article with trigger' do
+      art = Article.create!(:title => 'future article',
+                            :body => 'content',
+                            :published_at => Time.now + 2.seconds,
+                            :published => true)
+      art.should_not be_published
+      get(:show, :id => art.id)
+      assigns(:article).should_not be_published
+      sleep 3
+      get(:show, :id => art.id)
+      assigns(:article).should be_published
+    end
+
+  end
+
+
+  describe 'new action', :shared => true do
+
+    it 'should render new with get' do
+      get :new
+      response.should render_template('new')
+      assert_template_has 'article'
+    end
+
+    def base_article(options={})
+      { :title => "posted via tests!",
+        :body => "A good body",
+        :keywords => "tagged",
+        :allow_comments => '1', 
+        :allow_pings => '1' }.merge(options)
+    end
+
+    it 'should create article with no comments' do
+      post(:new, 'article' => base_article({:allow_comments => '0'}),
+                 'categories' => [categories(:software).id])
+      assigns(:article).should_not be_allow_comments
+      assigns(:article).should be_allow_pings
+      assigns(:article).should be_published
+    end
+
+    it 'should create article with no pings' do
+      post(:new, 'article' => {:allow_pings => '0'},
+                 'categories' => [categories(:software).id])
+      assigns(:article).should be_allow_comments
+      assigns(:article).should_not be_allow_pings
+      assigns(:article).should be_published
+    end
+
+    it 'should create' do
+      begin
+        ActionMailer::Base.perform_deliveries = true
+        ActionMailer::Base.deliveries = []
+        num_articles = Article.count_published_articles
+        emails = ActionMailer::Base.deliveries
+        tags = ['foo', 'bar', 'baz bliz', 'gorp gack gar']
+        post :new, 'article' => base_article(:keywords => tags) , 'categories' => [categories(:software).id]
+        assert_response :redirect, :action => 'show'
+
+        assert_equal num_articles + 1, Article.count_published_articles
+
+        new_article = Article.find(:first, :order => "id DESC")
+        assert_equal @user, new_article.user
+        assert_equal 1, new_article.categories.size
+        assert_equal [categories(:software)], new_article.categories
+        assert_equal 4, new_article.tags.size
+
+        assert_equal(1, emails.size)
+        assert_equal('randomuser@example.com', emails.first.to[0])
+      ensure
+        ActionMailer::Base.perform_deliveries = false
+      end
+    end
+
+    it 'should create article in future' do
+      assert_no_difference 'Article.count_published_articles' do
+        post(:new,
+             :article =>  base_article(:published_at => Time.now + 1.hour) )
+        assert_response :redirect, :action => 'show'
+        assigns(:article).should_not be_published
+      end
+      assert_equal 1, Trigger.count
+    end
+
+    it 'should create a filtered article' do
+      body = "body via *textile*"
+      extended="*foo*"
+      post :new, 'article' => { :title => "another test", :body => body, :extended => extended}
+      assert_response :redirect, :action => 'show'
+
+      new_article = Article.find(:first, :order => "created_at DESC")
+      assert_equal body, new_article.body
+      assert_equal extended, new_article.extended
+      assert_equal "textile", new_article.text_filter.name
+      assert_equal "<p>body via <strong>textile</strong></p>", new_article.html(:body)
+      assert_equal "<p><strong>foo</strong></p>", new_article.html(:extended)
+    end
+
+  end
+
+  describe 'destroy action', :shared => true do
+
+    it 'should_not destroy article by get' do
+      assert_no_difference 'Article.count' do
+        art_id = @article.id
+        assert_not_nil Article.find(art_id)
+
+        get :destroy, 'id' => art_id
+        response.should be_success
+      end
+    end
+
+    it 'should destroy article by post' do
+      assert_difference 'Article.count', -1 do
+        art_id = @article.id
+        post :destroy, 'id' => art_id
+        response.should redirect_to(:action => 'index')
+
+        lambda{
+          article = Article.find(art_id)
+        }.should raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+  end
+
+
   describe 'with admin connection' do
 
     before do
-      request.session = { :user => users(:tobi).id }
+      @user = users(:tobi)
+      @article = contents(:article1)
+      request.session = { :user => @user.id }
     end
 
-    describe 'index action' do
-
-      it 'should render template index' do
-        get 'index'
-        response.should render_template('index')
-      end
-
-      it 'should see all published in index' do
-        get :index, :search => {:published => '0', :published_at => '2008-08', :user_id => '2'}
-        response.should render_template('index')
-        response.should be_success
-      end
-
-    end
-
-    describe 'show action' do
-
-      it 'should render article' do
-        get :show, 'id' => contents(:article1).id
-        response.should render_template('show')
-        assert_template_has 'article'
-        assigns(:article).should be_valid
-        assigns(:article).should_not be_nil
-      end
-
-      it 'show article with trigger' do
-        art = Article.create!(:title => 'future article',
-                              :body => 'content',
-                              :published_at => Time.now + 2.seconds,
-                              :published => true)
-        art.should_not be_published
-        get(:show, :id => art.id)
-        assigns(:article).should_not be_published
-        sleep 3
-        get(:show, :id => art.id)
-        assigns(:article).should be_published
-      end
-
-    end
-
-    describe 'new action' do
-
-      it 'should render new with get' do
-        get :new
-        response.should render_template('new')
-        assert_template_has 'article'
-      end
-
-      def base_article(options={})
-        { :title => "posted via tests!",
-          :body => "A good body",
-          :keywords => "tagged",
-          :allow_comments => '1', 
-          :allow_pings => '1' }.merge(options)
-      end
-
-      it 'should create article with no comments' do
-        post(:new, 'article' => base_article({:allow_comments => '0'}),
-                   'categories' => [categories(:software).id])
-        assigns(:article).should_not be_allow_comments
-        assigns(:article).should be_allow_pings
-        assigns(:article).should be_published
-      end
-
-      it 'should create article with no pings' do
-        post(:new, 'article' => {:allow_pings => '0'},
-                   'categories' => [categories(:software).id])
-        assigns(:article).should be_allow_comments
-        assigns(:article).should_not be_allow_pings
-        assigns(:article).should be_published
-      end
-
-      it 'should create' do
-        begin
-          ActionMailer::Base.perform_deliveries = true
-          ActionMailer::Base.deliveries = []
-          num_articles = Article.count_published_articles
-          emails = ActionMailer::Base.deliveries
-          tags = ['foo', 'bar', 'baz bliz', 'gorp gack gar']
-          post :new, 'article' => base_article(:keywords => tags) , 'categories' => [categories(:software).id]
-          assert_response :redirect, :action => 'show'
-
-          assert_equal num_articles + 1, Article.count_published_articles
-
-          new_article = Article.find(:first, :order => "id DESC")
-          assert_equal users(:tobi), new_article.user
-          assert_equal 1, new_article.categories.size
-          assert_equal [categories(:software)], new_article.categories
-          assert_equal 4, new_article.tags.size
-
-          assert_equal(1, emails.size)
-          assert_equal('randomuser@example.com', emails.first.to[0])
-        ensure
-          ActionMailer::Base.perform_deliveries = false
-        end
-      end
-
-      it 'should create article in future' do
-        assert_no_difference 'Article.count_published_articles' do
-          post(:new,
-               :article =>  base_article(:published_at => Time.now + 1.hour) )
-          assert_response :redirect, :action => 'show'
-          assigns(:article).should_not be_published
-        end
-        assert_equal 1, Trigger.count
-      end
-
-      it 'should create a filtered article' do
-        body = "body via *textile*"
-        extended="*foo*"
-        post :new, 'article' => { :title => "another test", :body => body, :extended => extended}
-        assert_response :redirect, :action => 'show'
-
-        new_article = Article.find(:first, :order => "created_at DESC")
-        assert_equal body, new_article.body
-        assert_equal extended, new_article.extended
-        assert_equal "textile", new_article.text_filter.name
-        assert_equal "<p>body via <strong>textile</strong></p>", new_article.html(:body)
-        assert_equal "<p><strong>foo</strong></p>", new_article.html(:extended)
-      end
-
-    end
+    it_should_behave_like 'index action'
+    it_should_behave_like 'show action'
+    it_should_behave_like 'new action'
+    it_should_behave_like 'destroy action'
 
     describe 'edit action' do
 
@@ -176,24 +213,6 @@ describe Admin::ContentController do
 
     end
 
-    describe 'destroy action' do
-
-      it 'should destroy article' do
-        art_id = contents(:article1).id
-        assert_not_nil Article.find(art_id)
-
-        get :destroy, 'id' => art_id
-        assert_response :success
-
-        post :destroy, 'id' => art_id
-        assert_response :redirect, :action => 'index'
-
-        assert_raise(ActiveRecord::RecordNotFound) {
-          article = Article.find(art_id)
-        }
-      end
-
-    end
 
     describe 'resource_add action' do
 
@@ -246,6 +265,77 @@ describe Admin::ContentController do
         response.should be_success
         response.body.should == '<ul><li>bar</li><li>bazz</li></ul>'
       end
+    end
+
+  end
+
+  describe 'with publisher connection' do
+
+    before :each do
+      @user = users(:user_publisher)
+      @article = contents(:publisher_article)
+      request.session = {:user => @user.id}
+    end
+
+    it_should_behave_like 'index action'
+    it_should_behave_like 'show action'
+    it_should_behave_like 'new action'
+    it_should_behave_like 'destroy action'
+
+    describe 'edit action' do
+
+      it "should redirect if edit article doesn't his" do
+        get :edit, :id => contents(:article1).id
+        response.should redirect_to(:action => 'index')
+      end
+
+      it 'should edit article' do
+        get :edit, 'id' => contents(:publisher_article).id
+        assigns(:selected).should == contents(:publisher_article).categories.collect {|c| c.id}
+        response.should render_template('new')
+        assert_template_has 'article'
+        assigns(:article).should be_valid
+      end
+
+      it 'should update article by edit action' do
+        begin
+          ActionMailer::Base.perform_deliveries = true
+          emails = ActionMailer::Base.deliveries
+          emails.clear
+
+          art_id = contents(:publisher_article).id
+
+          body = "another *textile* test"
+          post :edit, 'id' => art_id, 'article' => {:body => body, :text_filter => 'textile'}
+          response.should redirect_to(:action => 'index')
+
+          article = contents(:publisher_article).reload
+          article.text_filter.name.should == "textile"
+          body.should == article.body
+
+          emails.size.should == 0
+        ensure
+          ActionMailer::Base.perform_deliveries = false
+        end
+      end
+    end
+
+    describe 'destroy action can be access' do
+
+      it 'should redirect when want destroy article' do
+        assert_no_difference 'Article.count' do
+          get :destroy, :id => contents(:article1)
+          response.should redirect_to(:action => 'index')
+        end
+      end
+
+      it 'should redirect when want destroy article' do
+        assert_no_difference 'Article.count' do
+          post :destroy, :id => contents(:article1)
+          response.should redirect_to(:action => 'index')
+        end
+      end
+
     end
 
   end
