7624a34ffd90d119a1ec141bda3c8f816bd427a6
Cyril Mougel <cyril.mougel@gmail.com>
2008-11-17 00:10:29 +0100

    revert commit 0ddae7e929f053f44cf25da8bbfabade19966329 and fix bug #1281 and #1283

--- db/migrate/001_initial_schema.rb
+++ db/migrate/001_initial_schema.rb
@@ -49,8 +49,7 @@ class InitialSchema < ActiveRecord::Migration
         :allow_comments => 1,
         :allow_pings =>1,
         :published => 1,
-        :permalink => 'hello-world',
-        :user_id => 1
+        :permalink => 'hello-world'
       )
 
       create_table :blacklist_patterns do |t|
--- db/migrate/003_add_article_user_id.rb
+++ db/migrate/003_add_article_user_id.rb
@@ -12,6 +12,17 @@ class AddArticleUserId < ActiveRecord::Migration
     modify_tables_and_update(:add_column, BareArticle, :user_id, :integer) do |art|
       art.user_id = (BareUser.find_by_name(art.author).id rescue nil)
     end
+    user_first = BareUser.first
+    if user_first.nil?
+      user_id = 1
+    else
+      user_id = user_first.id
+    end
+
+    BareArticle.find(:all, :conditions => 'user_id IS NULL').each do |art|
+      art.user_id = user_id
+      art.save!
+    end
   end
 
   def self.down
