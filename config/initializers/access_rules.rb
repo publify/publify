93aeab9f365d47f474d3d9c1376507b90a95be1c
Frédéric de Villamil <frederic@de-villamil.com>
2008-11-29 21:09:22 +0100

    Removing textfilters from the admin menu

--- config/initializers/access_rules.rb
+++ config/initializers/access_rules.rb
@@ -46,6 +46,7 @@ AccessControl.map :require => [ :admin, :publisher, :contributor ]  do |map|
   map.permission "admin/base"
   map.permission "admin/cache"
   map.permission "admin/dashboard"
+  map.permission "admin/textfilters"
 
   map.project_module :write, nil do |project|
     project.menu    "Write",            { :controller => "admin/content",    :action => "new" }
@@ -91,7 +92,6 @@ AccessControl.map :require => [ :admin, :publisher, :contributor ]  do |map|
     project.submenu "Read",                  { :controller => "admin/settings", :action => "read" }
     project.submenu "Feedback",              { :controller => "admin/settings", :action => "feedback" }			
     project.submenu "SEO",                   { :controller => "admin/settings", :action => "seo" }
-    project.submenu "Text Filters",          { :controller => "admin/textfilters", :action => "index" }
     project.submenu "Blacklist",             { :controller => "admin/blacklist", :action => "index" }
   end  
   
