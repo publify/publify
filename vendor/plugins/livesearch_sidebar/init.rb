88d143eceec547b41889e1746a37a4d8757df294
Frédéric de Villamil <frederic@de-villamil.com>
2009-01-03 13:11:49 +0100

    Replacing default live search with real search. Adding live search as a plugin instead

---
+++ vendor/plugins/livesearch_sidebar/init.rb
@@ -0,0 +1,4 @@
+require 'sidebar'
+require 'livesearch_sidebar'
+
+LivesearchSidebar.view_root =  File.dirname(__FILE__) + '/views'
---
+++ vendor/plugins/livesearch_sidebar/lib/livesearch_sidebar.rb
@@ -0,0 +1,5 @@
+class LivesearchSidebar < Sidebar
+  description "Adds livesearch to your Typo blog"
+
+  setting :title, 'Search'
+end
