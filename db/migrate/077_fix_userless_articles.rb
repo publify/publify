909e55fad9eb00e12c80968711a949935da50756
Frédéric de Villamil <frederic@de-villamil.com>
2009-01-04 18:03:17 +0100

    Fixing brocken migration if no user yet

--- db/migrate/077_fix_userless_articles.rb
+++ db/migrate/077_fix_userless_articles.rb
@@ -1,14 +1,13 @@
 class FixUserlessArticles < ActiveRecord::Migration
   def self.up
     unless $schema_generator
-      user = User.find(:first)
       articles = Article.find(:all)
       STDERR.puts "Fixing articles with empty user_id"
       
       articles.each do |article|
         if article.user.nil?
           STDERR.puts "Fixing article #{article.id} having empty user"
-          article.user_id = user.id
+          article.user_id = 1
           article.save!
         end
       end
