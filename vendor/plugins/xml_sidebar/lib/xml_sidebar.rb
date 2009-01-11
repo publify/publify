66c07ce18bf4eb5f6fa3bbcf162aed7d321b15ba
Frédéric de Villamil <frederic@de-villamil.com>
2009-01-11 10:35:12 +0100

    Fixing XML sidebar bug

--- vendor/plugins/xml_sidebar/lib/xml_sidebar.rb
+++ vendor/plugins/xml_sidebar/lib/xml_sidebar.rb
@@ -6,8 +6,8 @@ class XmlSidebar < Sidebar
   setting :comments,   true,  :input_type => :checkbox
   setting :trackbacks, false, :input_type => :checkbox
 
-  setting :format, 'atom10', :input_type => :radio,
-          :choices => [["rss20",  "RSS 2.0"], ["atom10", "Atom 1.0"]]
+  setting :format, 'atom', :input_type => :radio,
+          :choices => [["rss",  "RSS"], ["atom", "Atom"]]
 
   def format_strip
     format.gsub(/\d+/,'')
