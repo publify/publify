fa081ae24b463a8dca36da33d07f059cd07c8976
Frédéric de Villamil <frederic@de-villamil.com>
2009-01-19 13:53:58 +0100

    Adding themes helper the ugly way

--- app/helpers/theme_helper.rb
+++ app/helpers/theme_helper.rb
@@ -1,3 +1,5 @@
 module ThemeHelper
-
+  # adds per theme helpers if file exists. Ugly but at least it works.
+  # Use : just add your methods in yourtheme/helpers/theme_helper.rb
+  require "#{Blog.default.current_theme.path}/helpers/theme_helper.rb" if File.exists? "#{Blog.default.current_theme.path}/helpers/theme_helper.rb"
 end
