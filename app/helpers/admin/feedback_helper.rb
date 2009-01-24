5909f697a2ed3f869e0b28f9706ccda1d7dcb189
Frédéric de Villamil <frederic@de-villamil.com>
2009-01-24 20:37:32 +0100

    Adding visual signal on ham / spam comments the same way as we do on posts

--- app/helpers/admin/feedback_helper.rb
+++ app/helpers/admin/feedback_helper.rb
@@ -1,2 +1,9 @@
 module Admin::FeedbackHelper
+  def comment_class state
+    (state.to_s =~ /Ham/) ? 'published' : 'unpublished'
+#    if state.to_s == "Ham?" or state.to_s == "Ham"
+#      return 'published'
+#    end
+#    'unpublished'
+  end
 end
