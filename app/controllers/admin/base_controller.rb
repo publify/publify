a9fd312184b0eaf9aaebb95653ed8f493e07b052
Frédéric de Villamil <frederic@de-villamil.com>
2009-02-28 09:26:42 +0100

    Saving the user's default editor when switching from editor

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -7,6 +7,18 @@ class Admin::BaseController < ApplicationController
   before_filter :login_required, :except => [ :login, :signup ]
   before_filter :look_for_needed_db_updates, :except => [:login, :signup, :update_database, :migrate]
 
+  def create_fck_editor
+    current_user.editor = 2
+    current_user.save!
+    render :partial => "fckeditor"
+  end
+  
+  def create_simple_editor
+    current_user.editor = 0
+    current_user.save!
+    render :partial => "simple_editor"
+  end
+  
   private
   def look_for_needed_db_updates
     if Migrator.offer_migration_when_available
