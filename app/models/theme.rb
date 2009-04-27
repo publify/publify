9e93560dde862b8f38e61c1e5d9a47e251d26292
Frédéric de Villamil <frederic@de-villamil.com>
2009-04-27 17:32:26 +0200

    Solves ticket #41 : allows to define a secondary layout for Pages (ie a sidebar less layout)

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -9,6 +9,11 @@ class Theme
   end
 
   def layout
+    if controller.action_name == 'view_page'
+      if File.exists? "../../themes/#{name}/layouts/pages.html.erb"
+        return "../../themes/#{name}/layouts/pages.html.erb"
+      end
+    end
     "../../themes/#{name}/layouts/default.html.erb"
   end
 
