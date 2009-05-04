33a621acc7f48e53d3873f4ae3711beed6ed0951
Erik Ostrom <erik@echographia.com>
2009-05-04 15:02:53 -0700

    Added a bunch of pending specs.
    Signed-off-by: Matijs van Zuijlen <matijs@matijs.net>

--- vendor/plugins/typo_converter/spec/converters/wp25_spec.rb
+++ vendor/plugins/typo_converter/spec/converters/wp25_spec.rb
@@ -11,4 +11,28 @@ describe "WordPress 2.5 converter" do
   it "runs without crashing" do
     TypoPlugins.convert_from :wp25
   end
+  
+  describe "given a user" do
+    it "creates a user"
+  end
+  
+  describe "given a post" do
+    it "creates an article"
+    
+    describe "with a comment" do
+      it "creates a comment"
+      
+      describe "that is spam" do
+        it "marks the created comment as spam"
+      end
+    end
+    
+    describe "that is a page" do
+      it "creates a page"
+    end
+  end
+  
+  describe "given tags and categories" do
+    it "behaves in a manner to be determined"
+  end
 end
