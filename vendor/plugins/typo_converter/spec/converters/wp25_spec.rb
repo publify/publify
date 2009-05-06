92a616a4f8ffe3fd4f0fd84d56054286077375a3
Erik Ostrom <erik@echographia.com>
2009-05-05 18:01:50 -0700

    Test WP converter for articles. Also, work around a fixtures issue.
    Signed-off-by: Matijs van Zuijlen <matijs@matijs.net>

--- vendor/plugins/typo_converter/spec/converters/wp25_spec.rb
+++ vendor/plugins/typo_converter/spec/converters/wp25_spec.rb
@@ -4,6 +4,12 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "WordPress 2.5 converter" do
   before :each do
+    # Fixtures aren't being rolled back in the WP DB, so we delete them all before
+    # each test. This is a simple hack, but slows things down.
+    [WP25::Option, WP25::User, WP25::Post].each do |model_class|
+      model_class.delete_all
+    end
+    
     Factory('WP25/option',
       :option_name => 'blogname', :option_value => 'My WordPress Blog')
     Factory('WP25/option',
@@ -16,7 +22,7 @@ describe "WordPress 2.5 converter" do
   
   describe "given a user" do
     before :each do
-      Factory('WP25/user',
+      @user = Factory('WP25/user',
         :display_name => 'A. User',
         :user_email => 'auser@notfound.com',
         :user_login => 'auser',
@@ -29,6 +35,20 @@ describe "WordPress 2.5 converter" do
     end
 
     describe "and a post" do
+      before :each do
+        # TODO: figure out if we're handling GMT conversion correctly
+        Factory('WP25/post',
+          :post_title => 'A Post',
+          :post_excerpt => '',
+          :post_content => 'I got something to say!',
+          :post_author => @user.id
+        )
+      end
+      
+      it "creates an article" do
+        lambda { run_converter }.should change(Article, :count).by(1)
+      end
+      
       it "creates an article"
       
       describe "with a comment" do
--- vendor/plugins/typo_converter/spec/factories/wp25_factories.rb
+++ vendor/plugins/typo_converter/spec/factories/wp25_factories.rb
@@ -10,6 +10,22 @@ require 'converters/wp25/user'
 # TODO: factory_girl can guess WP25::Option from 'WP25/option', but it tries
 # to look up the class with const_get, which doesn't work for classes in modules.
 Factory.define 'WP25/option', :class => WP25::Option do |c| end
+
 Factory.define 'WP25/user', :class => WP25::User do |u|
   u.user_registered true
+end
+
+Factory.define 'WP25/post', :class => WP25::Post do |p|
+  now = Time.now
+  p.post_date now
+  # TODO: figure out how to handle GMT conversion
+  p.post_date_gmt {|p| p.post_date }
+  p.post_modified {|p| p.post_date }
+  p.post_modified_gmt {|p| p.post_modified }
+  p.post_title 'Default Title'
+  p.post_content 'This is default content.'
+  p.post_excerpt ''
+  p.to_ping '' # TODO: ?
+  p.pinged '' # TODO: ?
+  p.post_content_filtered {|p| p.post_content + ' [filtered]'} # TODO: ?
 end
