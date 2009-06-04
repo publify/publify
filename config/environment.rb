890c377a7da0cf89f458314a085db8129ef5e831
Frédéric de Villamil <frederic@de-villamil.com>
2009-06-04 21:15:26 +0200

    A bit more cleaning

--- config/environment.rb
+++ config/environment.rb
@@ -61,56 +61,18 @@ Rails::Initializer.run do |config|
   config.gem 'calendar_date_select'
   config.gem 'bluecloth', :version => '~> 2.0.0'
 
-  # Declare needed (github) gems
-  # This is for legacy documentation, these gems are vendored for release.
-  # config.gem 'datanoise-actionwebservice', :version => '2.2.2', :lib => 'actionwebservice', :source => 'http://gems.github.com'
-  # config.gem 'mislav-will_paginate', :version => '2.3.7', :lib => 'will_paginate', :source => 'http://gems.github.com'
-  # Rails 2.2.2 bug : decommenting this line forces the use of the test database regardless of the RAILS_ENV
-  # config.gem 'rspec-rails', :lib => 'spec/rails', :version => '~> 1.1.11'
-
-  # Force all environments to use the same logger level
-  # (by default production uses :info, the others :debug)
-  # config.log_level = :debug
-
   # Use the database for sessions instead of the file system
   # (create the session table with 'rake create_sessions_table')
   config.action_controller.session_store = :active_record_store
 
-  # Enable page/fragment caching by setting a file-based store
-  # (remember to create the caching directory and make it readable to the application)
-  config.action_controller.cache_store = :file_store, "#{RAILS_ROOT}/tmp/cache"
-
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
   config.active_record.observers = :email_notifier, :web_notifier
-
-  # Make Active Record use UTC-base instead of local time
-  # config.active_record.default_timezone = :utc
-
-  # Use Active Record's schema dumper instead of SQL when creating the test database
-  # (enables use of different database adapters for development and test environments)
-  # config.active_record.schema_format = :ruby
-
-  # See Rails::Configuration for more options
 end
 
-# Add new inflection rules using the following format
-# (all these examples are active by default):
-# Inflector.inflections do |inflect|
-#   inflect.plural /^(ox)$/i, '\1en'
-#   inflect.singular /^(ox)en/i, '\1'
-#   inflect.irregular 'person', 'people'
-#   inflect.uncountable %w( fish sheep )
-# end
-
-
-# Include your application configuration below
-
 # Load included libraries.
-require 'redcloth'
-require 'bluecloth'
 require 'rubypants'
-require 'flickr'
+#require 'flickr'
 require 'uuidtools'
 
 require 'migrator'
@@ -119,39 +81,7 @@ require 'login_system'
 require 'typo_version'
 $KCODE = 'u'
 require 'jcode'
-require 'xmlrpc_fix'
 require 'transforms'
-require 'builder'
-
-
-#MemoryProfiler.start(:delay => 10, :string_debug => false)
-
-unless Builder::XmlMarkup.methods.include? '_attr_value'
-  # Builder 2.0 has many important fixes, but for now we will only backport
-  # this one...
-  class Builder::XmlMarkup
-    # Insert the attributes (given in the hash).
-    def _insert_attributes(attrs, order=[])
-      return if attrs.nil?
-      order.each do |k|
-        v = attrs[k]
-        @target << %{ #{k}="#{_attr_value(v)}"} if v # " WART
-      end
-      attrs.each do |k, v|
-        @target << %{ #{k}="#{_attr_value(v)}"} unless order.member?(k) # " WART
-      end
-    end
-
-   def _attr_value(value)
-      case value
-      when Symbol
-        value.to_s
-      else
-        _escape(value.to_s).gsub(%r{"}, '&quot;')  # " WART
-      end
-    end
-  end
-end
 
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
   :long_weekday => '%a %B %e, %Y %H:%M'
