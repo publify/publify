f5891be8efc30251e00989f1f1d61414898e949d
Michael Reinsch <michael@mobalean.com>
2009-06-24 15:09:38 +0900

    enable author based RSS/atom feeds as in typo 5.2

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,5 +3,18 @@ class AuthorsController < ContentController
   
   def show
     @author = User.find_by_login(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.rss do
+        @limit = this_blog.limit_rss_display
+        auto_discovery_feed(:only_path => false)
+        render :partial => "articles/rss20_feed", :object => @author.articles
+      end
+      format.atom do
+        @limit = this_blog.limit_rss_display
+        render :partial => "articles/atom_feed", :object => @author.articles
+      end
+    end
   end
 end
