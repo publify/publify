2de1f8dbc09bc87643ff3eb5b70f756c98b03e5b
Michael Reinsch <michael@mobalean.com>
2009-06-24 14:39:04 +0900

    avoid double colon

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -2,7 +2,7 @@ module AuthorsHelper
 
   def display_profile_item(item, show_item, item_desc)
     content_tag :li do
-      "#{item_desc}: #{item}"
+      "#{item_desc} #{item}"
     end if show_item
   end
 end
