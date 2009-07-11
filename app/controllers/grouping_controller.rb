f02efb77c97dd9a5e7e8809662cb570a5eb18ae4
Frédéric de Villamil <frederic@de-villamil.com>
2009-07-11 22:39:16 +0200

    Better title for grouping pages. Closes ticket #100

--- app/controllers/grouping_controller.rb
+++ app/controllers/grouping_controller.rb
@@ -35,7 +35,23 @@ class GroupingController < ContentController
   def show
     set_noindex
     grouping = grouping_class.find_by_permalink(params[:id])
-    @page_title = "#{_(self.class.to_s.sub(/Controller$/,'').singularize)} #{grouping.name}, #{_('everything about')} #{grouping.name}"
+
+    @page_title = "#{_(self.class.to_s.sub(/Controller$/,'').singularize)} #{grouping.name}, "
+
+    if grouping.respond_to? :description and
+        not grouping.description.nil?
+      @page_title += grouping.description
+    else
+      @page_title += "#{_('everything about')} "
+
+      if grouping.respond_to? :display_name and
+          not grouping.display_name.nil?
+        @page_title += grouping.display_name
+      else
+        @page_title += grouping.name
+      end
+    end
+
     @page_title << " page " << params[:page] if params[:page]
     @description = (grouping.description.blank?) ? "" : grouping.description
     @keywords = (grouping.keywords.blank?) ? "" : grouping.keywords
