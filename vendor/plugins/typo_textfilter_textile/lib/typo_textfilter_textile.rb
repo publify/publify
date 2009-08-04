a182789316bdf6cdd434b44cb9f45563ec44ef8e
Cyril Mougel <cyril.mougel@gmail.com>
2009-08-05 00:36:31 +0200

    need require RedCloth to be use textile :)

--- vendor/plugins/typo_textfilter_textile/lib/typo_textfilter_textile.rb
+++ vendor/plugins/typo_textfilter_textile/lib/typo_textfilter_textile.rb
@@ -11,7 +11,11 @@ See [_why's Textile reference](http://hobix.com/textile/).
       end
   
       def self.filtertext(blog,content,text,params)
+        require 'RedCloth'
         RedCloth.new(text).to_html(:textile)
+      rescue LoadError => e
+        Rails.logger.warn 'You have not RedCloth install. Please install it gem install RedCloth'
+        text
       end
     end
   end
