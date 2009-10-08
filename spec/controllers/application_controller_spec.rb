655686d82ac5e134f28dfbb5d3a7f7bdb25165a8
Matijs van Zuijlen <matijs@matijs.net>
2009-10-08 12:38:24 +0200

    Make sure cached file does not exist before and after spec.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,11 +3,16 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe ApplicationController do
   it 'safely caches a page' do
     define_spec_public_cache_directory
+    file_path = path_for_file_in_spec_public_cache_directory('/test.html')
+    File.delete file_path if File.exists? file_path
+    File.should_not be_exist(file_path)
+
     ApplicationController.perform_caching = true
     ApplicationController.cache_page 'test', '/test'
-    file_path = path_for_file_in_spec_public_cache_directory('/test.html')
     File.should be_exist(file_path)
+
     ApplicationController.perform_caching = false
+    File.delete(file_path)
   end
 end
 
