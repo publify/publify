c798a4ccca2bf79f911b03ef743ee564150209ed
Matijs van Zuijlen <matijs@matijs.net>
2009-10-09 20:00:43 +0200

    Add spec for current behavior of profiles_controller

--- spec/controllers/admin/profiles_controller_spec.rb
+++ spec/controllers/admin/profiles_controller_spec.rb
@@ -1,8 +1,22 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe Admin::ProfilesController do
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  describe "#index" do
+    it 'should render index' do
+      users(:tobi).profile.label.should == 'admin'
+      request.session = { :user => users(:tobi).id }
+      get :index
+      response.should render_template('index')
+    end
+  end
+
+  # TODO: Make RESTful
+  describe "successful POST to index" do
+    it "redirects to the admin page" do
+      request.session = { :user => users(:tobi).id }
+      post :index, :user => {:email => 'foo@bar.com'}
+      response.should redirect_to(admin_url)
+    end
   end
 end
