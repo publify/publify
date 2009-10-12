67794b957bfc31f3a9a916f9477058ea0856c1d6
Matijs van Zuijlen <matijs@matijs.net>
2009-10-12 13:25:09 +0200

    Clean up spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,9 +6,6 @@ require 'spec/autorun'
 require 'spec/rails'
 
 Spec::Runner.configure do |config|
-  # If you're not using ActiveRecord you should remove these
-  # lines, delete config/database.yml and disable :active_record
-  # in your config/boot.rb
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
   config.fixture_path = RAILS_ROOT + '/test/fixtures/'
@@ -20,6 +17,8 @@ Spec::Runner.configure do |config|
   config.before(:each) do
     Localization.lang = :default
   end
+end
+
 def define_spec_public_cache_directory
   ActionController::Base.page_cache_directory = File.join(Rails.root, 'spec', 'public')
   unless File.exist? ActionController::Base.page_cache_directory
@@ -36,39 +35,6 @@ def create_file_in_spec_public_cache_directory(file)
   file_path = path_for_file_in_spec_public_cache_directory(file)
   File.open(file_path, 'a').close
   file_path
-end
-
-  # == Fixtures
-  #
-  # You can declare fixtures for each example_group like this:
-  #   describe "...." do
-  #     fixtures :table_a, :table_b
-  #
-  # Alternatively, if you prefer to declare them only once, you can
-  # do so right here. Just uncomment the next line and replace the fixture
-  # names with your fixtures.
-  #
-  # config.global_fixtures = :table_a, :table_b
-  #
-  # If you declare global fixtures, be aware that they will be declared
-  # for all of your examples, even those that don't use them.
-  #
-  # You can also declare which fixtures to use (for example fixtures for test/fixtures):
-  #
-  # config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
-  #
-  # == Mock Framework
-  #
-  # RSpec uses it's own mocking framework by default. If you prefer to
-  # use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  #
-  # == Notes
-  # 
-  # For more information take a look at Spec::Runner::Configuration and Spec::Runner
 end
 
 # TODO: Rewrite to be more RSpec-like instead of Test::Unit-like.
