1efca32ec6de96458acf1907f34ba5bd78a62636
Cyril Mougel <cyril.mougel@gmail.com>
2009-10-16 18:11:54 +0200

    fix problem in spec because override %d to {{count}}

--- lang/en_US.rb
+++ lang/en_US.rb
@@ -1,3 +1,5 @@
 Localization.define('en_US') do |l|
   l.store "Login successful", "Login successful"
+  # usefull to avoid space
+  l.store "You need a Format of permalink with an identifier : %%month%%, %%year%%, %% day%%, %%title%%", "You need a Format of permalink with an identifier : %%month%%, %%year%%, %%day%%, %%title%%"
 end
