83a86eaf1107439ed5512c485c4a6c5689df1faa
Matijs van Zuijlen <matijs@matijs.net>
2009-10-16 17:36:09 +0200

    Clean up PreviewsController spec and make it pass.

--- spec/controllers/previews_controller_spec.rb
+++ spec/controllers/previews_controller_spec.rb
@@ -1,32 +1,21 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-class Content
-  def self.find_last_posted
-    find(:first, :conditions => ['created_at < ?', Time.now],
-         :order => 'created_at DESC')
-  end
-end
-
-describe 'PreviewsController' do
+describe PreviewsController do
   controller_name :previews
-  Article.delete_all
 
   integrate_views
 
   before(:each) do
-    IPSocket.stub!(:getaddress).and_return do
-      raise SocketError.new("getaddrinfo: Name or service not known")
-    end
-    controller.send(:reset_blog_ids)
+    request.session = { :user => users(:tobi).id }
   end
 
   describe 'index action' do
     before :each do
-      get :index, :id => 1
+      get :index, :id => contents(:article1).id
     end
 
-    it 'should be render template /articles/read' do
-      response.should render_template('/articles/read')
+    it 'should render template /articles/read' do
+      response.should render_template('articles/read.html.erb')
     end
   end
 end
