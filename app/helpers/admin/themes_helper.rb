bc22c808b22eb3adb406c7b60af3bf0d70a1dfd2
Frédéric de Villamil <frederic@de-villamil.com>
2009-12-21 21:27:31 +0100

    Fixes theme editor not being styled. Closes #130

--- app/helpers/admin/themes_helper.rb
+++ app/helpers/admin/themes_helper.rb
@@ -1,24 +1,22 @@
 module Admin::ThemesHelper
   require('find')
   def fetch_stylesheets
-    list = '<ul>'
+    list = ''
     Find.find(this_blog.current_theme.path + "/stylesheets") do |path|
       if path =~ /css$/ 
-        list << content_tag(:li, link_to(File.basename(path), {:controller => 'themes', :action => 'editor', :type => 'stylesheet', :file => File.basename(path)}))
+        list << content_tag(:p, link_to(File.basename(path), {:controller => 'themes', :action => 'editor', :type => 'stylesheet', :file => File.basename(path)}))
       end
     end
-    list << '</ul>'
     list
   end
 
   def fetch_layouts
-    list = '<ul>'
+    list = ''
     Find.find(this_blog.current_theme.path + "/layouts") do |path|
       if path =~ /rhtml$|erb$/ 
-        list << content_tag(:li, link_to(File.basename(path), {:controller => 'themes', :action => 'editor', :type => 'layout', :file => File.basename(path)}))
+        list << content_tag(:p, link_to(File.basename(path), {:controller => 'themes', :action => 'editor', :type => 'layout', :file => File.basename(path)}))
       end
     end
-    list << '</ul>'
     list
   end
   
