b2785fb2fffcc690aaeffc423bd6b68682035bf7
Cyril Mougel <cyril.mougel@gmail.com>
2009-12-21 18:45:22 +0100

    test true-blue-3 themes in article views spec

--- spec/views/articles/index_spec.rb
+++ spec/views/articles/index_spec.rb
@@ -1,8 +1,11 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 # test standard view and all themes
-[ nil, "dirtylicious", "scribbish", "standard_issue", "typographic" ].each do |theme|
-  view_path = theme ? "#{RAILS_ROOT}/themes/#{theme}/views" : "" 
+[ nil, "true-blue-3", "dirtylicious", "scribbish", "standard_issue", "typographic" ].each do |theme|
+  view_path = theme ? "#{RAILS_ROOT}/themes/#{theme}/views" : ""
+  if File.exists?("#{RAILS_ROOT}/themes/#{theme}/helpers/theme_helper.rb")
+    require "#{RAILS_ROOT}/themes/#{theme}/helpers/theme_helper.rb"
+  end
   describe "#{view_path}/articles/index" do
     before(:each) do
       @controller.view_paths = [ "#{RAILS_ROOT}/themes/#{theme}/views" ] if theme
@@ -15,18 +18,18 @@ require File.dirname(__FILE__) + '/../../spec_helper'
         alias :tag_links :article_links
       end
     end
-  
+
     context "normally" do
       before(:each) do
         @controller.action_name = "index"
         assigns[:articles] = Article.paginate(:all, :page => 2, :per_page => 4)
         render "articles/index"
       end
-    
+
       it "should not have pagination link to page 2 without q param" do
         response.should_not have_tag("a[href=?]", "/page/2")
       end
-      
+
       it "should have pagination link to page 1 without q param if on page 2" do
         response.should have_tag("a[href=?]", "/page/1")
       end
@@ -36,7 +39,7 @@ require File.dirname(__FILE__) + '/../../spec_helper'
         response.should_not have_tag("p>p", "body")
       end
     end
-    
+
     # *notice
     # this assumptions has "&amp;", i don`t know why, but we want only to test the q= param in link and have separated this test from controller
     context "when search" do
@@ -48,14 +51,14 @@ require File.dirname(__FILE__) + '/../../spec_helper'
         assigns[:articles] = Blog.default.articles_matching(params[:q], :page => 2, :per_page => 4)
         render "articles/index"
       end
-    
+
       it "should not have pagination link to page 2 with q param" do
         response.should_not have_tag("a[href=?]", "/search/body?page=2") # *notice
       end
-      
+
       it "should have pagination link to page 1 with q param if on page 2" do
         response.should have_tag("a[href=?]", "/search/body?page=1") # *notice
-      end    
+      end
     end
   end
 end
