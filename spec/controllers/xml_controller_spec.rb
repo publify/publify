853c5811291cb0f875b88654806ce5bad0d1d936
Matijs van Zuijlen <matijs@matijs.net>
2009-12-27 14:49:08 +0100

    Use symbol for response code.

--- spec/controllers/xml_controller_spec.rb
+++ spec/controllers/xml_controller_spec.rb
@@ -149,7 +149,7 @@ describe XmlController do
 
   it "test_bad_format" do
     get :feed, :format => 'atom04', :type => 'feed'
-    assert_response 404
+    assert_response :missing
   end
 
   it "test_bad_type" do
