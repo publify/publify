7731547c5ece952423c17488070e19e7bcba3abb
Frédéric de Villamil <frederic@de-villamil.com>
2010-03-30 12:46:01 +0200

    Fixes 2 404 on lightbox images

--- public/javascripts/lightbox.js
+++ public/javascripts/lightbox.js
@@ -46,8 +46,8 @@
 //  Configurationl
 //
 LightboxOptions = Object.extend({
-    fileLoadingImage:        'images/loading.gif',     
-    fileBottomNavCloseImage: 'images/closelabel.gif',
+    fileLoadingImage:        '/images/loading.gif',     
+    fileBottomNavCloseImage: '/images/closelabel.gif',
 
     overlayOpacity: 0.8,   // controls transparency of shadow overlay
 
