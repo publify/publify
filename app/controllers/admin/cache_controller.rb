0c2c310ba198274e04ff1a0ca95602817ac70dca
Frédéric de Villamil <frederic@de-villamil.com>
2010-04-15 21:12:31 +0200

    Removing dead code

--- app/controllers/admin/cache_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::CacheController < Admin::BaseController
-
-  def sweep_html
-    sweep_cache
-    flash[:notice] = _('HTML was cleared')
-    redirect_to :controller => '/admin/settings'
-  end
-
-end
