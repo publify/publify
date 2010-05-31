2e6cb4492d55f47ea0582f52f56371a7d2261a15
Diego Elio 'Flameeyes' Pettenò <flameeyes@gmail.com>
2010-05-31 17:43:29 +0200

    Don't sweep the cache if we're saving a draft.
    Without this change, having the article editor open to write a new article
    will cause the cache to be continuously sweeped without reason.

--- app/models/blog_sweeper.rb
+++ app/models/blog_sweeper.rb
@@ -33,7 +33,7 @@ class BlogSweeper < ActionController::Caching::Sweeper
   end
 
   def after_save(record)
-    expire_for(record)
+    expire_for(record) unless (record.is_a?(Article) and record.state.downcase == 'draft')
   end
 
   def after_destroy(record)
