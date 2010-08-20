6cac5312482e789e32479532389a8f42fa168154
Matijs van Zuijlen <matijs@matijs.net>
2010-08-20 20:39:44 +0200

    Set routing behavior back to Rails 2 style.

--- config/initializers/new_rails_defaults.rb
+++ config/initializers/new_rails_defaults.rb
@@ -11,7 +11,7 @@ if defined?(ActiveRecord)
   ActiveRecord::Base.store_full_sti_class = true
 end
 
-ActionController::Routing.generate_best_match = false
+#ActionController::Routing.generate_best_match = false
 
 # Use ISO 8601 format for JSON serialized times and dates.
 ActiveSupport.use_standard_json_time_format = true
