8aedc77de7ac068557c6096ad7b10e5c65f34918
Matijs van Zuijlen <matijs@matijs.net>
2010-09-10 16:21:23 +0200

    Spec out SetupController.
    Part of the ongoing quest to increase spec coverage.

--- spec/controllers/setup_controller_spec.rb
+++ spec/controllers/setup_controller_spec.rb
@@ -1,24 +1,63 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe 'GET setup with no configured blog' do
-  controller_name :setup
+describe SetupController do
+  describe 'when no blog is configured' do
+    before do
+      Blog.delete_all
+      User.delete_all
+      Blog.new.save
+    end
 
-  before(:each) do
-    Blog.delete_all
-    @blog = Blog.new.save
+    describe 'GET setup' do
+      before do
+	get 'index'
+      end
+
+      specify { response.should render_template('index') }
+    end
+
+    describe 'POST setup' do
+      before do
+	post 'index', {:setting => {:blog_name => 'Foo', :email => 'foo@bar.net'}}
+      end
+
+      specify { response.should redirect_to(:action => 'confirm') }
+
+      it "should correctly initialize blog and users" do
+	this_blog.blog_name.should == 'Foo'
+	admin = User.find_by_login("admin")
+	admin.should_not be_nil
+	admin.email.should == 'foo@bar.net'
+	Article.find(:first).user.should == admin
+      end
+
+      it "should log in admin user" do
+	session[:user_id].should == User.find_by_login("admin").id
+      end
+    end
   end
 
-  it 'renders index' do
-    get 'index'
-    response.should render_template('index')
-  end
-end
+  describe 'when a blog is configured and has some users' do
+    describe 'GET setup' do
+      before do
+	get 'index'
+      end
 
-describe 'GET setup with a configured blog and some users' do
-  controller_name :setup
+      specify { response.should redirect_to(:controller => 'articles', :action => 'index') }
+    end
 
-  it 'redirects to login' do
-    get 'index'
-    response.should redirect_to(:controller => 'articles', :action => 'index')
+    describe 'POST setup' do
+      before do
+	post 'index', {:setting => {:blog_name => 'Foo', :email => 'foo@bar.net'}}
+      end
+
+      specify { response.should redirect_to(:controller => 'articles', :action => 'index') }
+
+      it "should not initialize blog and users" do
+	this_blog.blog_name.should_not == 'Foo'
+	admin = User.find_by_login("admin")
+	admin.should be_nil
+      end
+    end
   end
 end
