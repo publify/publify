3255b94a3a77e8d84d9deeeb376e0091baba916a
Matijs van Zuijlen <matijs@matijs.net>
2010-09-11 09:32:44 +0200

    Remove incorrect comment.

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,5 +1,3 @@
-# The filters added to this controller will be run for all controllers in the application.
-# Likewise will all the methods added be available for all controllers.
 class ContentController < ApplicationController
   class ExpiryFilter
     def before(controller)
