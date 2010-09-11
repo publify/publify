b7a2183d82166018371702dd8f12dedc3f7945c4
Matijs van Zuijlen <matijs@matijs.net>
2010-09-11 16:54:32 +0200

    Remove cache_template_loading option.
    It does not exist anymore.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,7 +13,6 @@ TypoBlog::Application.configure do
   # Show full error reports and disable caching
   config.action_controller.consider_all_requests_local = true
   config.action_controller.perform_caching             = false
-  config.action_view.cache_template_loading            = false
 
   # Disable request forgery protection in test environment
   config.action_controller.allow_forgery_protection    = false
