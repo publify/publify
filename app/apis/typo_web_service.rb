2e2dcb024ac20b5cd29d47a9c8f1c3a5b9c133ab
Matijs van Zuijlen <matijs@matijs.net>
2010-09-11 10:53:05 +0200

    Remove useless compatibility code.
    No need to take 0.6.2 and older versions of AWS into account.

--- app/apis/typo_web_service.rb
+++ app/apis/typo_web_service.rb
@@ -14,13 +14,7 @@ class TypoWebService < ActionWebService::Base
   def authenticate(name, args)
     method = self.class.web_service_api.api_methods[name]
 
-    # Coping with backwards incompatibility change in AWS releases post 0.6.2
-    begin
-      h = method.expects_to_hash(args)
-      raise "Invalid login" unless @user=User.authenticate(h[:username], h[:password])
-    rescue NoMethodError
-      username, password = method[:expects].index(:username=>String), method[:expects].index(:password=>String)
-      raise "Invalid login" unless @user = User.authenticate(args[username], args[password])
-    end
+    h = method.expects_to_hash(args)
+    raise "Invalid login" unless @user=User.authenticate(h[:username], h[:password])
   end
 end
