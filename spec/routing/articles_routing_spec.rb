d1ecdc53832674d1e42c89c832a36c1b3f4faf80
Matijs van Zuijlen <matijs@matijs.net>
2010-09-18 10:30:40 +0200

    Update routing specs to the new syntax.

--- spec/routing/articles_routing_spec.rb
+++ spec/routing/articles_routing_spec.rb
@@ -1,31 +1,17 @@
 require 'spec_helper'
 
 describe ArticlesController do
-  describe "route generation" do
-    it "should map #index" do
-      route_for(:controller => "articles", :action => "index").should == "/"
+  describe "routing" do
+    it "recognizes and generates #index" do
+      { :get => "/" }.should route_to(:controller => "articles", :action => "index")
     end
 
-    it "should map #index with rss format" do
-      route_for(:controller => "articles", :action => "index", :format => "rss").should == "/articles.rss"
+    it "recognizes and generates #index with rss format" do
+      { :get, "/articles.rss" }.should route_to(:controller => "articles", :action => "index", :format => "rss")
     end
 
-    it "should map #index with atom format" do
-      route_for(:controller => "articles", :action => "index", :format => "atom").should == "/articles.atom"
-    end
-  end
-
-  describe "route recognition" do
-    it "should generate params for #index" do
-      params_from(:get, "/").should == {:controller => "articles", :action => "index"}
-    end
-
-    it "should generate params for #index with rss format" do
-      params_from(:get, "/articles.rss").should == {:controller => "articles", :action => "index", :format => "rss"}
-    end
-
-    it "should generate params for #index with atom format" do
-      params_from(:get, "/articles.atom").should == {:controller => "articles", :action => "index", :format => "atom"}
+    it "recognizes and generates #index with atom format" do
+      { :get, "/articles.atom" }.should route_to(:controller => "articles", :action => "index", :format => "atom")
     end
   end
 end
--- spec/routing/xml_routing_spec.rb
+++ spec/routing/xml_routing_spec.rb
@@ -1,71 +1,37 @@
 require 'spec_helper'
 
 describe XmlController do
-  describe "route generation" do
-    it "should map #articlerss" do
-      route_for(:controller => "xml", :action => "articlerss", :id => "1").should == "/xml/articlerss/1/feed.xml"
+  describe "routing" do
+    it "recognizes and generates #articlerss" do
+      { :get, "/xml/articlerss/1/feed.xml"}.should route_to(:controller => "xml", :action => "articlerss", :id => "1")
     end
 
-    it "should map #commentrss" do
-      route_for(:controller => "xml", :action => "commentrss").should == "/xml/commentrss/feed.xml"
+    it "recognizes and generates #commentrss" do
+      { :get, "/xml/commentrss/feed.xml"}.should route_to(:controller => "xml", :action => "commentrss")
     end
 
-    it "should map #trackbackrss" do
-      route_for(:controller => "xml", :action => "trackbackrss").should == "/xml/trackbackrss/feed.xml"
+    it "recognizes and generates #trackbackrss" do
+      { :get, "/xml/trackbackrss/feed.xml"}.should route_to(:controller => "xml", :action => "trackbackrss")
     end
 
-    it "should map #feed" do
-      route_for(:controller => "xml", :action => "feed", :type => "feed", :format => "atom").should == "/xml/atom/feed.xml"
+    it "recognizes and generates #feed" do
+      { :get, "/xml/atom/feed.xml"}.should route_to(:controller => "xml", :action => "feed", :type => "feed", :format => "atom")
     end
 
-    it "should map #feed with a custom type" do
-      route_for(:controller => "xml", :action => "feed", :format => "atom", :type => "comments").should == "/xml/atom/comments/feed.xml"
+    it "recognizes and generates #feed with a custom type" do
+      { :get, "/xml/atom/comments/feed.xml"}.should route_to(:controller => "xml", :action => "feed", :format => "atom", :type => "comments")
     end
 
-    it "should map #feed with a custom type and an id" do
-      route_for(:controller => "xml", :action => "feed", :format => "atom", :type => "comments", :id => "1").should == "/xml/atom/comments/1/feed.xml"
+    it "recognizes and generates #feed with a custom type and an id" do
+      { :get, "/xml/atom/comments/1/feed.xml"}.should route_to(:controller => "xml", :action => "feed", :format => "atom", :type => "comments", :id => "1")
     end
 
-    it "should map #feed with rss type" do
-      route_for(:controller => "xml", :action => "feed", :type => "feed", :format => "rss").should == "/xml/rss"
+    it "recognizes and generates #feed with rss type" do
+      { :get, "/xml/rss"}.should route_to(:controller => "xml", :action => "feed", :type => "feed", :format => "rss")
     end
 
-    it "should map #feed with sitemap type" do
-      route_for(:controller => "xml", :action => "feed", :type => "sitemap", :format => "googlesitemap").should == "/sitemap.xml"
-    end
-  end
-
-  describe "route recognition" do
-    it "should generate params for #articlerss" do
-      params_from(:get, "/xml/articlerss/1/feed.xml").should == {:controller => "xml", :action => "articlerss", :id => "1"}
-    end
-
-    it "should generate params for #commentrss" do
-      params_from(:get, "/xml/commentrss/feed.xml").should == {:controller => "xml", :action => "commentrss"}
-    end
-
-    it "should generate params for #trackbackrss" do
-      params_from(:get, "/xml/trackbackrss/feed.xml").should == {:controller => "xml", :action => "trackbackrss"}
-    end
-
-    it "should generate params for #feed" do
-      params_from(:get, "/xml/atom/feed.xml").should == {:controller => "xml", :action => "feed", :type => "feed", :format => "atom"}
-    end
-
-    it "should generate params for #feed with a custom type" do
-      params_from(:get, "/xml/atom/comments/feed.xml").should == {:controller => "xml", :action => "feed", :format => "atom", :type => "comments"}
-    end
-
-    it "should generate params for #feed with a custom type and an id" do
-      params_from(:get, "/xml/atom/comments/1/feed.xml").should == {:controller => "xml", :action => "feed", :format => "atom", :type => "comments", :id => "1"}
-    end
-
-    it "should generate params for #feed with rss type" do
-      params_from(:get, "/xml/rss").should == {:controller => "xml", :action => "feed", :type => "feed", :format => "rss"}
-    end
-
-    it "should generate params for #feed with sitemap type" do
-      params_from(:get, "/sitemap.xml").should == {:controller => "xml", :action => "feed", :type => "sitemap", :format => "googlesitemap"}
+    it "recognizes and generates #feed with sitemap type" do
+      { :get, "/sitemap.xml"}.should route_to(:controller => "xml", :action => "feed", :type => "sitemap", :format => "googlesitemap")
     end
   end
 end
