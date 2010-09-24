807999492991547501d64695d1035624e32801e5
Matijs van Zuijlen <matijs@matijs.net>
2010-09-24 16:43:52 +0200

    Make sure feed discovery url includes host name.

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -25,8 +25,8 @@ class ContentController < ApplicationController
   # TODO: Make this work for all content.
   def auto_discovery_feed(options = { })
     with_options(options.reverse_merge(:only_path => true)) do |opts|
-      @auto_discovery_url_rss = opts.url_for(:format => 'rss')
-      @auto_discovery_url_atom = opts.url_for(:format => 'atom')
+      @auto_discovery_url_rss = opts.url_for(:format => 'rss', :only_path => false)
+      @auto_discovery_url_atom = opts.url_for(:format => 'atom', :only_path => false)
     end
   end
 
