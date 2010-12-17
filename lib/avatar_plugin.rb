bce2f8a9ab6e0160e0075d675b5d1931dad3e389
Thomas Lecavelier <thomas@lecavelier.name>
2010-12-17 13:44:17 +0100

    Plugins system for Typo. Pluginization of Avatar feature (feedbacks), addition of typo_avatar_gravatar plugin

---
+++ lib/avatar_plugin.rb
@@ -0,0 +1,19 @@
+require 'typo_plugins'
+
+module TypoPlugins
+
+  class AvatarPlugin < Base
+
+    class << self
+      def kind
+        :avatar
+      end
+
+      def get_avatar(options = {})
+        raise NotImplementedError
+      end
+    end # << self
+
+  end
+
+end
---
+++ vendor/plugins/typo_avatar_gravatar/init.rb
@@ -0,0 +1,4 @@
+require 'avatar_plugin'
+require 'typo_avatar_gravatar'
+
+TypoPlugins::Keeper.register(TypoPlugins::Gravatar)
