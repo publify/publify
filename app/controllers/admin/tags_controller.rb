0612dc258005bdf76e666d0532a8d0da1c978c19
Matijs van Zuijlen <matijs@matijs.net>
2011-01-17 22:09:34 +0100

    Remove unused ordering code.

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -4,16 +4,6 @@ class Admin::TagsController < Admin::BaseController
   cache_sweeper :blog_sweeper
 
   def index
-    if params[:order] and params[:order] =~ /\A(?:name|display_name|article_counter)\Z/
-      if params[:sense] and params[:sense] == 'desc'
-        order = params[:order] + " asc"
-      else
-        order = params[:order] + " desc"
-      end
-    else
-      order = 'display_name ASC'
-    end
-
     @tags = Tag.paginate(:page => params[:page], :order => :display_name, :per_page => this_blog.admin_display_elements)
   end
 
