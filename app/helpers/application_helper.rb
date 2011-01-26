5790fd99c5dd67ee287336f8488f0c5b265a05a1
Frédéric de Villamil <frederic@de-villamil.com>
2011-01-26 07:59:53 +0100

    Reverts back some js_distance_of_time_in_words_to_now thing to make sure it won't break when using static caching system.
    I originally wanted to move this as a full plugin, but it appears we can't do it a 'clean' way yet, so this code is back in the core AND still allows users to chose the way they want date and time to be displayed on their blog.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -212,6 +212,12 @@ module ApplicationHelper
     end
   end
   
+  def new_js_distance_of_time_in_words_to_now(date)
+    time = _(date.utc.strftime(_("%%a, %%d %%b %%Y %%H:%%M:%%S GMT", date.utc)))
+    timestamp = date.utc.to_i ;
+    "<span class=\"typo_date date gmttimestamp-#{timestamp}\" title=\"#{time}\" >#{time}</span>"
+  end
+  
   def display_date(date)
     date.strftime(this_blog.date_format)
   end
@@ -221,7 +227,7 @@ module ApplicationHelper
   end  
   
   def display_date_and_time(timestamp)
-    return "#{distance_of_time_in_words Time.now, timestamp} #{_('ago')}" if this_blog.date_format == 'distance_of_time_in_words'
+    return new_js_distance_of_time_in_words_to_now(timestamp) if this_blog.date_format == 'distance_of_time_in_words'
     "#{display_date(timestamp)} #{_('at')} #{display_time(timestamp)}"
   end
   
