01b444a5e972f1bdf150f89e2554025facf1560c
Frederic de Villamil <frederic@t37.net>
2011-04-07 22:52:33 +0200

    Loading typo_* plugins custom routes first as a way to have enhanced plugins without adding routes to the original rouging file.
    When on Rails 2.x, I was using
    
    class << ActionController::Routing::Routes;self;end.class_eval do
     define_method :clear,s lambda {}
    end
    
    ActionController::Routing::Routes.draw do |map|
     map.connect '/gallery/:action', :controller => 'typo_gallery'
    end
    
    in the plugin init, but it seems not to work anymore, so this is the best way I found to do so.
    
    Example of a working plugin: https://github.com/fdv/typo-gallery

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
+  # Load plugin routes first. A little bit ugly, but I didn't find any better way to do it
+  # We consider that only typo_* plugins are concerned
+  Dir.glob(File.join("vendor", "plugins", "typo_*")).each do |dir|
+    if File.exists?(File.join(dir, "config", "routes.rb"))
+      require File.join(dir, "config", "routes.rb")
+    end
+  end
 
   # for CK Editor
   map.connect 'fm/filemanager/:action/:id', :controller => 'Fm::Filemanager'
