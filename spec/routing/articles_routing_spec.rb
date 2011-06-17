7b07127f07f507d77d25dfadc08d630f84204819
Matijs van Zuijlen <matijs@matijs.net>
2011-06-17 09:59:17 +0200

    Adjust article routing specs to match new glob handling.
    Before, *from would match any path and return an array of elements. The
    new router just returns the matched path.
    
    Also, assert_routing was replaced with should route_to.

--- spec/routing/articles_routing_spec.rb
+++ spec/routing/articles_routing_spec.rb
@@ -16,25 +16,31 @@ describe ArticlesController do
   end
 
   describe "routing for #redirect action" do
-    it 'should split the path into its components' do
-      assert_routing "foo/bar/baz", {
-        :from => ["foo", "bar", "baz"],
-        :controller => 'articles', :action => 'redirect'}
+    it 'picks up any previously undefined path' do
+      get("/foobar").should route_to(:controller => 'articles',
+                                     :action => 'redirect',
+                                     :from => 'foobar')
+    end
+
+    it 'matches paths with multiple components' do
+      { :get => "foo/bar/baz" }.should route_to(:controller => 'articles',
+                                                :action => 'redirect',
+                                                :from => "foo/bar/baz")
     end
 
     it 'should route URLs under /articles' do
-      assert_routing "articles", {
-        :from => ["articles"],
-        :controller => 'articles', :action => 'redirect'}
-      assert_routing "articles/foo", {
-        :from => ["articles", "foo"],
-        :controller => 'articles', :action => 'redirect'}
-      assert_routing "articles/foo/bar", {
-        :from => ["articles", "foo", "bar"],
-        :controller => 'articles', :action => 'redirect'}
-      assert_routing "articles/foo/bar/baz", {
-        :from => ["articles", "foo", "bar", "baz"],
-        :controller => 'articles', :action => 'redirect'}
+      { :get => "/articles" }.should route_to(:controller => 'articles',
+                                              :action => 'redirect',
+                                              :from => "articles")
+      { :get => "/articles/foo" }.should route_to(:controller => 'articles',
+                                                  :action => 'redirect',
+                                                  :from => "articles/foo")
+      { :get => "/articles/foo/bar" }.should route_to(:controller => 'articles',
+                                                      :action => 'redirect',
+                                                      :from => "articles/foo/bar")
+      { :get => "/articles/foo/bar/baz" }.should route_to(:controller => 'articles',
+                                                          :action => 'redirect',
+                                                          :from => "articles/foo/bar/baz")
     end
   end
 end
