064719238e8b1ff486461ea9991f82939012b37f
Matijs van Zuijlen <matijs@matijs.net>
2011-07-08 16:56:26 +0200

    Remove dependency on present model code in migration 096.

--- db/migrate/096_add_origin_to_redirects.rb
+++ db/migrate/096_add_origin_to_redirects.rb
@@ -1,9 +1,29 @@
 class AddOriginToRedirects < ActiveRecord::Migration
+  class Redirect < ActiveRecord::Base
+    has_many :redirections
+    has_many :contents, :through => :redirections
+  end
+
+  class Redirection < ActiveRecord::Base
+    belongs_to :content
+    belongs_to :redirect
+  end
+
+  class Content < ActiveRecord::Base
+    has_many :redirections
+    has_many :redirects, :through => :redirections
+    scope :already_published, {
+      :conditions => ['published = ? AND published_at < ?', true, Time.now] }
+
+    # Avoid STI errors
+    set_inheritance_column :bogustype
+  end
+
   def self.up
     add_column :redirects, :origin, :string
 
     say "Adding origin to redirects made by URL shortener"
-    Content.find_already_published.each do |art|
+    Content.already_published.each do |art|
 
       # Begin / rescue statement is mandatory here because I have something
       # fishy in my database coming from a very old Wordpress import
