6e32ab4be7571ddc4422db03bf235e5ea643def5
Matijs van Zuijlen <matijs@matijs.net>
2011-07-08 10:50:03 +0200

    Fix rendering of image carousel in content (article) form.

--- spec/views/admin/content/new_spec.rb
+++ spec/views/admin/content/new_spec.rb
@@ -1,15 +1,28 @@
 require 'spec_helper'
 
 describe "admin/content/new.html.erb" do
-  it "renders" do
+  before do
     admin = stub_model(User, :editor => "simple", :admin? => true,
                        :text_filter_name => "", :profile_label => "admin")
+    blog = mock_model(Blog, :base_url => "http://myblog.net/")
+
     view.stub(:current_user) { admin }
+    view.stub(:this_blog) { blog }
 
     text_filter = "markdown smartypants".to_text_filter
     assign :article,
       stub_model(Article, :text_filter => text_filter).as_new_record
+  end
+
+  it "renders with no resources or macros" do
     assign(:images, [])
+    assign(:macros, [])
+    assign(:resources, [])
+    render
+  end
+
+  it "renders with image resources" do
+    assign(:images, [stub_model(Resource)])
     assign(:macros, [])
     assign(:resources, [])
     render
