4f1336ebf34ea14e1c834b0994a9250168a6e9bf
Matijs van Zuijlen <matijs@matijs.net>
2011-07-08 17:07:25 +0200

    Remove unused method in migration 024.

--- db/migrate/024_cleanup_contents.rb
+++ db/migrate/024_cleanup_contents.rb
@@ -16,15 +16,6 @@ class Bare24Article < ActiveRecord::Base
   has_and_belongs_to_many :tags,
       :class_name => 'Bare24Tag', :foreign_key => 'article_id',
       :join_table => 'articles_tags', :association_foreign_key => 'tag_id'
-
-  def keywords_to_tags
-    Bare24Article.transaction do
-      tags.clear
-      keywords.to_s.split.uniq.each do |tagword|
-        tags << Bare24Tag.get(tagword)
-      end
-    end
-  end
 end
 
 class Bare24Tag < ActiveRecord::Base
@@ -42,7 +33,6 @@ class CleanupContents < ActiveRecord::Migration
     Bare24Article.transaction do
       Bare24Article.find(:all, :conditions => "type = 'Article'").each do |a|
         a.create_guid
-#       a.keywords_to_tags      (not needed?)
         a.save!
       end
     end
