5e9250442d483a3720f292b39c43c8960b6b70a9
Matijs van Zuijlen <matijs@matijs.net>
2011-07-08 09:29:53 +0200

    Don't stub out subtabs_for anymore in spec.

--- spec/views/admin/content/new_spec.rb
+++ spec/views/admin/content/new_spec.rb
@@ -2,10 +2,9 @@ require 'spec_helper'
 
 describe "admin/content/new.html.erb" do
   it "renders" do
-    user = double(:user, :text_filter_name => "",
-                  :editor => "simple", :admin? => true)
-    view.stub(:current_user) { user }
-    view.stub(:subtabs_for) {""}
+    admin = stub_model(User, :editor => "simple", :admin? => true,
+                       :text_filter_name => "", :profile_label => "admin")
+    view.stub(:current_user) { admin }
 
     text_filter = "markdown smartypants".to_text_filter
     assign(:article, stub_model(Article, :text_filter => text_filter))
