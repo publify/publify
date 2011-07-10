015000e01e05f8daad481a9f6458b156dde24087
Matijs van Zuijlen <matijs@matijs.net>
2011-07-10 23:45:52 +0200

    Add (passing) specs for TrackbacksController#index for feeds.

---
+++ spec/controllers/trackbacks_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe TrackbacksController do
+  before do
+    blog = stub_model(Blog, :base_url => "http://myblog.net")
+    Blog.stub(:default) { blog }
+    Trigger.stub(:fire) { }
+  end
+
+  describe "#index" do
+    before do
+      Trackback.stub(:find) { [ "some", "items" ] }
+    end
+
+    describe "with :format => atom" do
+      before do
+        get :index, :format => :atom
+      end
+
+      it "returns an atom feed" do
+        response.should be_success
+        response.should render_template("articles/_atom_feed")
+      end
+    end
+
+    describe "with :format => rss" do
+      before do
+        get :index, :format => :rss
+      end
+
+      it "returns an rss feed" do
+        response.should be_success
+        response.should render_template("articles/_rss20_feed")
+      end
+    end
+  end
+end
+
