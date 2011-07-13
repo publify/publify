9c5f70a9cd4930c8b2b6ee505b6b2f07bdbeaf21
Yannick François <yannick.francois@free.fr>
2011-07-13 17:01:40 +0200

    improve test perf

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -3,18 +3,13 @@ require 'spec_helper'
 describe CategoriesController, "/index" do
   render_views
 
-  before do
-    Factory(:blog)
-    3.times {
-      category = Factory(:category)
-      2.times { category.articles << Factory(:article) }
-    }
-  end
-
   describe "normally" do
     before do
+      Factory(:blog)
       cat = Factory.build(:category) 
-      Category.stub!(:find_by_permalink).with('Social').and_return(cat)
+      cat_p = [cat]
+      cat_p.stub!(:total_pages).and_return(1)
+      Category.should_receive(:paginate).and_return(cat_p)
       get 'index'
     end
 
@@ -25,7 +20,13 @@ describe CategoriesController, "/index" do
   end
 
   describe "if :index template exists" do
+    
     it "should render :index" do
+      Factory(:blog)
+      3.times {
+        category = Factory(:category)
+        2.times { category.articles << Factory(:article) }
+      }
       pending "Stubbing #template_exists is not enough to fool Rails"
       controller.stub!(:template_exists?) \
         .and_return(true)
@@ -53,6 +54,12 @@ describe CategoriesController, '/articles/category/personal' do
   end
 
   it 'should be successful' do
+    c = Factory.build(:category, :permalink => 'personal')
+    articles = [Factory.build(:article, :categories => [c])]
+    controller.should_receive(:show_page_title_for)
+    controller.should_receive(:permalink_with_page)
+    Category.should_receive(:find_by_permalink).with('personal').and_return(c)
+    c.should_receive(:published_articles).and_return(articles)
     do_get
     response.should be_success
   end
@@ -99,6 +106,13 @@ describe CategoriesController, '/articles/category/personal' do
   end
 
   it 'should have a canonical URL' do
+    c = Factory.build(:category, :permalink => 'personal')
+    art = Factory.build(:article, :categories => [c])
+    controller.should_receive(:show_page_title_for)
+    controller.should_receive(:permalink_with_page).and_return('http://myblog.net/category/personal/')
+    Category.should_receive(:find_by_permalink).with('personal').and_return(c)
+    c.should_receive(:published_articles).and_return([art])
+   
     do_get
     response.should have_selector('head>link[href="http://myblog.net/category/personal/"]')
   end
@@ -166,40 +180,42 @@ describe CategoriesController, "SEO Options" do
     get 'show', :id => 'personal'
     response.should_not have_selector('head>meta[content="noindex, follow"]')
   end
-    
-  it 'category with keywords and activated option should have meta keywords' do
-    @blog.use_meta_keyword = true
-    @blog.save
-    @cat.keywords = "some, keywords"
-    @cat.save
-    
-    get 'show', :id => 'personal'
-    response.should have_selector('head>meta[name="keywords"]')
-  end
   
-  it 'category without meta keywords and activated should not have meta keywords' do
-    @blog.use_meta_keyword = true
-    @blog.save
+  describe "with meta_keyword usage" do
+    before do
+      @blog.use_meta_keyword = true
+      @blog.save
+    end
+
+    it 'category with keywords and activated option should have meta keywords' do
+      cat = Factory.build(:category, :permalink => 'personal', :keywords => "some, keywords")
+      art = Factory.build(:article, :categories => [cat])
+      Category.should_receive(:find_by_permalink).with('personal').and_return(cat)
+      cat.should_receive(:published_articles).and_return([art])
     
-    get 'show', :id => 'personal'
-    response.should_not have_selector('head>meta[name="keywords"]')
-  end
+      get 'show', :id => 'personal'
+      response.should have_selector('head>meta[name="keywords"]')
+    end
+ 
+    it 'category without meta keywords and activated should not have meta keywords' do
+      get 'show', :id => 'personal'
+      response.should_not have_selector('head>meta[name="keywords"]')
+    end
     
-  it 'category without meta keywords and activated options should not have meta keywords' do
-    @blog.use_meta_keyword = true
-    @blog.save
-    
-    get 'show', :id => 'personal'
-    response.should_not have_selector('head>meta[name="keywords"]')
+    it 'category without meta keywords and activated options should not have meta keywords' do
+      get 'show', :id => 'personal'
+      response.should_not have_selector('head>meta[name="keywords"]')
+    end
+
   end
 
-  it 'category with meta keywords and deactivated options should not have meta keywords' do
-    @blog.use_meta_keyword = false
-    @blog.save
-    @cat.keywords = "some, keywords"
-    @cat.save
+    it 'category with meta keywords and deactivated options should not have meta keywords' do
+      @blog.use_meta_keyword = false
+      @blog.save
+      @cat.keywords = "some, keywords"
+      @cat.save
     
-    get 'show', :id => 'personal'
-    response.should_not have_selector('head>meta[name="keywords"]')
-  end
+      get 'show', :id => 'personal'
+      response.should_not have_selector('head>meta[name="keywords"]')
+    end
 end
