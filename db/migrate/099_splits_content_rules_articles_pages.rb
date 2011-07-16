a8b4cb53a8e3a817847d15c6e682c9aab05abc81
Frédéric de Villamil <neuro@asocial.org>
2011-07-16 22:25:27 +0200

    Forgot to add migration to add new rules for articles and pages.

---
+++ db/migrate/099_splits_content_rules_articles_pages.rb
@@ -0,0 +1,19 @@
+class SplitsContentRulesArticlesPages < ActiveRecord::Migration
+  class Profile < ActiveRecord::Base
+    include BareMigration
+
+    serialize :profiles
+  end
+
+  def self.up
+    say "Splits content modules into pages and articles"
+    Profile.find_by_label("admin").update_attributes(:modules => [:dashboard, :articles, :pages, :media, :feedback, :themes, :sidebar, :users, :settings, :profile, :seo])
+    Profile.find_by_label("publisher").update_attributes(:modules => [:dashboard, :articles, :media, :pages, :feedback, :profile])
+  end
+
+  def self.down
+    say "Merges pages and articles to content"
+    Profile.find_by_label("admin").update_attributes(:modules => [:dashboard, :write, :content, :feedback, :themes, :sidebar, :users, :settings, :profile, :seo ])
+    Profile.find_by_label("publisher").update_attributes(:modules => [:dashboard, :write, :content, :feedback, :profile ])
+  end
+end
