5c3fecb8f0789bacd0019b7274cc8912481ef41a
Matijs van Zuijlen <matijs@matijs.net>
2011-07-16 20:38:49 +0200

    Use stub_full_article helper in articles atom feed view spec.

--- spec/views/articles/index_atom_feed_spec.rb
+++ spec/views/articles/index_atom_feed_spec.rb
@@ -5,21 +5,6 @@ describe "articles/index_atom_feed.atom.builder" do
     stub_default_blog
   end
 
-  let(:author) { stub_model(User, :name => "not empty") }
-
-  let(:text_filter) { stub_model(TextFilter) }
-
-  def base_article(time=Time.now)
-    a = stub_model(Article, :published_at => time, :user => author,
-                   :created_at => time, :updated_at => time,
-                   :title => "not empty either", :permalink => 'foo-bar')
-    a.stub(:tags) { [] }
-    a.stub(:categories) { [] }
-    a.stub(:resources) { [] }
-    a.stub(:text_filter) { text_filter }
-    a
-  end
-
   describe "with no items" do
     before do
       assign(:articles, [])
@@ -36,9 +21,9 @@ describe "articles/index_atom_feed.atom.builder" do
 
   describe "rendering articles" do
     it 'should create valid atom feed when articles contains funny bits' do
-      article1 = base_article(1.minute.ago)
+      article1 = stub_full_article(1.minute.ago)
       article1.body = '&eacute;coute!'
-      article2 = base_article(2.minutes.ago)
+      article2 = stub_full_article(2.minutes.ago)
       article2.body = 'is 4 < 2? no!'
       assign(:articles, [article1, article2])
 
