61d81a2d90d49b39e443d0ec5e8199d47013aa1b
Frédéric de Villamil <neuro@asocial.org>
2011-07-23 16:08:42 +0200

    Adding permalinks SEO tab.
    Fixes a bug where SEO params would never be saved.

--- app/controllers/admin/seo_controller.rb
+++ app/controllers/admin/seo_controller.rb
@@ -11,7 +11,39 @@ class Admin::SeoController < Admin::BaseController
       file.each do |line|
         @setting.robots << line
       end
+    else
+      build_robots
     end
+  end
+
+  def permalinks 
+    load_settings
+    if @setting.permalink_format != '/%year%/%month%/%day%/%title%' and
+      @setting.permalink_format != '/%year%/%month%/%title%' and
+      @setting.permalink_format != '/%title%'
+      @setting.custom_permalink = @setting.permalink_format
+      @setting.permalink_format = 'custom'
+    end
+  end
+    
+
+  def update
+    if request.post?
+      if params[:setting]['permalink_format'] and params[:setting]['permalink_format'] == 'custom'
+        params[:setting]['permalink_format'] = params[:setting]['custom_permalink']
+      end
+      Blog.transaction do
+        params[:setting].each { |k,v| this_blog.send("#{k.to_s}=", v) }
+        this_blog.save
+        flash[:notice] = _('config updated.')
+      end
+
+      save_robots unless params[:setting][:robots].blank?
+
+      redirect_to :action => params[:from]
+    end
+  rescue ActiveRecord::RecordInvalid
+    render params[:from]
   end
 
   private
@@ -25,6 +57,14 @@ class Admin::SeoController < Admin::BaseController
       robots.write(params[:setting][:robots])
       robots.close
     end
+  end
+  
+  def build_robots
+    robots = File.new("#{::Rails.root.to_s}/public/robots.txt", "w+")
+    line = "User-agent: *\nAllow: /\nDisallow: /admin\n"
+    robots.write(line)
+    robots.close
+    @setting.robots = line
   end
 
 end
