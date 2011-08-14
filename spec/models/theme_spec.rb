717dd9d888b3928154d55d7b4f690ebad5ab0100
Yannick Francois <yannick.francois@free.fr>
2011-08-14 13:17:25 +0200

    remove unused files amd directory in test dir

--- spec/models/theme_spec.rb
+++ spec/models/theme_spec.rb
@@ -1,12 +1,9 @@
 require 'spec_helper'
 
 describe 'Given a new test theme' do
-  before(:each) do
-    @theme = Theme.new("test", "test")
-  end
-
   it 'layout path should be "#{::Rails.root.to_s}/themes/test/layouts/default.html.erb"'  do
-    @theme.layout('index').should == "#{::Rails.root.to_s}/themes/test/layouts/default.html.erb"
+    theme = Theme.new("test", "test")
+    theme.layout('index').should == "#{::Rails.root.to_s}/themes/test/layouts/default.html.erb"
   end
 end
 
@@ -30,14 +27,18 @@ describe 'Given the default theme' do
     Theme.theme_from_path(::Rails.root.to_s + 'themes/scribbish').name.should == 'scribbish'
   end
 
-  it '#search_theme_path finds the right things' do
-    Theme.should_receive(:themes_root).and_return(::Rails.root.to_s + '/test/mocks/themes')
-    Theme.search_theme_directory.collect{|t| File.basename(t)}.sort.should ==
-      %w{ 123-numbers-in-path CamelCaseDirectory i-have-special-chars typographic }
+  it '#search_theme_path finds the right things 2' do
+    fake_blue_theme_dir = 'fake_blue_theme_dir'
+    fake_red_theme_dir = 'fake_red_theme_dir'
+    fake_bad_theme_dir = 'fake_bad_theme_dir'
+    Dir.should_receive(:glob).and_return([fake_blue_theme_dir, fake_bad_theme_dir, fake_red_theme_dir])
+    File.should_receive(:readable?).with(fake_blue_theme_dir + "/about.markdown").and_return(true)
+    File.should_receive(:readable?).with(fake_bad_theme_dir + "/about.markdown").and_return(false)
+    File.should_receive(:readable?).with(fake_red_theme_dir + "/about.markdown").and_return(true)
+    Theme.search_theme_directory.should == %w{ fake_blue_theme_dir fake_red_theme_dir }
   end
 
   it 'find_all finds all the installed themes' do
-    Theme.find_all.collect{|t| t.name}.should include(@theme.name)
     Theme.find_all.size.should ==
       Dir.glob(::Rails.root.to_s + '/themes/[a-zA-Z0-9]*').select do |file|
         File.readable? "#{file}/about.markdown"
