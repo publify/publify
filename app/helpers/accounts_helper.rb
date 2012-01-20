33ba179b26f54c1b9f1905764f329f9669df792a
Frédéric de Villamil <frederic@de-villamil.com>
2012-01-20 08:15:25 +0100

    Make the login form slightly better. Sometimes it's just a matter of connceting the dots.

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,8 +1,9 @@
+# coding: utf-8
 module AccountsHelper
   def display_accounts_link
     html = ""
     html << link_to("<small>#{_("Create an account")}</small>".html_safe, :action => 'signup') if this_blog.allow_signup == 1
-    html << link_to("<small>#{_("I've lost my password")}</small>".html_safe, :action => 'recover_password')
+    html << link_to("<small> • #{_("I've lost my password")}</small>".html_safe, :action => 'recover_password')
     html.html_safe
   end
 
