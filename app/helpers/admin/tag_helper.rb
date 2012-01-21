8d409b1abc4e27f136f03d8e117295cfaf30b62b
Frédéric de Villamil <frederic@de-villamil.com>
2012-01-21 13:51:00 +0100

    Tags to bootstrap

--- app/helpers/admin/tag_helper.rb
+++ app/helpers/admin/tag_helper.rb
@@ -2,8 +2,8 @@ module Admin::TagHelper
   def show_tag_actions item
     html = <<-HTML
       <div class='action'>
-        <small>#{link_to _("Edit"), :action => 'edit', :id => item.id}</small> |
         <small>#{link_to_permalink item, _("Show")}</small> |
+        <small>#{link_to _("Edit"), :action => 'edit', :id => item.id}</small> |
         <small>#{link_to _("Delete"), :action => 'destroy', :id => item.id}</small>
     </div>
     HTML
