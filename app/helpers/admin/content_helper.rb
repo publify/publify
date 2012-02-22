476837135b9cb9da63351b5e9bdf4beb022bef03
Frédéric de Villamil <frederic@de-villamil.com>
2012-02-22 08:25:17 +0100

    Fixes post types display when there is no post type at all

--- app/helpers/admin/content_helper.rb
+++ app/helpers/admin/content_helper.rb
@@ -43,7 +43,8 @@ module Admin::ContentHelper
       return hidden_field_tag "article[post_type]", "read"
     end
     
-    html = content_tag(:h4, _("Article type"))
+    html = "<div class='well'>"
+    html << content_tag(:h4, _("Article type"))
     html << "<select name=article[post_type]>"
         
     post_types.each do |pt|
@@ -52,5 +53,6 @@ module Admin::ContentHelper
     end
     
     html << "</select>"
+    html << "</div>"
   end  
 end
