7cf7345d53ee6b28409d9d20a6fac40ccc974c39
James Eady <jmeady@gmail.com>
2012-06-19 22:13:15 -0700

    Adding cucumber starter features

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -15,6 +15,8 @@ module NavigationHelpers
 
     when /^the home\s?page$/
       '/'
+    when /^the new article page$/
+      '/admin/content/new'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
