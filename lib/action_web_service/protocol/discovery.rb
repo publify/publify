6c8442493930e9ba39db9f00c28a599a7312959c
Matijs van Zuijlen <matijs@matijs.net>
2012-07-06 13:05:51 +0200

    Make web_service_protocols an class attribute

--- lib/action_web_service/protocol/discovery.rb
+++ lib/action_web_service/protocol/discovery.rb
@@ -4,18 +4,20 @@ module ActionWebService # :nodoc:
       def self.included(base)
         base.extend(ClassMethods)
         base.send(:include, ActionWebService::Protocol::Discovery::InstanceMethods)
+        base.send(:class_attribute, :web_service_protocols)
+        base.send(:web_service_protocols=, [])
       end
 
       module ClassMethods # :nodoc:
         def register_protocol(klass)
-          write_inheritable_array("web_service_protocols", [klass])
+          self.web_service_protocols += [klass]
         end
       end
 
       module InstanceMethods # :nodoc:
         private
           def discover_web_service_request(action_pack_request)
-            (self.class.read_inheritable_attribute("web_service_protocols") || []).each do |protocol|
+            (self.class.web_service_protocols || []).each do |protocol|
               protocol = protocol.create(self)
               request = protocol.decode_action_pack_request(action_pack_request)
               return request unless request.nil?
