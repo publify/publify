c95ea13f25df9916ac7a6e3fb0c5f2f3bfe9f3f7
James Eady <jmeady@gmail.com>
2012-07-22 15:57:52 -0700

    Merging latest version of typo

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -25,12 +25,7 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new_or_edit
     @categories = Category.find(:all)
-    @category = case params[:id]
-                when nil
-                  Category.new
-                else
-                  Category.find(params[:id])
-                end
+    @category = Category.find(params[:id])
     @category.attributes = params[:category]
     if request.post?
       respond_to do |format|
