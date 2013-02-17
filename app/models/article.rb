f28c8b672081f5ffe2db94cb9180f562310beb05
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2013-02-17 10:56:06 -0800

    comments: use relative URLs for comment forms.
    Using full, absoltue URLs will cause testing environment to comment on
    production instances, and will also break HTTPS support on
    mixed-protocols sites.

--- app/models/article.rb
+++ app/models/article.rb
@@ -144,11 +144,11 @@ class Article < Content
   end
 
   def comment_url
-    blog.url_for("comments?article_id=#{self.id}", :only_path => false)
+    blog.url_for("comments?article_id=#{self.id}", :only_path => true)
   end
 
   def preview_comment_url
-    blog.url_for("comments/preview?article_id=#{self.id}", :only_path => false)
+    blog.url_for("comments/preview?article_id=#{self.id}", :only_path => true)
   end
 
   def feed_url(format)
