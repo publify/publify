946bc911613439a0d29b5d62337852732d74a1c9
Kyle Fazzari <typo_development@status.e4ward.com>
2013-04-27 13:28:52 -0400

    Updated search sidebar to echo form_tag. Also changed the text input from specifying a hard-coded size to specifying a pixel width parameter, configurable from the sidebar customization page.

--- vendor/plugins/search_sidebar/lib/search_sidebar.rb
+++ vendor/plugins/search_sidebar/lib/search_sidebar.rb
@@ -3,4 +3,5 @@ class SearchSidebar < Sidebar
   description "Adds basic search sidebar in your Typo blog"
 
   setting :title, 'Search'
+  setting :field_width, 210, :label => 'Field Width (px)'
 end
