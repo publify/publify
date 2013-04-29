f9e2c391868f25c973f7dd52fe4f2b37c58b46ba
Kyle Fazzari <typo_development@status.e4ward.com>
2013-04-28 20:05:13 -0400

    Removed pixel width parameter for search plugin; it should be styled via CSS.

--- vendor/plugins/search_sidebar/lib/search_sidebar.rb
+++ vendor/plugins/search_sidebar/lib/search_sidebar.rb
@@ -3,5 +3,4 @@ class SearchSidebar < Sidebar
   description "Adds basic search sidebar in your Typo blog"
 
   setting :title, 'Search'
-  setting :field_width, 210, :label => 'Field Width (px)'
 end
