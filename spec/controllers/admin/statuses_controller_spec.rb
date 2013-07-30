4119f11499d954ecc6a2d91cba96347880d29f21
Yannick Francois <yannick.francois@free.fr>
2013-07-30 23:39:46 +0200

    Fix spec
    Fail on postgresql because of bad test case

--- spec/controllers/admin/statuses_controller_spec.rb
+++ spec/controllers/admin/statuses_controller_spec.rb
@@ -11,14 +11,14 @@ describe Admin::StatusesController do
       henri = create(:user, :login => 'henri', :profile => create(:profile_admin, :label => Profile::ADMIN))
       request.session = { :user => henri.id }
     end
-    
+
     it 'should redirect to new' do
       get 'index'
       response.should redirect_to(:action => 'new')
     end
-  
+
   end
-  
+
   describe "For a new status" do
     before :each do
       create(:blog)
@@ -27,13 +27,13 @@ describe Admin::StatusesController do
       henri = create(:user, :login => 'henri', :profile => create(:profile_admin, :label => Profile::ADMIN))
       request.session = { :user => henri.id }
     end
-    
+
     it 'should render template new' do
       get 'new'
       response.should render_template('new')
     end
   end
-  
+
   describe "Editing an existing status" do
     before :each do
       create(:blog)
@@ -43,48 +43,37 @@ describe Admin::StatusesController do
       request.session = { :user => henri.id }
       @status = FactoryGirl.create(:status)
     end
-    
+
     it 'should render template edit' do
       get 'edit', :id => @status.id
       response.should render_template('new')
     end
   end
-  
-  describe "Creating a new status" do
-    before :each do
-      create(:blog)
-      #TODO Delete after removing fixtures
-      Profile.delete_all
-      Status.delete_all
-      henri = create(:user, :login => 'henri', :profile => create(:profile_admin, :label => Profile::ADMIN))
-      request.session = { :user => henri.id }
-      @status = FactoryGirl.create(:status)
-    end
-    
-    it 'creates a status' do
-      post :new, :status => { :body => "Emphasis _mine_, arguments *strong*" }
-      
-      Status.count.should == 2
-      status = Status.find(:last)
-      status.body.should == "Emphasis _mine_, arguments *strong*"
 
-      response.should redirect_to(:controller => 'statuses', :action => 'new')
-      flash[:notice].should ==  "Status was successfully created."
-    end
+  describe :new do
+    describe "Creating a new status" do
+      let!(:blog) { create(:blog) }
+      let(:henri) { create(:user, :login => 'henri', :profile => create(:profile_admin, :label => Profile::ADMIN)) }
 
-    it 'creates a status with a chosen permalink' do
-      post :new, :status => { :body => "Emphasis _mine_, arguments *strong*", :permalink => 'my-cool-permalink' }
-      
-      Status.count.should == 2
-      status = Status.find(:last)
-      status.body.should == "Emphasis _mine_, arguments *strong*"
-      status.permalink.should == "my-cool-permalink"
+      before(:each) { request.session = { :user => henri.id } }
 
-      response.should redirect_to(:controller => 'statuses', :action => 'new')
-      flash[:notice].should ==  "Status was successfully created."
+      context "without permalink" do
+        before(:each) { post :new, status: { body: "Emphasis _mine_, arguments *strong*" } }
+
+        it {expect(response).to redirect_to(controller: 'statuses', action: 'new')}
+        it {expect(Status.count).to eq(1) }
+        it {expect(Status.first.body).to eq("Emphasis _mine_, arguments *strong*") }
+        it {expect(flash[:notice]).to eq("Status was successfully created.") }
+      end
+
+      context "with permalink" do
+        before(:each) { post :new, status: { body: "Emphasis _mine_, arguments *strong*", permalink: 'my-cool-permalink'} }
+
+        it { expect(Status.last.permalink).to eq("my-cool-permalink") }
+      end
     end
   end
-    
+
   describe "Destorying a status" do
     before :each do
       create(:blog)
@@ -95,19 +84,19 @@ describe Admin::StatusesController do
       Status.delete_all
       @status = FactoryGirl.create(:status, :user_id => henri.id)
     end
-    
+
     it 'should render template destroy' do
       get 'destroy', :id => @status.id
       response.should render_template('destroy')
       Status.count.should == 1
     end
-    
+
     it 'should destroy the last existing status and return zero' do
       post 'destroy', :id => @status.id
       response.should redirect_to(:controller => 'admin/statuses', :action => 'index')
       Status.count.should == 0
     end
-    
+
   end
 
 end
