513d48287109d4579930c4b7b8d399869729f8d1
Yannick Francois <yannick.francois@free.fr>
2013-08-12 17:32:22 +0200

    Create a robot to manage Robots.txt file

--- app/controllers/admin/seo_controller.rb
+++ app/controllers/admin/seo_controller.rb
@@ -1,76 +1,49 @@
 class Admin::SeoController < Admin::BaseController
   cache_sweeper :blog_sweeper
+  before_filter :set_setting, only: [:index, :titles]
 
   def index
-    load_settings
-    if File.exists? "#{::Rails.root.to_s}/public/robots.txt"
-      @setting.robots = ""
-      file = File.readlines("#{::Rails.root.to_s}/public/robots.txt")
-      file.each do |line|
-        @setting.robots << line
-      end
-    else
-      build_robots
-    end
+    @setting.robots = Robot.new.rules
   end
 
   def permalinks
     if request.post?
-      if params[:setting]['permalink_format'] and params[:setting]['permalink_format'] == 'custom'
-        params[:setting]['permalink_format'] = params[:setting]['custom_permalink']
+      update_settings
+    else
+      set_setting
+      if @setting.permalink_format != '/%year%/%month%/%day%/%title%' and
+        @setting.permalink_format != '/%year%/%month%/%title%' and
+        @setting.permalink_format != '/%title%'
+        @setting.custom_permalink = @setting.permalink_format
+        @setting.permalink_format = 'custom'
       end
-      update
-      return
     end
-
-    load_settings
-    if @setting.permalink_format != '/%year%/%month%/%day%/%title%' and
-      @setting.permalink_format != '/%year%/%month%/%title%' and
-      @setting.permalink_format != '/%title%'
-      @setting.custom_permalink = @setting.permalink_format
-      @setting.permalink_format = 'custom'
-    end
-  end
-
-  def titles
-    load_settings
   end
 
   def update
-    if request.post?
-      Blog.transaction do
-        params[:setting].each { |k,v| this_blog.send("#{k.to_s}=", v) }
-        this_blog.save
-        flash[:notice] = _('config updated.')
-      end
-
-      save_robots unless params[:setting][:robots].blank?
-
-      redirect_to :action => params[:from]
-    end
+    update_settings if request.post?
   rescue ActiveRecord::RecordInvalid
     render params[:from]
   end
 
   private
-  def load_settings
+
+  def update_settings
+    if params[:setting]['permalink_format'] and params[:setting]['permalink_format'] == 'custom'
+      params[:setting]['permalink_format'] = params[:setting]['custom_permalink']
+    end
+    Blog.transaction do
+      params[:setting].each { |k,v| this_blog.send("#{k.to_s}=", v) }
+      this_blog.save
+      flash[:notice] = _('config updated.')
+    end
+    if params[:setting][:robots].present?
+      Robot.new.add(params[:setting][:robots])
+    end
+    redirect_to action: params[:from]
+  end
+
+  def set_setting
     @setting = this_blog
   end
-
-  def save_robots
-    if File.writable? "#{::Rails.root.to_s}/public/robots.txt"
-      robots = File.new("#{::Rails.root.to_s}/public/robots.txt", "r+")
-      robots.write(params[:setting][:robots])
-      robots.close
-    end
-  end
-
-  def build_robots
-    robots = File.new("#{::Rails.root.to_s}/public/robots.txt", "w+")
-    line = "User-agent: *\nAllow: /\nDisallow: /admin\n"
-    robots.write(line)
-    robots.close
-    @setting.robots = line
-  end
-
 end
