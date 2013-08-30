d3e74c14149db8a6977a7cae8f624d383c3f2880
Frédéric de Villamil <frederic@de-villamil.com>
2013-08-30 18:18:13 +0200

    Makes the visual editor more usable

--- public/javascripts/ckcustom.js
+++ public/javascripts/ckcustom.js
@@ -13,6 +13,7 @@ CKEDITOR.editorConfig = function( config )
 	config.entities_greek = false;
 	config.entities_latin = false;
 	config.entities_processNumerical = false;
+  config.height = 360;
 	config.toolbar_Basic =
 	[
 	        ['Format', '-', 'Bold', 'Italic'],
