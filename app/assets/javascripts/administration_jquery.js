06fbd98b9cc107903455fe7979e940bb09c80e54
Thomas Lecavelier <tl@pierlis.com>
2013-10-07 14:02:30 +0200

    Replace calendar_date_picker slutty plugin for Jquery datepicker. Not as fun, but UJS

--- app/assets/javascripts/administration_jquery.js
+++ app/assets/javascripts/administration_jquery.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+  // Spinners
   $('form.spinnable').on('ajax:before', function(evt, xhr, status){ $('#spinner').show();})
   $('form.spinnable').on('ajax:after', function(evt, xhr, status){ $('#spinner').hide();})
+
+  // DatePickers
+  $('.datepicker').datepicker();
 });
