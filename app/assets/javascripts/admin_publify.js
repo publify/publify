9f43e59f8a43c621a0c4d474248d41f3fadcd9ed
Frédéric de Villamil <frederic@de-villamil.com>
2013-12-30 22:49:33 +0100

    Ensures placeholder are loaded on new contents.
    Issue #307

--- app/assets/javascripts/admin_publify.js
+++ app/assets/javascripts/admin_publify.js
@@ -28,8 +28,14 @@ $(document).ready(function() {
 });
 
 function load_article_form() {
-  $('#article-title-placeholder').text($('#article_title').val());
-  $('#article-body-placeholder').text($('#article_body_and_extended').text());
+  if ($('#article_title').val() != "") {
+    $('#article-title-placeholder').text($('#article_title').val());
+  }
+  
+  if ($('#article_body_and_extended').text() != "") {
+    $('#article-body-placeholder').text($('#article_body_and_extended').text());
+  }
+  
   $('#article_title').hide();
   $('#article_body_and_extended').hide();  
 }
@@ -40,8 +46,14 @@ function submit_article_form() {
 }
 
 function load_page_form() {
-  $('#page-title-placeholder').text($('#page_title').val());
-  $('#page-body-placeholder').text($('#page_body').text());
+  if ($('#page_title').val() != "") {
+    $('#page-title-placeholder').text($('#page_title').val());
+  }
+  
+  if ($('#page_body').text() != "") {
+    $('#page-body-placeholder').text($('#page_body').text());    
+  }
+  
   $('#page_title').hide();
   $('#page_body').hide();  
 }
