446593fb6c5065f53215a660a648440713aa39db
Frédéric de Villamil <frederic@de-villamil.com>
2013-12-30 20:52:00 +0100

    Uses `contenteditable=true` in a div / h1 instead of a Javascript resized textarea in the article form. If Javascript is disabled, the form uses the `textarea` and `input`
    Issue #307.

--- app/assets/javascripts/admin_publify.js
+++ app/assets/javascripts/admin_publify.js
@@ -21,4 +21,19 @@ $(document).ready(function() {
 //  $('.autosave').each(function(e){autosave_request(e)});
 //  $('#article_form .new_category').each(function(cat_link){ cat_link.click(bind_new_category_overlay); });
   $('.merge_link').each(function(merge_link){ merge_link.click(bind_merge_link); });
+  $('#article_form').each(function(){load_article_form();})
+  $('#article_form').submit(function(){submit_article_form()});
+  
 });
+
+function load_article_form() {
+  $('#article-title-placeholder').text($('#article_title').val());
+  $('#article-body-placeholder').html($('#article_body_and_extended').text());
+  $('#article_title').hide();
+  $('#article_body_and_extended').hide();  
+}
+
+function submit_article_form() {
+  $('#article_title').val($('#article-title-placeholder').text());
+  $('#article_body_and_extended').text($('#article-body-placeholder').html());
+}
