458c49787c2d521eee0dd581bb2c87585401a20f
Yannick Francois <yannick.francois@free.fr>
2014-01-21 11:39:59 +0100

    Move lang to locale for post_Type error

--- app/models/post_type.rb
+++ app/models/post_type.rb
@@ -6,7 +6,7 @@ class PostType < ActiveRecord::Base
   before_save :sanitize_title
   
   def name_is_not_read
-      errors.add(:name, _("This article type already exists")) if name == 'read'
+    errors.add(:name, I18n.t(".errors.article_type_already_exist")) if name == 'read'
   end
   
   def sanitize_title
