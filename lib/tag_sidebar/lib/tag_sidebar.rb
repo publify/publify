159e3b03674ffaa95a12ef803ef2dea4ec48c280
Matijs van Zuijlen <matijs@matijs.net>
2014-01-22 07:51:18 +0100

    Fix call to find_all_with_article_counters in tag sidebar

--- lib/tag_sidebar/lib/tag_sidebar.rb
+++ lib/tag_sidebar/lib/tag_sidebar.rb
@@ -5,12 +5,13 @@ class TagSidebar < Sidebar
   setting :maximum_tags, 20
 
   def tags
-    @tags ||= Tag.find_all_with_article_counters(maximum_tags.to_i).sort_by {|t| t.name}
+    @tags ||= Tag.find_all_with_article_counters.
+      take(maximum_tags.to_i).sort_by {|t| t.name}
   end
 
   def sizes
     return @sizes if @sizes
-    total = @tags.inject(0) {|total, tag| total + tag.article_counter }
+    total = @tags.inject(0) {|sum, tag| sum + tag.article_counter }
     average = total.to_f / @tags.size.to_f
     @sizes = @tags.inject({}) do |h,tag|
       size = tag.article_counter.to_f / average
