3e3e83efff1ecb24f17949891df9d9fe94b902f9
Frédéric de Villamil <frederic@de-villamil.com>
2014-02-01 13:47:53 +0100

    Fixes the build.
    The new tagging system now returns all the tags in an array so the Javascript can do its job without hassling the server.
    
    Issue #330

--- spec/controllers/admin/content_controller_spec.rb
+++ spec/controllers/admin/content_controller_spec.rb
@@ -483,19 +483,7 @@ describe Admin::ContentController do
       it 'should return foo for keywords fo' do
         get :auto_complete_for_article_keywords, :article => {:keywords => 'fo'}
         response.should be_success
-        response.body.should == '<ul class="unstyled" id="autocomplete"><li>foo</li></ul>'
-      end
-
-      it 'should return nothing for hello' do
-        get :auto_complete_for_article_keywords, :article => {:keywords => 'hello'}
-        response.should be_success
-        response.body.should == '<ul class="unstyled" id="autocomplete"></ul>'
-      end
-
-      it 'should return bar and bazz for ba keyword' do
-        get :auto_complete_for_article_keywords, :article => {:keywords => 'ba'}
-        response.should be_success
-        response.body.should == '<ul class="unstyled" id="autocomplete"><li>bar</li><li>bazz</li></ul>'
+        response.body.should == "[\"bar\", \"bazz\", \"foo\"]"
       end
     end
   end
