287ae2da6abf865468f7e4c97b8adf2006a6d2d2
Yannick Francois <yannick.francois@free.fr>
2014-02-05 23:27:07 +0100

    Move lang to locale

--- lang/fr_FR.rb
+++ lang/fr_FR.rb
@@ -1,9 +1,4 @@
 #coding: utf-8
 Localization.define("fr_FR") do |l|
-  l.store "no articles", "aucun article"
-  l.store "Markdown", "Markdown"
-  l.store "Texttile", "Texttile"
   l.store "No comments", "Pas de commentaires"
-  l.store "Page not found", "Page non trouvée"
-  l.store "The page you are looking for has moved or does not exist.", "La page que vous cherchez a été déplacée ou n'existe pas"
 end
