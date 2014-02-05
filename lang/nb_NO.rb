9ca55f5ff0bbeec1f135f17194e516004e748166
Yannick Francois <yannick.francois@free.fr>
2014-02-05 23:20:04 +0100

    Move lang to locale

--- lang/nb_NO.rb
+++ lang/nb_NO.rb
@@ -1,7 +1,6 @@
 # coding: utf-8
 Localization.define("nb_NO") do |l|
   l.store "No comments", "Ingen kommentarer"
-  l.store "Welcome to your %s blog setup. Just fill in your blog title and your email, and Publify will take care of everything else", "Velkommen til din %s bloggoppsett. Bare fyll inn bloggtittel og epostadressen din, så vil Publify ta seg av resten"
   l.store "You can follow any response to this entry through the %s", "Du kan følge alle svar til denne artikkelen via %s"
   l.store "Markup", "Markup"
 end
--- lang/nl_NL.rb
+++ lang/nl_NL.rb
@@ -1,5 +1,4 @@
 # coding: utf-8
 Localization.define("nl_NL") do |l|
   l.store "%%a, %%d %%b %%Y %%H:%%M:%%S GMT", "%%a, %%d %%b %%Y %%H:%%M:%%S GMT"
-  l.store "Welcome to your %s blog setup. Just fill in your blog title and your email, and Publify will take care of everything else", "Welkom bij je %s blog setup. Vul een titel voor je blog in, en een e-mailadres, en Publify zorgt voor de rest"
 end
