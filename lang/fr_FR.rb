d366ccd095810fcb738563641396727b2daec3de
Yannick Francois <yannick.francois@free.fr>
2014-02-05 17:47:56 +0100

    Move lang to locales

--- lang/fr_FR.rb
+++ lang/fr_FR.rb
@@ -4,9 +4,6 @@ Localization.define("fr_FR") do |l|
   l.store "Markdown", "Markdown"
   l.store "Texttile", "Texttile"
   l.store "No comments", "Pas de commentaires"
-  l.store "Trackbacks are disabled", "Les rétroliens sont désactivés"
   l.store "Page not found", "Page non trouvée"
   l.store "The page you are looking for has moved or does not exist.", "La page que vous cherchez a été déplacée ou n'existe pas"
-  l.store "If you liked this article you can %s", "Si vous avez aimé cet article, vous pouvez %s"
-  l.store "add me to Twitter", "me suivre sur Twitter"
 end
--- lang/he_IL.rb
+++ lang/he_IL.rb
@@ -1,6 +1,4 @@
 # coding: utf-8
 Localization.define("he_IL") do |l|
-  l.store "Localization.rtl", "1"
-  l.store "Edit", "ערוך"
   l.store "%%a, %%d %%b %%Y %%H:%%M:%%S GMT", "%%a, %%d %%b %%Y %%H:%%M:%%S GMT"
 end
