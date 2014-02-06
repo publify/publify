2dcdbb42a7755d9a7b43aec1873fd80f94fabb84
Yannick Francois <yannick.francois@free.fr>
2014-02-06 22:59:27 +0100

    Remove unused keys

--- lang/nb_NO.rb
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-Localization.define("nb_NO") do |l|
-  l.store "You can follow any response to this entry through the %s", "Du kan følge alle svar til denne artikkelen via %s"
-  l.store "Markup", "Markup"
-end
