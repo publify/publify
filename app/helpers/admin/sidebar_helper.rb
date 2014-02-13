70d7f8e5b8bd70756f9f5d46ea0a12181856dad9
Thomas Lecavelier <thomas@lecavelier.name>
2014-02-13 14:10:23 +0100

    Switch warning and info class

--- app/helpers/admin/sidebar_helper.rb
+++ app/helpers/admin/sidebar_helper.rb
@@ -2,12 +2,12 @@ module Admin::SidebarHelper
   def class_for_admin_state(sidebar, this_position)
     case sidebar.admin_state
     when :active
-      return 'active alert-warning'
+      return 'active alert-info'
     when :will_change_position
       if this_position == sidebar.active_position
         return 'will_change ghost'
       else
-        return 'will_change alert-info'
+        return 'will_change alert-warning'
       end
     else
       raise sidebar.admin_state.inspect
