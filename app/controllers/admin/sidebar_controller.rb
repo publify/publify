0aa3d9648ae3045f807cd022615636736e15c8d7
Thomas Lecavelier <thomas@lecavelier.name>
2014-02-23 16:32:10 +0100

    Attempt to fix fdv problem with sortable

--- app/controllers/admin/sidebar_controller.rb
+++ app/controllers/admin/sidebar_controller.rb
@@ -42,25 +42,28 @@ class Admin::SidebarController < Admin::BaseController
 
   # Callback for admin sidebar sortable plugin
   def sortable
+    sorted = params[:sidebar].map(&:to_i)
+
+    Sidebar.transaction do
+      sorted.each_with_index do |sidebar_id, staged_index|
+        # DEV NOTE : Ok, that's a HUGE hack. Sidebar.available are Class, not Sidebar instances. In order to use jQuery.sortable we need that hack: Sidebar.available is an Array, so it's ordered. I arbitrary shift by? IT'SOVER NINE THOUSAND! considering we'll never reach 9K Sidebar instances or Sidebar specializations
+        sidebar = if sidebar_id >= 9000
+          Sidebar.available_sidebars[sidebar_id - 9000].new
+        else
+          Sidebar.find(sidebar_id)
+        end
+        sidebar.update_attributes(staged_position: staged_index)
+      end
+    end
+
+    @ordered_sidebars = Sidebar.ordered_sidebars
+    @available = Sidebar.available_sidebars
     respond_to do |format|
       format.json do
-        sorted = params[:sidebar].map(&:to_i)
-
-        Sidebar.transaction do
-          sorted.each_with_index do |sidebar_id, staged_index|
-            # DEV NOTE : Ok, that's a HUGE hack. Sidebar.available are Class, not Sidebar instances. In order to use jQuery.sortable we need that hack: Sidebar.available is an Array, so it's ordered. I arbitrary shift by? IT'SOVER NINE THOUSAND! considering we'll never reach 9K Sidebar instances or Sidebar specializations
-            sidebar = if sidebar_id >= 9000
-              Sidebar.available_sidebars[sidebar_id - 9000].new
-            else
-              Sidebar.find(sidebar_id)
-            end
-            sidebar.update_attributes(staged_position: staged_index)
-          end
-        end
-
-        @ordered_sidebars = Sidebar.ordered_sidebars
-        @available = Sidebar.available_sidebars
         render json: { html: render_to_string('admin/sidebar/_config.html.erb', layout: false) }
+      end
+      format.html do
+        return redirect_to admin_sidebar_index_path
       end
     end
   end
