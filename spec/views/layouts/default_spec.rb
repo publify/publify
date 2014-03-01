9c0afebb0a0f9cf1daa15443d7a50648e6e09b7b
Matijs van Zuijlen <matijs@matijs.net>
2014-03-01 19:53:57 +0100

    Simplify specs, silencing warnings

--- spec/views/layouts/default_spec.rb
+++ spec/views/layouts/default_spec.rb
@@ -10,16 +10,16 @@ describe "layouts/default.html.erb" do
         @controller.view_paths.unshift(view_path) if theme
       end
 
-      context "when use_meta_keyword set to true" do
-        let!(:blog) { create(:blog, use_meta_keyword: true) }
-        before(:each) {render}
-        it { expect(rendered).to have_selector('head>meta[name="keywords"]') }
+      it "has keyword meta tag when use_meta_keyword set to true" do
+        create(:blog, use_meta_keyword: true)
+        render
+        expect(rendered).to have_selector('head>meta[name="keywords"]')
       end
 
-      context "when use_meta_keyword set to false" do
-        let!(:blog) { create(:blog, use_meta_keyword: false) }
-        before(:each) {render}
-        it { expect(rendered).to_not have_selector('head>meta[name="keywords"]') }
+      it "does not have keyword meta tag when use_meta_keyword set to false" do
+        create(:blog, use_meta_keyword: false)
+        render
+        expect(rendered).to_not have_selector('head>meta[name="keywords"]')
       end
     end
   end
