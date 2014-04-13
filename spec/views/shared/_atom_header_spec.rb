d9f985c88f208716b95773b0b8c72974a864b198
Yannick Francois <yannick.francois@free.fr>
2014-04-13 21:19:53 +0200

    Just a little cleanup of a spec

--- spec/views/shared/_atom_header_spec.rb
+++ spec/views/shared/_atom_header_spec.rb
@@ -1,24 +1,23 @@
 require 'spec_helper'
 
 describe "shared/_atom_header.atom.builder" do
-  let!(:blog) { build_stubbed :blog }
+  let!(:blog) { create :blog }
 
   describe "with no items" do
-    before(:each) do
-      @xml = ::Builder::XmlMarkup.new
-      @xml.foo do
+    it "shows publify with the current version as the generator" do
+      xml = ::Builder::XmlMarkup.new
+      xml.foo do
         render partial: "shared/atom_header",
           formats: [:atom], handlers: [:builder],
-          locals: { feed: @xml, items: [] }
+          locals: { feed: xml, items: [] }
       end
-    end
 
-    it "shows publify with the current version as the generator" do
-      xml = Nokogiri::XML.parse(@xml.target!)
+      xml = Nokogiri::XML.parse(xml.target!)
       generator = xml.css("generator").first
-      generator.should_not be_nil
-      generator.content.should == "Publify"
-      generator["version"].should == PUBLIFY_VERSION
+
+      expect(generator).to_not be_nil
+      expect(generator.content).to eq("Publify")
+      expect(generator["version"]).to eq(PUBLIFY_VERSION)
     end
   end
 end
