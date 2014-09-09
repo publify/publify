f6adb79f0e3ae7f6a03e701fa460e54d2b483e96
Matijs van Zuijlen <matijs@matijs.net>
2014-09-09 16:48:36 +0200

    Match files as generated by Rails 4.1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Publify::Application.config.session_store :cookie_store, :key => '_publify_blog_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# Publify::Application.config.session_store :active_record_store
+Rails.application.config.session_store :cookie_store, key: '_publify_blog_session'
