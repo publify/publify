18ae147f9796bc5c694c50f1e9fd7611cdbdef12
Matijs van Zuijlen <matijs@matijs.net>
2014-09-17 12:31:04 +0200

    Include admin assets in precompile list

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,7 @@
-Rails.application.config.assets.precompile += %w(publify.js publify.css)
+Rails.application.config.assets.precompile += %w(
+  publify.js
+  publify_admin.js
+  publify.css
+  accounts.css
+  bootstrap.css
+)
