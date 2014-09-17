e6a18f51b1fa5c4816e8099102fb267f17cce3db
Frédéric de Villamil <frederic@de-villamil.com>
2014-09-17 22:22:22 +0200

    Fixes publishing. Need to investigate why the specs did not break on that one.
    Fixes #497

--- app/models/trigger.rb
+++ app/models/trigger.rb
@@ -34,7 +34,7 @@ class Trigger < ActiveRecord::Base
       return if pending_item.new_record?
       conditions_string =
         conditions.keys.collect{ |k| "(#{k} = :#{k})"}.join(' AND ')
-      with_scope(:find => where(conditions_string, conditions)) do
+      where(conditions_string, conditions) do
         delete_all(["pending_item_id = ? AND pending_item_type = ?",
                     pending_item.id, pending_item.class.to_s])
       end
