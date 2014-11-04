2eb424c18d3efb0241e12b92d6d8d94c44016200
alexwllms <alex.williams@moneyadviceservice.org.uk>
2014-11-04 12:01:27 +0000

    Add page titles to the styleguide controller

--- app/controllers/styleguide_controller.rb
+++ app/controllers/styleguide_controller.rb
@@ -2,8 +2,10 @@ class StyleguideController < ApplicationController
   layout 'styleguide.html.erb'
 
   def show
+    @page_title = 'Styleguide'
   end
 
   def article_page
+    @page_title = 'Article demo page'
   end
 end
