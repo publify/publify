e35e2cd2309fe688b3059d5cb9eb10d40c545209
Peter Guntrip <peter@guntrisoft.com>
2014-11-27 15:40:56 +0000

    altered date format to closer match design of article page

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -55,7 +55,7 @@ class Blog < ActiveRecord::Base
   setting :send_outbound_pings,        :boolean, true
   setting :email_from,                 :string, 'publify@example.com'
   setting :allow_signup,               :integer, 0
-  setting :date_format,                :string, '%d/%m/%Y'
+  setting :date_format,                :string, '%B %d %Y'
   setting :time_format,                :string, '%Hh%M'
   setting :image_avatar_size,          :integer, 48
   setting :image_thumb_size,           :integer, 125
