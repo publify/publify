d5f06c000b9406eab4eaaf22457dd14f3307ca47
David Kewal <david.kewal@gmail.com>
2014-12-01 11:54:22 +0000

    Set mailer url for test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,6 +29,7 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr.
