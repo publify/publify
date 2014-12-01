a52f16d0cc8e4c0646a5fdee8a6f69ff945dda78
David Kewal <david.kewal@gmail.com>
2014-12-01 11:53:09 +0000

    Configure mails to go via mailjet in prod and staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'moneyadviceservice.org.uk' }
+  config.action_mailer.delivery_method = :mailjet
 end
