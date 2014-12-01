786c4f2aa86d66e0a85a4fbd684a68a388c6dc95
David Kewal <david.kewal@gmail.com>
2014-12-01 11:42:38 +0000

    Set default mailer url in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'moneyadviceservice.org.uk' }
 end
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,1 @@
 require_relative "production"
-
-Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'blog-staging.moneyadviceservice.org.uk' }
-end
