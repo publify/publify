7b2fa7a2960db98f87bf132de05b5f4aa691a906
Jon Gilbraith <jon@jon.vc>
2014-12-09 16:59:46 +0000

    Add uploaders for the blog hero and teaser images.

---
+++ app/uploaders/content_hero_image_uploader.rb
@@ -0,0 +1,15 @@
+class ContentHeroImageUploader < CarrierWave::Uploader::Base
+
+  include CarrierWave::MiniMagick
+
+  DISPLAY_WIDTH = 1440
+  DISPLAY_HEIGHT = 488
+
+  def store_dir
+    "files/#{model.class.to_s.underscore}/#{model.id}/hero"
+  end
+
+  version :resized do
+    process resize_to_fill: [DISPLAY_WIDTH, DISPLAY_HEIGHT]
+  end
+end
