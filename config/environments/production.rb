953a1ee23fb79e6b4da04475514e06c884c89575
Yomi Colledge <baphled@boodah.net>
2014-12-22 10:49:19 +0000

    Add flag to enable basic auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,9 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'moneyadviceservice.org.uk' }
   config.action_mailer.delivery_method = :mailjet
 
-  config.middleware.use '::Rack::Auth::Basic' do |u, p|
-    [u,p] == [ENV['BASIC_AUTH_USER'], ENV['BASIC_AUTH_PASSWORD']]
+  if ENV['BASIC_AUTH_ENABLED']
+    config.middleware.use '::Rack::Auth::Basic' do |u, p|
+      [u,p] == [ENV['BASIC_AUTH_USER'], ENV['BASIC_AUTH_PASSWORD']]
+    end
   end
 end
