f0235cfe277218c345b0dc26c822a95901f1a1f1
pfonseca <paulovitor.java@gmail.com>
2015-01-05 21:12:47 -0200

    - configuração de email

--- config/environment.rb
+++ config/environment.rb
@@ -11,6 +11,6 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
+  :domain         => ENV['SENDGRID_DOMAIN'],
   :enable_starttls_auto => true
 }
