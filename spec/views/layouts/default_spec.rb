34beb5d8c2d4e17277e64b5cf591c575a961837a
Peter Guntrip <peter@guntrisoft.com>
2015-01-12 10:16:59 +0000

    fix breaking rspec tests

--- spec/views/layouts/default_spec.rb
+++ spec/views/layouts/default_spec.rb
@@ -3,6 +3,7 @@ describe "layouts/default.html.erb", :type => :view do
     assign(:keywords, ["foo", "bar"])
     assign(:auto_discovery_url_atom, "")
     assign(:auto_discovery_url_rss, "")
+    allow(view).to receive(:use_custom_header?).and_return(false)
   end
 
   it "has keyword meta tag when use_meta_keyword set to true" do
