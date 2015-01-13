1b80b80b79d7981440c1bc715163564db08dd6fc
Jon Gilbraith <jon@jon.vc>
2015-01-13 16:04:45 +0000

    Quotes.

--- lib/publify_textfilter_links_bold_italic.rb
+++ lib/publify_textfilter_links_bold_italic.rb
@@ -1,7 +1,7 @@
 class PublifyApp
   class Textfilter
     class LinksBoldItalic < TextFilterPlugin::PostProcess
-      plugin_display_name "Only Links, Bold and Italic tags permitted"
+      plugin_display_name 'Only Links, Bold and Italic tags permitted'
       plugin_description 'Strips out all html other than links, bold and italic tags.'
 
       def self.filtertext(_, _, text, _)
