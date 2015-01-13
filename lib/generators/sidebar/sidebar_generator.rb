e87cb714ecd8b5036547f30e7820bfe6e390cd13
Matijs van Zuijlen <matijs@matijs.net>
2015-01-13 10:43:39 +0100

    Update sidebar generator

--- lib/generators/sidebar/sidebar_generator.rb
+++ lib/generators/sidebar/sidebar_generator.rb
@@ -2,11 +2,9 @@ class SidebarGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
 
   def copy_stuff
-    template 'init.rb',       "#{plugin_path}/init.rb"
     template 'sidebar.rb',    "#{plugin_path}/lib/#{file_name}.rb"
-    template 'unit_test.rb',  "#{plugin_path}/test/#{file_name}_test.rb"
-    template 'Rakefile',      "#{plugin_path}/Rakefile"
-    template 'content.html.erb', "#{plugin_path}/views/content.html.erb"
+    template 'content.html.erb', "#{plugin_path}/app/views/#{file_name}/_content.html.erb"
+    template 'model_spec.rb',  "spec/models/#{file_name}_spec.rb"
   end
 
   private
