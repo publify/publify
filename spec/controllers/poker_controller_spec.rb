bdad5f8edbd96301fbc0168c686180528f2c1c35
pfonseca <paulovitor.java@gmail.com>
2015-01-20 23:16:02 -0200

    - poker page fix

--- spec/controllers/poker_controller_spec.rb
+++ spec/controllers/poker_controller_spec.rb
@@ -2,11 +2,6 @@ require 'rails_helper'
 
 RSpec.describe PokerController, :type => :controller do
 
-  describe "GET index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
+
 
 end
