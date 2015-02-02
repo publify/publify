bff2b28f875feca7fabcab15d051c274be0833ae
Yomi Colledge <baphled@boodah.net>
2015-02-02 14:47:45 +0000

    Temp disable database_cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -49,17 +49,6 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.order = :random
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
-  end
-
   Kernel.srand config.seed
 
   config.include SessionHelpers, type: :feature
