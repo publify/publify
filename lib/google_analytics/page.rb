d9e70074e4892c9e702ef91948d065b88bdc1c55
Yomi Colledge <baphled@boodah.net>
2015-02-04 14:15:00 +0000

    Add basic functionality to allow us to query Google Analytics to gather the most popular articles

---
+++ lib/google_analytics/page.rb
@@ -0,0 +1,20 @@
+module GoogleAnalytics
+  class Page
+    extend Legato::Model
+
+    metrics :pageviews
+    dimensions :page_path
+
+    def self.popular_articles(profile)
+      popular_pages = results(profile, start_date: 1.day.ago, end_date: Time.now, sort: '-pageviews', limit: 10_000)
+      popular_pages
+        .select { |page| page.pagePath =~ /\/en\/blog\/[^tags|categories|previews]([\w-])*/i }
+        .map { |p| { page_views: p.pageviews.to_i, label: label(p.pagePath) } }
+    end
+
+    def self.label(url)
+      match_data = url.match(/\/en\/blog\/([\w-]+)*/i)
+      match_data.captures.first
+    end
+  end
+end
