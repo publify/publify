5f33b11f08929ea31b3d4da31afe92339db34607
Matijs van Zuijlen <matijs@matijs.net>
2015-03-01 09:27:17 +0100

    Remove unused parts of BareMigration

--- lib/bare_migration.rb
+++ lib/bare_migration.rb
@@ -55,89 +55,12 @@
 #   script will still work.
 
 module BareMigration
-  def self.append_features(base)
-    base.extend(ClassMethods)
-
+  def self.included(base)
     # Set the table name by eradicating "Bare" from the calculated table name.
     # You can still use table_name= if this is wrong.
     base.table_name = base.table_name.sub(/.*?bare\d*_?/, '')
 
     # Default to ignoring the inheritance column
     base.inheritance_column = :ignore_inheritance_column
-  end
-
-  module ClassMethods
-    # The problem with STI is that it causes AR to instantiate and
-    # return classes DIFFERENT from the class that called find()
-    # (classes that come from your app/models dir, a no-no when
-    # migrating).  For now, this routine overrides that behavior
-    # so that you will ONLY get the same class as the one that called
-    # find.  (todo: make STI work again)
-    def instantiate_without_callbacks(record)
-      object = allocate
-      object.instance_variable_set('@attributes', record)
-      object
-    end
-
-    def find_and_update(find_type = :all, *rest, &update_block)
-      transaction do
-        find(find_type, *rest).each do |item|
-          update_block[item]
-          item.save!
-        end
-      end
-    end
-  end
-end
-
-class ActiveRecord::Migration
-  def self.opposite_of(method)
-    method = method.to_s
-    method.sub!(/^add_/, 'remove_') || method.sub!(/^remove_/, 'add_') ||
-      method.sub!(/^create/, 'drop') || method.sub!(/^drop/, 'create') ||
-      method.sub!(/^rename_column/, 'reverse_columns')
-  end
-
-  def self.modify_schema(method, *args)
-    case method.to_s
-    when 'create_table'
-      block = args.last.is_a?(Proc) ? args.pop : proc { |_t| nil }
-      create_table(*args, &block)
-    when 'remove_column'
-      remove_column args[0], args[1]
-    when 'reverse_columns'
-      (table, to_col, from_col) = args
-      rename_column table, from_col, to_col
-    else
-      send(method, *args)
-    end
-  end
-
-  def self.modify_tables_and_update(*colspecs, &block)
-    unless colspecs.first.is_a?(Array)
-      colspecs = [colspecs]
-    end
-    begin
-      updated_classes = []
-      colspecs.each do |spec|
-        if spec[1].is_a?(Class)
-          updated_classes << spec[1]
-          spec[1] = spec[1].table_name.to_sym
-        end
-      end
-      colspecs.each { |spec| modify_schema(*spec) }
-      updated_classes.uniq!
-      if updated_classes.size == 1 && block && block.arity == 1
-        say 'About to call find_and_update'
-        updated_classes.first.find_and_update(:all, &block)
-      else
-        block.call if block
-      end
-    rescue Exception => e
-      colspecs.reverse.each do |(method, table, column, *rest)|
-        modify_schema(opposite_of(method), table, column, *rest) rescue nil
-      end
-      raise e
-    end
   end
 end
