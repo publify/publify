3276bfb7aa2e5058b29bd696692db2f6c0858fb3
David Kewal <david.kewal@gmail.com>
2015-03-25 16:21:05 +0000

    Opt in new errors behaviour for callbacks and remove asset warnings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # Enable deflate / gzip compression of controller-generated responses
   config.middleware.use Rack::Deflater
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
