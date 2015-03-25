cd4d84abeeb7e8f14c8cbd15cd47a590aa29728f
David Kewal <david.kewal@gmail.com>
2015-03-25 16:37:57 +0000

    Replace secret_token by secret_key_base

--- spec/services/admin/token_checker_spec.rb
+++ spec/services/admin/token_checker_spec.rb
@@ -4,12 +4,12 @@ describe Admin::TokenChecker do
 
   describe '#safe_token_in_use?' do
     it 'returns false with the default token loaded' do
-      Publify::Application.config.secret_token = $default_token
+      Publify::Application.config.secret_key_base = $default_token
       expect(checker.safe_token_in_use?).to be_falsey
     end
 
     it 'returns true with some other token loaded' do
-      Publify::Application.config.secret_token = 'foo'
+      Publify::Application.config.secret_key_base = 'foo'
       expect(checker.safe_token_in_use?).to be_truthy
     end
   end
