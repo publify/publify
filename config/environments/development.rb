3bf55deb2fa5f2f01121d8f8486aacba51e4250f
David Kewal <david.kewal@gmail.com>
2015-03-26 10:39:32 +0000

    Dont fingerprint assets in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,6 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.assets.digest = true
-
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :letter_opener
 
