841684e0c52ec76393e447252c7f4271a42d2896
Dumsterdev <dsd@dumsterdev.net>
2015-04-27 00:00:34 -0700

    Fix for - Test a fix for Added AWS Region variable for AWS V4 Auth by pacergh - adding comma

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,7 +11,7 @@ else
       config.fog_credentials = {
         :provider               => 'AWS',
         :aws_access_key_id      => ENV["aws_access_key_id"],
-        :aws_secret_access_key  => ENV["aws_secret_access_key"]
+        :aws_secret_access_key  => ENV["aws_secret_access_key"],
         :region                 => ENV["aws_region"]
       }
 
