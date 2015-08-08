05fd7aceb480c02f153ab212dd33cb5837f54485
Matijs van Zuijlen <matijs@matijs.net>
2015-08-08 21:27:55 +0200

    Add blog id to redirects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807134129) do
+ActiveRecord::Schema.define(version: 20150808191127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,6 +128,7 @@ ActiveRecord::Schema.define(version: 20150807134129) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "content_id"
+    t.integer  "blog_id"
   end
 
   create_table "resources", force: :cascade do |t|
