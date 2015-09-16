c19e147e6dc4d035a9006fb6ef25b2a62e73d019
Matijs van Zuijlen <matijs@matijs.net>
2015-09-16 07:45:04 +0200

    Fix RuboCop offenses

--- lib/active_support_patch.rb
+++ lib/active_support_patch.rb
@@ -8,14 +8,14 @@ module ActionView
         html_options, name = name, nil if block_given?
         html_options = (html_options || {}).stringify_keys
 
-        extras = %w{ cc bcc body subject }.map! { |item|
+        extras = %w( cc bcc body subject ).map! { |item|
           option = html_options.delete(item) || next
           "#{item}=#{Rack::Utils.escape_path(option)}"
         }.compact
         extras = extras.empty? ? '' : '?' + extras.join('&')
 
-        encoded_email_address = ERB::Util.url_encode(email_address.to_str).gsub("%40", "@")
-        html_options["href"] = "mailto:#{encoded_email_address}#{extras}"
+        encoded_email_address = ERB::Util.url_encode(email_address.to_str).gsub('%40', '@')
+        html_options['href'] = "mailto:#{encoded_email_address}#{extras}"
 
         content_tag(:a, name || email_address, html_options, &block)
       end
