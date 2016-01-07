d7ab3682829897bc347c23fa080832323c1f048d
Matijs van Zuijlen <matijs@matijs.net>
2016-01-07 08:01:38 +0100

    Crudely detect extra HTML escaping in specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,12 @@ RSpec.configure do |config|
 
   # shortcuts for factory_girl to use: create / build / build_stubbed
   config.include FactoryGirl::Syntax::Methods
+
+  config.after :each, type: :controller do
+    if response.body =~ /(&lt;[a-z]+)/
+      raise "Double escaped HTML in text (#{$1})"
+    end
+  end
 end
 
 def define_spec_public_cache_directory
