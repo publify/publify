0a1656beb0f3183294750d0b246f6e3881385626
Matijs van Zuijlen <matijs@matijs.net>
2016-01-08 18:15:40 +0100

    Let Devise handle access denied when not logged in

--- lib/publify_login_system/login_system.rb
+++ lib/publify_login_system/login_system.rb
@@ -6,38 +6,26 @@ module LoginSystem
       AccessControl.allowed_controllers(current_user.profile.label, current_user.profile.modules).include?(params[:controller])
     end
 
-    def authorized?
-      user_signed_in? && allowed?
+    def login_required
+      authenticate_user! && authorized
     end
 
-    def login_required
-      authorized? || access_denied
+    def authorized
+      allowed? || access_denied
     end
 
     def access_denied
       respond_to do |accepts|
         accepts.html do
-          # store_location
-          session[:return_to] = request.fullpath
-          if user_signed_in?
-            flash[:error] = "You're not allowed to perform this action"
-            redirect_to controller: 'admin/dashboard', action: 'index'
-          elsif User.first
-            redirect_to new_user_session_path
-          else
-            redirect_to new_user_registration_path
-          end
+          flash[:error] = "You're not allowed to perform this action"
+          redirect_to controller: 'admin/dashboard', action: 'index'
         end
         accepts.xml do
           headers['Status'] = 'Unauthorized'
           headers['WWW-Authenticate'] = %(Basic realm="Web Password")
-          render text: "Could't authenticate you", status: '401 Unauthorized'
+          render text: "Could't authorize you", status: '401 Unauthorized'
         end
       end
       false
-    end
-
-    def store_location
-      session[:return_to] = request.fullpath
     end
 end
