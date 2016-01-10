dd9673c59617e027d1613a49e4b3fadb65a3b4e9
Matijs van Zuijlen <matijs@matijs.net>
2016-01-10 11:14:16 +0100

    Hard code accessible controllers the CanCanCan way

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,11 +4,35 @@ class Ability
   def initialize(user)
     return unless user
 
-    allowed_controllers = AccessControl.allowed_controllers(user.profile.label, user.profile.modules)
-    allowed_controllers.each do |controller_name|
-      can :manage, controller_name
+    case user.profile.label
+    when 'admin'
+      can :manage, "admin/cache"
+      can :manage, "admin/content"
+      can :manage, "admin/feedback"
+      can :manage, "admin/notes"
+      can :manage, "admin/pages"
+      can :manage, "admin/post_types"
+      can :manage, "admin/redirects"
+      can :manage, "admin/resources"
+      can :manage, "admin/seo"
+      can :manage, "admin/settings"
+      can :manage, "admin/sidebar"
+      can :manage, "admin/tags"
+      can :manage, "admin/themes"
+      can :manage, "admin/users"
+    when 'publisher'
+      can :manage, "admin/content"
+      can :manage, "admin/feedback"
+      can :manage, "admin/notes"
+      can :manage, "admin/pages"
+      can :manage, "admin/post_types"
+      can :manage, "admin/redirects"
+      can :manage, "admin/resources"
+      can :manage, "admin/tags"
+    when 'contributor'
     end
 
+    # TODO: Limit these!
     can :manage, "admin/cache"
     can :manage, "admin/dashboard"
     can :manage, "admin/textfilters"
