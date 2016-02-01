78e13fd65159dcd741df384dd5efc2c2fce5f71e
Matijs van Zuijlen <matijs@matijs.net>
2016-02-01 08:33:20 +0100

    Enable unused keys spec

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe 'I18n' do
   end
 
   it 'does not have unused keys' do
-    skip 'Needs to be fixed first :-)'
     expect(unused_keys).to be_empty,
       "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
   end
