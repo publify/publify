33b47527ba3973ca00f03f62b84dd10f8b37be01
Matijs van Zuijlen <matijs@matijs.net>
2016-04-07 07:53:56 +0200

    Remove broken and unused plugin initializer
    This code was never called, and the plugin's kind is determined by its
    class and not at initialization time.

--- lib/publify_plugins.rb
+++ lib/publify_plugins.rb
@@ -58,13 +58,5 @@ module PublifyPlugins
         :void
       end
     end # << self
-
-    def initialize(h = {})
-      h = h.dup
-      kind = h.delete(:kind)
-      raise ArgumentError.new "#{kind} is not part of available plugins targets (#{KINDS.map(&:to_s).join(',')})" unless KINDS.include?(kind)
-      @kind = kind
-      raise ArgumentError.new "Too many keys in PublifyPlugins::Base hash: I don't know what to do with your remainder: #{h.inspect}" unless h.empty?
-    end
   end
 end
