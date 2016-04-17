563309cfb731ffe465c5a1b2b5eeca3e9f46e8c2
Matijs van Zuijlen <matijs@matijs.net>
2016-04-17 12:25:25 +0200

    Extract service TitleBuilder

--- lib/transforms.rb
+++ lib/transforms.rb
@@ -30,40 +30,7 @@ class String
   end
 
   def to_title(item, settings, params)
-    s = self
-
-    # Tags for params
-    s = s.gsub('%date%', parse_date(s, params)) if s =~ /%date%/
-    s = s.gsub('%search%', params[:q]) if params[:q]
-    s = s.gsub('%page%', parse_page(s, params)) if s =~ /%page%/
-
-    # Tags for settings
-    s = s.gsub('%blog_name%', settings.blog_name)
-    s = s.gsub('%blog_subtitle%', settings.blog_subtitle)
-    s = s.gsub('%meta_keywords%', settings.meta_keywords)
-
-    # Tags for item
-    s = s.gsub('%title%', item.title) if s =~ /%title/ && item.respond_to?(:title)
-    s = s.gsub('%excerpt%', item.excerpt_text) if s =~ /%excerpt%/ && item.respond_to?(:excerpt_text)
-    s = s.gsub('%description%', item.description) if s =~ /%description%/ && item.respond_to?(:description)
-    s = s.gsub('%name%', item.name) if s =~ /%name%/ && item.respond_to?(:name)
-    s = s.gsub('%author%', item.name) if s =~ /%author%/ && item.respond_to?(:name)
-    s = s.gsub('%body%', item.body) if s =~ /%body%/ && item.respond_to?(:body)
-
-    if s =~ /%categories%/ && item.respond_to?(:categories)
-      s = s.gsub('%categories%', item.categories.map(&:name).join(', '))
-    end
-
-    if s =~ /%tags%/ && item.respond_to?(:tags)
-      s = s.gsub('%tags%', item.tags.map(&:display_name).join(', '))
-    end
-
-    # Other
-    s = s.gsub('%currentdate%', Time.now.strftime(settings.date_format))
-    s = s.gsub('%currenttime%', Time.now.strftime(settings.time_format))
-    s = s.gsub('%currentmonth%', Time.now.strftime('%B'))
-    s = s.gsub('%currentyear%', Time.now.year.to_s)
-    s
+    TitleBuilder.new(self).build(item, settings, params)
   end
 
   # Strips any html markup from a string
@@ -74,23 +41,5 @@ class String
   TAG = %r{<[!/?\[]?(?:#{TYPO_TAG_KEY}|--)(?:\s+#{TYPO_ATTRIBUTES})?\s*(?:[!/?\]]+|--)?>}
   def strip_html
     gsub(TAG, '').gsub(/\s+/, ' ').strip
-  end
-
-  private
-
-  def parse_date(string, params)
-    return '' unless params[:year]
-
-    format = ''
-    format << '%A %d ' if params[:day]
-    format << '%B ' if params[:month]
-    format << '%Y' if params[:year]
-
-    string.gsub('%date%', Time.mktime(*params.values_at(:year, :month, :day)).strftime(format))
-  end
-
-  def parse_page(_string, params)
-    return '' unless params[:page]
-    "#{I18n.t('articles.index.page')} #{params[:page]}"
   end
 end
