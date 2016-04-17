c5733a4576afc3b652f471df71d62afb720b65be
Matijs van Zuijlen <matijs@matijs.net>
2016-04-17 16:19:38 +0200

    Update setup controller specs
    - Remove tests for the case where an article but no users exist. This
      case should no longer occur since creating an article is no longer
      part of the seeding.
    - Clean up and unify spec preparation

--- spec/controllers/setup_controller_spec.rb
+++ spec/controllers/setup_controller_spec.rb
@@ -5,7 +5,6 @@ describe SetupController, type: :controller do
     before do
       # Set up database similar to result of seeding
       @blog = Blog.create
-      create :tag, blog: @blog
       create :none
     end
 
@@ -22,79 +21,49 @@ describe SetupController, type: :controller do
         post 'index', setting: { blog_name: 'Foo', email: 'foo@bar.net' }
       end
 
-      context 'when a first article exists' do
-        before do
-          Blog.first.articles.create(title: 'First post').save!
-          post_setup_index
-        end
-
-        specify do
-          expect(response).to redirect_to(controller: 'accounts',
-                                          action: 'confirm')
-        end
-
-        it 'should correctly initialize blog and users' do
-          expect(Blog.first.blog_name).to eq('Foo')
-          admin = User.find_by_login('admin')
-          expect(admin).not_to be_nil
-          expect(admin.email).to eq('foo@bar.net')
-          expect(Article.first.user).to eq(admin)
-          expect(Page.first.user).to eq(admin)
-        end
-
-        it 'should log in admin user' do
-          expect(controller.current_user).to eq(User.find_by_login('admin'))
-        end
+      before do
+        post_setup_index
       end
 
-      context 'when no first article exists' do
-        before do
-          post_setup_index
-        end
+      specify do
+        expect(response).to redirect_to(controller: 'accounts',
+                                        action: 'confirm')
+      end
 
-        specify do
-          expect(response).to redirect_to(controller: 'accounts',
-                                          action: 'confirm')
-        end
+      it 'should correctly initialize blog and users' do
+        expect(Blog.first.blog_name).to eq('Foo')
+        admin = User.find_by_login('admin')
+        expect(admin).not_to be_nil
+        expect(admin.email).to eq('foo@bar.net')
+        expect(Article.first.user).to eq(admin)
+        expect(Page.first.user).to eq(admin)
+      end
 
-        it 'should correctly initialize blog and users' do
-          expect(Blog.first.blog_name).to eq('Foo')
-          admin = User.find_by_login('admin')
-          expect(admin).not_to be_nil
-          expect(admin.email).to eq('foo@bar.net')
-          expect(Article.first.user).to eq(admin)
-          expect(Page.first.user).to eq(admin)
-        end
-
-        it 'should log in admin user' do
-          expect(controller.current_user).to eq(User.find_by_login('admin'))
-        end
+      it 'should log in admin user' do
+        expect(controller.current_user).to eq(User.find_by_login('admin'))
       end
     end
-  end
 
-  describe 'POST setup with incorrect parameters' do
-    before do
-      Blog.delete_all
-      User.delete_all
-      Blog.new.save
-    end
+    describe 'POST setup with incorrect parameters' do
+      it 'empty blog name should raise an error' do
+        post 'index', setting: { blog_name: '', email: 'foo@bar.net' }
+        expect(response).to redirect_to(action: 'index')
+      end
 
-    it 'empty blog name should raise an error' do
-      post 'index', setting: { blog_name: '', email: 'foo@bar.net' }
-      expect(response).to redirect_to(action: 'index')
-    end
-
-    it 'empty email should raise an error' do
-      post 'index', setting: { blog_name: 'Foo', email: '' }
-      expect(response).to redirect_to(action: 'index')
+      it 'empty email should raise an error' do
+        post 'index', setting: { blog_name: 'Foo', email: '' }
+        expect(response).to redirect_to(action: 'index')
+      end
     end
   end
 
   describe 'when a blog is configured and has some users' do
+    before do
+      create(:blog)
+    end
+
     describe 'GET setup' do
       before do
-        FactoryGirl.create(:blog)
         get 'index'
       end
 
@@ -103,7 +72,6 @@ describe SetupController, type: :controller do
 
     describe 'POST setup' do
       before do
-        FactoryGirl.create(:blog)
         post 'index', setting: { blog_name: 'Foo', email: 'foo@bar.net' }
       end
 
