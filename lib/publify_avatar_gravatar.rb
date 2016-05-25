893d65f600602999eb1a4b2c874b616ed9260ae4
Matijs van Zuijlen <matijs@matijs.net>
2016-05-25 09:29:56 +0200

    Clean up Gravatar plugin code
    Removes unused files and moves implementation to the base file.

--- lib/publify_avatar_gravatar.rb
+++ lib/publify_avatar_gravatar.rb
@@ -1,5 +1,36 @@
-require File.join(Rails.root, 'lib', 'publify_plugins.rb')
-require File.join(Rails.root, 'lib', 'avatar_plugin.rb')
-require File.join(Rails.root, 'lib', 'publify_avatar_gravatar', 'lib', 'publify_avatar_gravatar.rb')
+require 'publify_plugins'
+require 'avatar_plugin'
+
+# PublifyAvatarGravatar
+module PublifyPlugins
+  class Gravatar < AvatarPlugin
+    @description = 'Provide user avatar image throught the http://gravatar.com service.'
+
+    class << self
+      def get_avatar(options = {})
+        email = options.delete(:email) || ''
+        gravatar_tag(email, options)
+      end
+
+      def name
+        'Gravatar'
+      end
+
+      private
+
+      # Generate the image tag for a commenters gravatar based on their email address
+      # Valid options are described at http://www.gravatar.com/implement.php
+      def gravatar_tag(email, options = {})
+        options[:gravatar_id] = Digest::MD5.hexdigest(email.strip)
+        options[:default] = CGI.escape(options[:default]) if options.include?(:default)
+        options[:size] ||= 48
+        klass = options[:class] ? options[:class] : 'avatar gravatar'
+
+        url = '//www.gravatar.com/avatar.php?' << options.map { |key, value| "#{key}=#{value}" }.sort.join('&amp;')
+        "<img src=\"#{url}\" class=\"#{klass}\" alt=\"Gravatar\" />"
+      end
+    end
+  end
+end
 
 PublifyPlugins::Keeper.register(PublifyPlugins::Gravatar)
--- lib/publify_avatar_gravatar/init.rb
+++
@@ -1,4 +0,0 @@
-require 'avatar_plugin'
-require 'publify_avatar_gravatar'
-
-PublifyPlugins::Keeper.register(PublifyPlugins::Gravatar)
