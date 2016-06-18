377650eda874522f300d4780328e3eb39d6d3dfd
Matijs van Zuijlen <matijs@matijs.net>
2016-06-18 17:03:47 +0200

    Set up FactoryGirl

--- publify_core/lib/publify_core/engine.rb
+++ publify_core/lib/publify_core/engine.rb
@@ -2,6 +2,7 @@ module PublifyCore
   class Engine < ::Rails::Engine
     config.generators do |generators|
       generators.test_framework :rspec, fixture: false
+      generators.fixture_replacement :factory_girl, dir: 'spec/factories'
     end
   end
 end
--- publify_core/spec/rails_helper.rb
+++ publify_core/spec/rails_helper.rb
@@ -6,6 +6,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'factory_girl'
+FactoryGirl.find_definitions
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -54,4 +56,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # shortcuts for factory_girl to use: create / build / build_stubbed
+  config.include FactoryGirl::Syntax::Methods
 end
