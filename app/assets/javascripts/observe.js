7a2493a6398a9a9777a9d5489cf5e15765b38ed0
Matijs van Zuijlen <matijs@matijs.net>
2016-06-26 11:40:31 +0200

    Make live search respond more directly

--- app/assets/javascripts/observe.js
+++ app/assets/javascripts/observe.js
@@ -1,12 +1,22 @@
 $(document).ready(function(){
-  $('.observable').on('change',
-      function(evt) {
-        var $field = $(evt.target);
-        var url = $field.data('url');
-        var target = $field.data('target');
-        $.get(url, $field.serialize(),
-            function(data) {
-              $(target).html(data);
-            });
-      });
+  var timer = 0;
+
+  var handle_field_change = function(evt) {
+    var $field = $(evt.target);
+    var url = $field.data('url');
+    var target = $field.data('target');
+    $.get(url, $field.serialize(),
+        function(data) {
+          $(target).html(data);
+        });
+  };
+
+  var delay_callback = function(callback) {
+    clearTimeout(timer);
+    timer = setTimeout(callback, 200);
+  }
+
+  $('.observable').on('keyup keydown paste', function(evt) {
+    delay_callback(function() { handle_field_change(evt) });
+  });
 });
