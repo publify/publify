92fa8740dda2a6215307aa28352129614e0e5038
Matijs van Zuijlen <matijs@matijs.net>
2016-06-26 22:38:30 +0200

    Refactor SidebarRegistry to store set of Strings
    This under the hood change will allow delayed loading of sidebar
    classes.

--- lib/sidebar_registry.rb
+++ lib/sidebar_registry.rb
@@ -1,22 +1,21 @@
 class SidebarRegistry
   class << self
-    def register_sidebar(klass)
-      registered_sidebars << klass
-      @available_sidebar_types = nil
+    def register_sidebar(klass_name)
+      registered_sidebars << klass_name.to_s
     end
 
     def available_sidebars
-      registered_sidebars.sort_by(&:to_s)
+      registered_sidebars.sort.map(&:constantize)
     end
 
     def available_sidebar_types
-      registered_sidebars.map(&:to_s).sort
+      registered_sidebars.sort
     end
 
     private
 
     def registered_sidebars
-      @registered_sidebars ||= []
+      @registered_sidebars ||= Set.new
     end
   end
 end
