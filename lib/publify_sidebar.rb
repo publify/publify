a67c5838aa9e4c56a78ee831a1768d3c2049b433
Matijs van Zuijlen <matijs@matijs.net>
2016-06-26 23:05:04 +0200

    Autoload sidebars on first use

--- lib/publify_sidebar.rb
+++ lib/publify_sidebar.rb
@@ -1,7 +1,10 @@
+require 'sidebar_registry'
+
 plugins_root = File.join(::Rails.root.to_s, 'lib')
 separator = plugins_root.include?('/') ? '/' : '\\'
 
 Dir.glob(File.join(plugins_root, '*_sidebar')).select do |file|
   plugin_name = file.split(separator).last
-  require File.join(plugin_name, 'lib', plugin_name)
+  SidebarRegistry.register_sidebar plugin_name.classify
+  Rails.application.config.autoload_paths << File.join(plugins_root, plugin_name, 'lib')
 end
