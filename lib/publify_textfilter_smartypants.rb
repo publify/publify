51248717fd746a84ec1ea378b40970b740dbf56e
Matijs van Zuijlen <matijs@matijs.net>
2016-07-12 21:44:56 +0200

    Fix substitution error

--- lib/publify_textfilter_smartypants.rb
+++ lib/publify_textfilter_smartypants.rb
@@ -2,7 +2,7 @@ class PublifyApp
   class Textfilter
     class Smartypants < TextFilterPlugin::PostProcess
       plugin_display_name 'Smartypants'
-      plugin_description 'Converts HTML to use publifygraphically correct quotes and dashes'
+      plugin_description 'Converts HTML to use typographically correct quotes and dashes'
 
       def self.filtertext(text)
         RubyPants.new(text).to_html
