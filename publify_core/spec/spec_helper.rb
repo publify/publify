0921c3478440b47571cca9c02912e59f57d61104
Matijs van Zuijlen <matijs@matijs.net>
2016-07-14 09:25:22 +0200

    Almost use recommended RSpec config for publify_core
    Still allow monkey patching, since disallowing would mean a rather large
    change to the existing spec files. This is left as a TODO.

--- publify_core/spec/spec_helper.rb
+++ publify_core/spec/spec_helper.rb
@@ -40,9 +40,6 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-# The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart's content.
-=begin
   # These two settings work together to allow you to limit a spec run
   # to individual examples or groups you care about by tagging them with
   # `:focus` metadata. When nothing is tagged with `:focus`, all examples
@@ -60,7 +57,9 @@ RSpec.configure do |config|
   #   - http://rspec.info/blog/2012/06/rspecs-new-expectation-syntax/
   #   - http://www.teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
   #   - http://rspec.info/blog/2014/05/notable-changes-in-rspec-3/#zero-monkey-patching-mode
-  config.disable_monkey_patching!
+  #
+  # TODO: Disable monkey patching and fix spec code
+  # config.disable_monkey_patching!
 
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
@@ -88,5 +87,4 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
-=end
 end
