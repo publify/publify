bd1f2adf3693b1add6cf34e4adb4b90a2ad31e83
Matijs van Zuijlen <matijs@matijs.net>
2016-11-26 23:22:42 +0100

    Remove page cache related code

--- config/application.rb
+++ config/application.rb
@@ -12,10 +12,6 @@ module Publify
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Setup the cache path
-    config.action_controller.page_cache_directory = "#{::Rails.root.to_s}/public/cache/"
-    config.cache_store=:file_store, "#{::Rails.root.to_s}/public/cache/"
-
     config.plugins = [ :all ]
 
     # Turn om timestamped migrations
--- lib/tasks/sweep_cache.rake
+++
@@ -1,6 +0,0 @@
-desc "Force a sweeping run of publify's static page caches (all of them!)"
-task sweep_cache: :environment do
-  require 'page_cache'
-  PageCache.sweep_all
-  puts 'Cache swept.'
-end
