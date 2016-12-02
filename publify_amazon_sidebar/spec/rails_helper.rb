bef045afe2bcb8846942aa5b830a047396706f31
Matijs van Zuijlen <matijs@matijs.net>
2016-12-02 14:09:53 +0100

    Remove pending migrations check
    This check leads to environment warnings.

--- publify_amazon_sidebar/spec/rails_helper.rb
+++ publify_amazon_sidebar/spec/rails_helper.rb
@@ -22,10 +22,6 @@ require 'rspec/rails'
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-# Checks for pending migration and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.maintain_test_schema!
-
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
