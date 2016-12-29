4391eb57c9bb1b0af13ce01215eca1ea25299150
Matijs van Zuijlen <matijs@matijs.net>
2016-12-29 20:11:04 +0100

    Re-implement magic byte mime type check
    Implementation based on CarrierWave 1.0.0's MagicMimeBlacklist.

--- publify_core/app/models/resource.rb
+++ publify_core/app/models/resource.rb
@@ -6,15 +6,5 @@ class Resource < ActiveRecord::Base
   belongs_to :article, optional: true
 
   mount_uploader :upload, ResourceUploader
-  validate :image_mime_type_consistent
   validates :upload, presence: true
-
-  private
-
-  def image_mime_type_consistent
-    if upload.content_type =~ %r{^image/}
-      expected_type = upload.file.content_type
-      errors.add(:upload, 'Has MIME type mismatch') unless upload.content_type == expected_type
-    end
-  end
 end
--- publify_core/app/uploaders/resource_uploader.rb
+++ publify_core/app/uploaders/resource_uploader.rb
@@ -1,5 +1,8 @@
+require 'filemagic'
+
 class ResourceUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
+  before :cache, :check_image_content_type!
 
   def store_dir
     "files/#{model.class.to_s.underscore}/#{model.id}"
@@ -26,5 +29,34 @@ class ResourceUploader < CarrierWave::Uploader::Base
   def image?(new_file)
     content_type = new_file.content_type
     content_type && content_type.include?('image')
+  end
+
+  def check_image_content_type!(new_file)
+    if image?(new_file)
+      magic_type = filemagic_content_type(new_file)
+      if magic_type != new_file.content_type
+        raise CarrierWave::IntegrityError, 'has MIME type mismatch'
+      end
+    end
+  end
+
+  private
+
+  # NOTE: This method was adapted from MagicMimeBlacklist#extract_content_type
+  # from CarrierWave 1.0.0.
+  def filemagic_content_type(new_file)
+    content_type = nil
+
+    File.open(new_file.path) do |fd|
+      data = fd.read(1024) || ""
+      content_type = filemagic.buffer(data)
+    end
+
+    content_type
+  end
+
+  # NOTE: This method was copied from MagicMimeBlacklist from CarrierWave 1.0.0.
+  def filemagic
+    @filemagic ||= FileMagic.new(FileMagic::MAGIC_MIME_TYPE)
   end
 end
