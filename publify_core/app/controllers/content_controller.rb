382e4f5d9a64e94431030872ace062563abed9db
Matijs van Zuijlen <matijs@matijs.net>
2017-05-20 20:44:52 +0200

    Remove unused expiry filter class

--- publify_core/app/controllers/content_controller.rb
+++ publify_core/app/controllers/content_controller.rb
@@ -1,21 +1,5 @@
 class ContentController < BaseController
-  class ExpiryFilter
-    def before(_controller)
-      @request_time = Time.now
-    end
-
-    def after(controller)
-      future_article =
-        this_blog.articles.where('published = ? AND published_at > ?', true, @request_time).
-          order('published_at ASC').first
-      if future_article
-        delta = future_article.published_at - Time.now
-        controller.response.lifetime = delta <= 0 ? 0 : delta
-      end
-    end
-  end
-
-  protected
+  private
 
   # TODO: Make this work for all content.
   def auto_discovery_feed(options = {})
