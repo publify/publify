b53a443f8a7abd6401a99977841edff65d3a986b
Matijs van Zuijlen <matijs@matijs.net>
2017-06-03 10:06:17 +0200

    Clean up ArchivesSidebar#parse_request
    Rewrites the method to not use .find_by_sql but use ActiveRecord's
    methods and defined scopes for Article instead.

--- publify_core/app/models/archives_sidebar.rb
+++ publify_core/app/models/archives_sidebar.rb
@@ -6,16 +6,12 @@ class ArchivesSidebar < Sidebar
 
   attr_accessor :archives
 
-  def self.date_func
+  def self.date_funcs
     @date_func ||=
-      begin
-        if Content.connection.is_a?(ActiveRecord::ConnectionAdapters::SQLite3Adapter)
-          "strftime('%Y', published_at) as year, strftime('%m', published_at) as month"
-        else
-          'extract(year from published_at) as year,extract(month from published_at) as month'
-        end
-      rescue NameError
-        'extract(year from published_at) as year,extract(month from published_at) as month'
+      if Content.connection.class.name =~ /SQLite3Adapter/
+        ["strftime('%Y', published_at) as year", "strftime('%m', published_at) as month"]
+      else
+        ['extract(year from published_at) as year', 'extract(month from published_at) as month']
       end
   end
 
@@ -25,9 +21,10 @@ class ArchivesSidebar < Sidebar
     # Unfortunately, there's no universally-supported way to do this
     # across all three of our supported DBs.  So, we resort to a bit of
     # DB-specific code.
-    date_func = self.class.date_func
+    date_funcs = self.class.date_funcs
 
-    article_counts = Content.find_by_sql(["select count(*) as count, #{date_func} from contents where type='Article' and state = 'published' and published_at < ? group by year,month order by year desc,month desc limit ? ", Time.now, count.to_i])
+    article_counts = Article.published.select('count(*) as count', *date_funcs).
+      group(:year, :month).reorder('year desc', 'month desc').limit(count.to_i)
 
     @archives = article_counts.map do |entry|
       month = entry.month.to_i
--- publify_core/spec/models/archives_sidebar_spec.rb
+++ publify_core/spec/models/archives_sidebar_spec.rb
@@ -10,11 +10,19 @@ describe ArchivesSidebar do
   describe '#parse_request' do
     before { build_stubbed :blog }
 
-    it 'creates correct month + year labels' do
+    it 'creates the correct data structure for each month' do
       create :article, published_at: DateTime.new(2014, 3, 2)
+      create :article, published_at: DateTime.new(2015, 2, 2)
+      create :article, published_at: DateTime.new(2015, 2, 5)
+
       sidebar.parse_request nil, nil
 
-      expect(sidebar.archives.first[:name]).to eq 'March 2014'
+      expected_structure = [
+        { name: 'February 2015', month: 2, year: 2015, article_count: 2 },
+        { name: 'March 2014', month: 3, year: 2014, article_count: 1 }
+      ]
+
+      expect(sidebar.archives).to eq expected_structure
     end
   end
 end
