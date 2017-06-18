cb22016b965d554c3cb23b98841c48203a25834f
Matijs van Zuijlen <matijs@matijs.net>
2017-06-18 21:37:52 +0200

    Add specs for feedback feed templates

---
+++ publify_core/spec/views/feedback/index.rss.builder_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'feedback/index.rss.builder', type: :view do
+  describe 'rendering feedback' do
+    let(:article) { build_stubbed :article }
+    let(:comment) { build_stubbed(:comment, article: article, body: 'Comment body', guid: '12313123123123123') }
+    let(:trackback) { build_stubbed(:trackback, article: article) }
+
+    before(:each) do
+      assign(:feedback, [comment, trackback])
+      render
+    end
+
+    it 'renders a valid rss feed with two entries' do
+      assert_rss20 rendered, 2
+    end
+
+    describe 'the comment entry' do
+      let(:rendered_entry) { Feedjira::Feed.parse(rendered).entries.first }
+      let(:xml_entry) { Nokogiri::XML.parse(rendered).css('item').first }
+
+      it 'should have all the required attributes' do
+        expect(rendered_entry.title).to eq "Comment on #{article.title} by #{comment.author}"
+        expect(rendered_entry.entry_id).to eq('urn:uuid:12313123123123123')
+        expect(rendered_entry.summary).to eq('<p>Comment body</p>')
+        expect(xml_entry.css('link').first.content).to eq("#{article.permalink_url}#comment-#{comment.id}")
+      end
+    end
+
+    describe 'the trackback entry' do
+      let(:rendered_entry) { Feedjira::Feed.parse(rendered).entries.last }
+      let(:xml_entry) { Nokogiri::XML.parse(rendered).css('item').last }
+
+      it 'should have all the required attributes' do
+        expect(rendered_entry.title).to eq "Trackback from #{trackback.blog_name}: #{trackback.title} on #{article.title}"
+        expect(rendered_entry.entry_id).to eq('urn:uuid:dsafsadffsdsf')
+        expect(rendered_entry.summary).to eq('This is an excerpt')
+        expect(xml_entry.css('link').first.content).to eq(trackback.url)
+      end
+    end
+  end
+end
+
