7501dc0b1266fcc95dbd4890a8d6710943bbe962
Matijs van Zuijlen <matijs@matijs.net>
2018-07-15 13:44:17 +0200

    Remove test_unit railtie

--- publify_core/spec/dummy/config/application.rb
+++ publify_core/spec/dummy/config/application.rb
@@ -11,7 +11,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 # require "action_cable/engine"
 require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
