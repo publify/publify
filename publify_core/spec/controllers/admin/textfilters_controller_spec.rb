476c2923647983d87a105e5bcab740d80bb751d0
Matijs van Zuijlen <matijs@matijs.net>
2018-10-15 10:59:52 +0200

    Improve Admin::TextfiltersController specs

--- publify_core/spec/controllers/admin/textfilters_controller_spec.rb
+++ publify_core/spec/controllers/admin/textfilters_controller_spec.rb
@@ -6,12 +6,19 @@ describe Admin::TextfiltersController, type: :controller do
   render_views
 
   describe 'macro help action' do
-    it 'renders success' do
+    before do
       create(:blog)
       henri = create(:user, :as_admin)
       sign_in henri
       get 'macro_help', params: { id: 'textile' }
+    end
+
+    it 'renders success' do
       expect(response).to be_successful
+    end
+
+    it 'contains help text' do
+      expect(response.body).to match /Textile reference/
     end
   end
 end
