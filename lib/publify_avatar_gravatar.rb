5e08fb1cb2d7f2241baf3dffca17daefe5a74825
Matijs van Zuijlen <matijs@matijs.net>
2018-11-12 13:21:17 +0100

    Fix gravatar request URL
    - Avoid double escape of ampersands
    - Always use https URL

--- lib/publify_avatar_gravatar.rb
+++ lib/publify_avatar_gravatar.rb
@@ -24,12 +24,13 @@ module PublifyPlugins
       # Generate the image tag for a commenters gravatar based on their email address
       # Valid options are described at http://www.gravatar.com/implement.php
       def gravatar_tag(email, options = {})
-        options[:gravatar_id] = Digest::MD5.hexdigest(email.strip)
-        options[:default] = CGI.escape(options[:default]) if options.include?(:default)
-        options[:size] ||= 48
+        opts = {}
+        opts[:gravatar_id] = Digest::MD5.hexdigest(email.strip)
+        opts[:default] = CGI.escape(options[:default]) if options.include?(:default)
+        opts[:size] = options[:size] || 48
         klass = options[:class] || 'avatar gravatar'
 
-        url = +'//www.gravatar.com/avatar.php?' << options.map { |key, value| "#{key}=#{value}" }.sort.join('&amp;')
+        url = +'https://www.gravatar.com/avatar.php?' << opts.map { |key, value| "#{key}=#{value}" }.sort.join('&')
         tag 'img', src: url, class: klass, alt: 'Gravatar'
       end
     end
