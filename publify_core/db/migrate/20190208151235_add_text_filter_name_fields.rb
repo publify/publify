ed1a4e64bf0503a0250b2b59ab142cc7f895380c
Matijs van Zuijlen <matijs@matijs.net>
2019-02-08 16:26:00 +0100

    Add text_filter_name columns

---
+++ publify_core/db/migrate/20190208151235_add_text_filter_name_fields.rb
@@ -0,0 +1,7 @@
+class AddTextFilterNameFields < ActiveRecord::Migration[5.2]
+  def change
+    add_column :contents, :text_filter_name, :string
+    add_column :feedback, :text_filter_name, :string
+    add_column :users, :text_filter_name, :string
+  end
+end
--- publify_core/spec/dummy/db/schema.rb
+++ publify_core/spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_07_02_105201) do
+ActiveRecord::Schema.define(version: 2019_02_08_151235) do
 
   create_table "blogs", force: :cascade do |t|
     t.text "settings"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2017_07_02_105201) do
     t.text "settings"
     t.string "post_type", default: "read"
     t.integer "blog_id", null: false
+    t.string "text_filter_name"
     t.index ["id", "type"], name: "index_contents_on_id_and_type"
     t.index ["text_filter_id"], name: "index_contents_on_text_filter_id"
     t.index ["user_id"], name: "index_contents_on_user_id"
@@ -71,6 +72,7 @@ ActiveRecord::Schema.define(version: 2017_07_02_105201) do
     t.string "blog_name"
     t.string "state"
     t.string "user_agent"
+    t.string "text_filter_name"
     t.index ["article_id"], name: "index_feedback_on_article_id"
     t.index ["id", "type"], name: "index_feedback_on_id_and_type"
     t.index ["text_filter_id"], name: "index_feedback_on_text_filter_id"
@@ -192,6 +194,7 @@ ActiveRecord::Schema.define(version: 2017_07_02_105201) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "profile"
+    t.string "text_filter_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["resource_id"], name: "index_users_on_resource_id"
