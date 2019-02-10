0283ce8a9b78be596a3556a2259bd57c63ad66df
Matijs van Zuijlen <matijs@matijs.net>
2019-02-10 13:14:54 +0100

    Migrate main app database

---
+++ db/migrate/20190210121317_remove_text_filters.publify_core_engine.rb
@@ -0,0 +1,16 @@
+# This migration comes from publify_core_engine (originally 20190209160610)
+class RemoveTextFilters < ActiveRecord::Migration[5.2]
+  def up
+    drop_table :text_filters
+  end
+
+  def down
+    create_table :text_filters do |t|
+      t.string :name
+      t.string :description
+      t.string :markup
+      t.text :filters
+      t.text :params
+    end
+  end
+end
