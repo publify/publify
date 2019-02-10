bf91b7452401e07649d21a7356da19925e385b53
Matijs van Zuijlen <matijs@matijs.net>
2019-02-10 13:16:47 +0100

    Migrate dummy app for publify_textfilter_code

---
+++ publify_textfilter_code/spec/dummy/db/migrate/20190210121621_add_text_filter_name_fields.publify_core_engine.rb
@@ -0,0 +1,8 @@
+# This migration comes from publify_core_engine (originally 20190208151235)
+class AddTextFilterNameFields < ActiveRecord::Migration[5.2]
+  def change
+    add_column :contents, :text_filter_name, :string
+    add_column :feedback, :text_filter_name, :string
+    add_column :users, :text_filter_name, :string
+  end
+end
