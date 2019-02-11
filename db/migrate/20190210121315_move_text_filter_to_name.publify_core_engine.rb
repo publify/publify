a0211f1f1bcf026618998f542bef6528fed8e91c
Matijs van Zuijlen <matijs@matijs.net>
2019-02-11 09:39:44 +0100

    Autocorrect Layout/EmptyLineAfterGuardClause

--- db/migrate/20190210121315_move_text_filter_to_name.publify_core_engine.rb
+++ db/migrate/20190210121315_move_text_filter_to_name.publify_core_engine.rb
@@ -64,6 +64,7 @@ class MoveTextFilterToName < ActiveRecord::Migration[5.2]
     Content.find_each do |content|
       filter_name = content.text_filter_name
       next unless filter_name
+
       filter = TextFilter.find(name: filter_name)
       raise "Filter #{filter_name} not found" unless filter
       content.update!(text_filter: filter)
@@ -72,6 +73,7 @@ class MoveTextFilterToName < ActiveRecord::Migration[5.2]
     Feedback.find_each do |feedback|
       filter_name = feedback.text_filter_name
       next unless filter_name
+
       filter = TextFilter.find(name: filter_name)
       raise "Filter #{filter_name} not found" unless filter
       feedback.update!(text_filter: filter)
@@ -80,6 +82,7 @@ class MoveTextFilterToName < ActiveRecord::Migration[5.2]
     User.find_each do |user|
       filter_name = user.text_filter_name
       next unless filter_name
+
       filter = TextFilter.find(name: filter_name)
       raise "Filter #{filter_name} not found" unless filter
       user.update!(text_filter: filter)
