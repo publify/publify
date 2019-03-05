1b50ecfd5e23b6602c21236221dee543cccecbba
Matijs van Zuijlen <matijs@matijs.net>
2019-03-05 09:46:17 +0100

    Remove carousel element

--- publify_core/app/assets/javascripts/bootstrap-sprockets.js
+++ publify_core/app/assets/javascripts/bootstrap-sprockets.js
@@ -1,7 +1,6 @@
 //= require ./bootstrap/affix
 //= require ./bootstrap/alert
 //= require ./bootstrap/button
-//= require ./bootstrap/carousel
 //= require ./bootstrap/collapse
 //= require ./bootstrap/dropdown
 //= require ./bootstrap/modal
