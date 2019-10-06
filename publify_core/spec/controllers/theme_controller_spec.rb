cce5f440c29caa8250a76eb35311004e593b192b
Charlie Savage <cfis@zerista.com>
2019-10-05 18:59:52 -0700

    Fix failing tests.

--- publify_core/spec/controllers/theme_controller_spec.rb
+++ publify_core/spec/controllers/theme_controller_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe ThemeController, type: :controller do
     assert_response :success
     assert_equal "text/css", @response.content_type
     assert_equal "utf-8", @response.charset
-    assert_equal 'inline; filename="theme.css"', @response.headers["Content-Disposition"]
+    assert_equal 'inline; filename="theme.css"; filename*=UTF-8\'\'theme.css', @response.headers["Content-Disposition"]
   end
 
   it "test_javascripts" do
@@ -18,7 +18,7 @@ RSpec.describe ThemeController, type: :controller do
     assert_response :success
     assert_equal "text/javascript", @response.content_type
     assert_equal "utf-8", @response.charset
-    assert_equal 'inline; filename="theme.js"', @response.headers["Content-Disposition"]
+    assert_equal 'inline; filename="theme.js"; filename*=UTF-8\'\'theme.js', @response.headers["Content-Disposition"]
   end
 
   it "test_malicious_path" do
