3d77e1578f78ec0b8c579f1e564cd2c92b40e957
Matijs van Zuijlen <matijs@matijs.net>
2019-11-17 18:31:40 +0100

    Use less antagonistic gem to get digestless assets

---
+++ config/initializers/non_digest_assets.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+NonDigestAssets.whitelist = [/\.(?:svg|eot|woff|ttf)$/]
--- config/initializers/non_stupid_digest_assets.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-NonStupidDigestAssets.whitelist = [/\.(?:svg|eot|woff|ttf)$/]
