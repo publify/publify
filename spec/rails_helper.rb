5a9df7cd764b6b32552c444b1c8babd4cb283f8c
Matijs van Zuijlen <matijs@matijs.net>
2020-01-04 15:19:27 +0100

    Autocorrect Lint/NonDeterministicRequireOrder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,7 +17,7 @@ end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
