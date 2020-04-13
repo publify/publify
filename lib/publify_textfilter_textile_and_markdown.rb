c1811ee33a1e16d66401f10c907f9c50c771a7b8
Matijs van Zuijlen <matijs@matijs.net>
2020-04-13 20:14:18 +0200

    Deprecate unused Textile with Markdown filter

--- lib/publify_textfilter_textile_and_markdown.rb
+++ lib/publify_textfilter_textile_and_markdown.rb
@@ -7,6 +7,8 @@ class PublifyApp
       plugin_description "Textile and Markdown markup languages"
 
       def self.filtertext(text)
+        ActiveSupport::Deprecation.warn \
+          "Use of the Textile with Markdown filter is deprecated"
         RedCloth.new(text).to_html
       end
     end
