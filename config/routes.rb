761bbd6ee3e9140661adb18d822cd098cb3890a4
Matijs van Zuijlen <matijs@matijs.net>
2021-01-29 17:38:40 +0100

    Run rake app:update and adjust results
    - The new migration for ActiveStorage was skipped since Publify does not
      use it at the moment.
    - RuboCop offenses were fixed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
