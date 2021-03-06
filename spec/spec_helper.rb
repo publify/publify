5a3f7ee445406aa7bea483a16b87cc517b40a836
Matijs van Zuijlen <matijs@matijs.net>
2021-03-06 11:12:24 +0100

    Disable should syntax. All uses have already been removed earlier

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,12 +40,6 @@ RSpec.configure do |config|
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
-    # Enable only the newer, non-monkey-patching expect syntax.
-    # For more details, see:
-    #   - http://teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
-    # TODO: Disable :should syntax
-    mocks.syntax = [:expect, :should]
-
     # Prevents you from mocking or stubbing a method that does not exist on
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
