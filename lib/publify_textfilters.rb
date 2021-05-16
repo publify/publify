1497d6a4103c1338ad6be40e65ab546dad2da8f9
Matijs van Zuijlen <matijs@matijs.net>
2021-05-16 09:45:11 +0200

    Autocorrect Style/StringLiterals

--- lib/publify_textfilters.rb
+++ lib/publify_textfilters.rb
@@ -4,7 +4,7 @@ require "publify_plugins"
 require "text_filter_plugin"
 
 plugins_root = File.join(::Rails.root.to_s, "lib")
-separator = plugins_root.include?("/") ? "/" : '\\'
+separator = plugins_root.include?("/") ? "/" : "\\"
 
 Dir.glob(File.join(plugins_root, "*_textfilter_*")).select do |file|
   require file.split(separator).last.gsub(".rb", "")
