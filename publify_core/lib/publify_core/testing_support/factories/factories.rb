ab088d44fbe5c96981304e3afbd83c0bdbf90389
VictorPS <victorpsilvano@gmail.com>
2021-10-15 12:00:12 +0100

    Moving resources out of factories.rb

--- publify_core/lib/publify_core/testing_support/factories/factories.rb
+++ publify_core/lib/publify_core/testing_support/factories/factories.rb
@@ -4,19 +4,6 @@ require "publify_core/testing_support/upload_fixtures"
 
 # Factory definitions
 FactoryBot.define do
-  factory :resource do
-    upload { PublifyCore::TestingSupport::UploadFixtures.file_upload }
-    mime { "image/jpeg" }
-    size { 110 }
-    blog { Blog.first || create(:blog) }
-  end
-
-  factory :avatar, parent: :resource do
-    upload { "avatar.jpg" }
-    mime { "image/jpeg" }
-    size { 600 }
-  end
-
   factory :redirect do
     from_path { "foo/bar" }
     to_path { "/someplace/else" }
