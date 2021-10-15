0acc9107a85207e6ab5a87c7402b80b5800026cc
VictorPS <victorpsilvano@gmail.com>
2021-10-15 11:32:41 +0100

    Moving sequences out of factories.rb

--- publify_core/lib/publify_core/testing_support/factories/factories.rb
+++ publify_core/lib/publify_core/testing_support/factories/factories.rb
@@ -4,15 +4,6 @@ require "publify_core/testing_support/upload_fixtures"
 
 # Factory definitions
 FactoryBot.define do
-  sequence(:name) { |n| "name_#{n}" }
-  sequence(:body) { |n| "body #{n}" * (n + 3 % 5) }
-  sequence(:user) { |n| "user#{n}" }
-  sequence(:email) { |n| "user#{n}@example.com" }
-  sequence(:guid) { |n| "deadbeef#{n}" }
-  sequence(:label) { |n| "lab_#{n}" }
-  sequence(:file_name) { |f| "file_name_#{f}" }
-  sequence(:time) { |n| Time.new(2012, 3, 26, 19, 56).in_time_zone - n }
-
   factory :user do
     login { FactoryBot.generate(:user) }
     email { generate(:email) }
---
+++ publify_core/lib/publify_core/testing_support/factories/sequences.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  sequence(:name) { |n| "name_#{n}" }
+  sequence(:body) { |n| "body #{n}" * (n + 3 % 5) }
+  sequence(:user) { |n| "user#{n}" }
+  sequence(:email) { |n| "user#{n}@example.com" }
+  sequence(:guid) { |n| "deadbeef#{n}" }
+  sequence(:label) { |n| "lab_#{n}" }
+  sequence(:file_name) { |f| "file_name_#{f}" }
+  sequence(:time) { |n| Time.new(2012, 3, 26, 19, 56).in_time_zone - n }
+end
