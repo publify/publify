262b18fdb16f831fe9bd1d22f5babbd5655b8695
VictorPS <victorpsilvano@gmail.com>
2021-10-15 12:01:27 +0100

    Moving comments out of factories.rb

---
+++ publify_core/lib/publify_core/testing_support/factories/comments.rb
@@ -0,0 +1,38 @@
+FactoryBot.define do
+  factory :comment do
+    article
+    author { "Bob Foo" }
+    url { "http://fakeurl.com" }
+    body { "Comment body" }
+    guid
+    state { "ham" }
+
+    factory :unconfirmed_comment do
+      state { "presumed_ham" }
+    end
+
+    factory :published_comment do
+      state { "ham" }
+    end
+
+    factory :not_published_comment do
+      state { "spam" }
+    end
+
+    factory :ham_comment do
+      state { "ham" }
+    end
+
+    factory :presumed_ham_comment do
+      state { "presumed_ham" }
+    end
+
+    factory :presumed_spam_comment do
+      state { "presumed_spam" }
+    end
+
+    factory :spam_comment do
+      state { "spam" }
+    end
+  end
+end
--- publify_core/lib/publify_core/testing_support/factories/factories.rb
+++ publify_core/lib/publify_core/testing_support/factories/factories.rb
@@ -4,43 +4,6 @@ require "publify_core/testing_support/upload_fixtures"
 
 # Factory definitions
 FactoryBot.define do
-  factory :comment do
-    article
-    author { "Bob Foo" }
-    url { "http://fakeurl.com" }
-    body { "Comment body" }
-    guid
-    state { "ham" }
-
-    factory :unconfirmed_comment do
-      state { "presumed_ham" }
-    end
-
-    factory :published_comment do
-      state { "ham" }
-    end
-
-    factory :not_published_comment do
-      state { "spam" }
-    end
-
-    factory :ham_comment do
-      state { "ham" }
-    end
-
-    factory :presumed_ham_comment do
-      state { "presumed_ham" }
-    end
-
-    factory :presumed_spam_comment do
-      state { "presumed_spam" }
-    end
-
-    factory :spam_comment do
-      state { "spam" }
-    end
-  end
-
   factory :page do
     name { FactoryBot.generate(:name) }
     title { "Page One Title" }
