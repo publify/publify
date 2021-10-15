dc7944553f0b21147c7a6592a228dd2a01c7a7ec
VictorPS <victorpsilvano@gmail.com>
2021-10-15 11:33:20 +0100

    Moving users out of factories.rb

--- publify_core/lib/publify_core/testing_support/factories/factories.rb
+++ publify_core/lib/publify_core/testing_support/factories/factories.rb
@@ -4,46 +4,6 @@ require "publify_core/testing_support/upload_fixtures"
 
 # Factory definitions
 FactoryBot.define do
-  factory :user do
-    login { FactoryBot.generate(:user) }
-    email { generate(:email) }
-    name { "Bond" }
-    nickname { "James Bond" }
-    notify_via_email { false }
-    notify_on_new_articles { false }
-    notify_on_comments { false }
-    password { "top-secret" }
-    state { "active" }
-    profile { User::CONTRIBUTOR }
-
-    trait :without_twitter do
-      twitter { nil }
-      association :resource, nil
-    end
-
-    trait :with_a_full_profile do
-      description { "I am a poor lonesone factory generated user" }
-      url { "http://myblog.net" }
-      msn { "random@mail.com" }
-      aim { "randomaccount" }
-      yahoo { "anotherrandomaccount" }
-      twitter { "@random" }
-      jabber { "random@account.com" }
-    end
-
-    trait :as_admin do
-      profile { User::ADMIN }
-    end
-
-    trait :as_publisher do
-      profile { User::PUBLISHER }
-    end
-
-    trait :as_contributor do
-      profile { User::CONTRIBUTOR }
-    end
-  end
-
   factory :article do
     title { "A big article" }
     body { "A content with several data" }
