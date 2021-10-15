5e3c38e92bf6d3acd4e7eead4c4d1fcca138c01b
VictorPS <victorpsilvano@gmail.com>
2021-10-15 11:52:00 +0100

    Moving blogs out of factories.rb

--- publify_core/lib/publify_core/testing_support/factories/factories.rb
+++ publify_core/lib/publify_core/testing_support/factories/factories.rb
@@ -4,27 +4,6 @@ require "publify_core/testing_support/upload_fixtures"
 
 # Factory definitions
 FactoryBot.define do
-  factory :blog do
-    base_url { "http://test.host/blog" }
-    hide_extended_on_rss { true }
-    blog_name { "test blog" }
-    limit_article_display { 2 }
-    sp_url_limit { 3 }
-    plugin_avatar { "" }
-    blog_subtitle { "test subtitle" }
-    limit_rss_display { 10 }
-    geourl_location { "" }
-    default_allow_pings { false }
-    send_outbound_pings { false }
-    sp_global { true }
-    default_allow_comments { true }
-    email_from { "scott@sigkill.org" }
-    sp_article_auto_close { 0 }
-    permalink_format { "/%year%/%month%/%day%/%title%" }
-    rss_description_text { "rss description text" }
-    lang { "en_US" }
-  end
-
   factory :tag do |tag|
     tag.name { FactoryBot.generate(:name) }
     tag.display_name { |a| a.name } # rubocop:disable Style/SymbolProc
