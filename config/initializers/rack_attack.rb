503a8560122ddb90b2227a3881006cdb79f1aa1d
Matijs van Zuijlen <matijs@matijs.net>
2021-10-23 14:59:37 +0200

    Rate-limit Devise logins and password resets

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,13 @@
+# Throttle login attempts
+Rack::Attack.throttle("logins/ip", limit: 20, period: 1.hour) do |req|
+  req.ip if req.post? && req.path.start_with?("/users/sign_in")
+end
+
+# Throttle password reset attempts
+Rack::Attack.throttle("password-reset-requests/ip", limit: 20, period: 1.hour) do |req|
+  req.ip if req.post? && req.path.start_with?("/users/password")
+end
+
+ActiveSupport::Notifications.subscribe("rack.attack") do |name, start, finish, request_id, req|
+  Rails.logger.info "Throttled #{req.env["rack.attack.match_discriminator"]}"
+end
