284d5c963fb2909f024af118c74d06cb8a7c1d77
Matijs van Zuijlen <matijs@matijs.net>
2021-11-13 18:01:08 +0100

    Wrap long lines

--- publify_core/spec/views/articles/index_atom_feed_spec.rb
+++ publify_core/spec/views/articles/index_atom_feed_spec.rb
@@ -156,7 +156,8 @@ RSpec.describe "articles/index_atom_feed.atom.builder", type: :view do
       it "shows only a link to the article" do
         expect(rendered_entry.content).
           to eq "<p>This article is password protected. Please" \
-                " <a href='#{@article.permalink_url}'>fill in your password</a> to read it</p>"
+                " <a href='#{@article.permalink_url}'>fill in your password</a>" \
+                " to read it</p>"
       end
 
       it "does not have a summary element in addition to the content element" do
