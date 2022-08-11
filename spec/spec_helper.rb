89579f525d704028e99984f915496c6072d47615
Matijs van Zuijlen <matijs@matijs.net>
2022-08-11 14:57:34 +0200

    Make rspec's -n and --only-failures options work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,11 @@ RSpec.configure do |config|
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
 
+  # Allows RSpec to persist some state between runs in order to support
+  # the `--only-failures` and `--next-failure` CLI options. We recommend
+  # you configure your source control system to ignore this file.
+  config.example_status_persistence_file_path = "spec/examples.txt"
+
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
   # individual spec file.
