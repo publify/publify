bcd4ee87238bab19c37363fc6c65bf55f8d9b98d
Matijs van Zuijlen <matijs@matijs.net>
2023-04-10 23:25:27 +0200

    Fix Capybara/SpecificMatcher offenses

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -101,7 +101,7 @@ RSpec.describe ArticlesController, type: :controller do
 
           it "shows a password form for the article" do
             get :redirect, params: { from: from_param }
-            expect(response.body).to have_selector('input[id="article_password"]', count: 1)
+            expect(response.body).to have_field "article_password"
           end
 
           it "does not include the article body anywhere" do
@@ -370,14 +370,14 @@ RSpec.describe ArticlesController, type: :controller do
           post :check_password, xhr: true,
                                 params: { article: { id: article.id,
                                                      password: article.password } }
-          expect(response.body).not_to have_selector('input[id="article_password"]')
+          expect(response.body).not_to have_field "article_password"
         end
 
         it "shows password form when given incorrect password" do
           post :check_password, xhr: true,
                                 params: { article: { id: article.id,
                                                      password: "wrong password" } }
-          expect(response.body).to have_selector('input[id="article_password"]')
+          expect(response.body).to have_field "article_password"
         end
       end
     end
