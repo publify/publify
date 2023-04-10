c1ab721e4f6f1e0275f5db374d61298255abaa25
Matijs van Zuijlen <matijs@matijs.net>
2023-04-10 14:47:33 +0200

    Autocorrect Style/RedundantConstantBase

--- lib/publify_textfilters.rb
+++ lib/publify_textfilters.rb
@@ -3,7 +3,7 @@
 require "publify_plugins"
 require "text_filter_plugin"
 
-plugins_root = File.join(::Rails.root.to_s, "lib")
+plugins_root = File.join(Rails.root.to_s, "lib")
 separator = plugins_root.include?("/") ? "/" : "\\"
 
 Dir.glob(File.join(plugins_root, "*_textfilter_*")).select do |file|
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,7 +56,7 @@ end
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = "#{Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
