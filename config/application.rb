ec8dfc4926fad2a1b3537a8ab731b2cb48390d33
Matijs van Zuijlen <matijs@matijs.net>
2025-09-14 11:35:54 +0200

    Enable Rails 7.1 framework defaults that are safe before first deploy

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Publify
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.add_autoload_paths_to_load_path = false
 
     # Configuration for the application, engines, and railties goes here.
     #
